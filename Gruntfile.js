de4a8e3e5c2d8257079eb89cd21106fc96b29e2d
Chris Malley <cmalley@pixelzoom.com>
2013-05-10 15:10:50 -0600

    move Gruntfile.js and build.sh to chipper, add prerequisite checks, use curly braces sparingly to disambiguate shell vars

---
+++ Gruntfile.js
@@ -0,0 +1,127 @@
+/**
+ * Grunt configuration file for simulations.
+ * Requires a package.json file containing project settings.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+module.exports = function( grunt ) {
+
+  // Project configuration.
+  grunt.initConfig(
+      {
+        // Read in the project settings from the package.json file into the pkg property.
+        // This allows us to refer to project settings from within this config file.
+        pkg: grunt.file.readJSON('package.json'),
+
+        // configure the RequireJS plugin
+        requirejs: {
+
+          // builds the minified script
+          build: {
+            options: {
+              almond: true,
+              mainConfigFile: "js/<%= pkg.project %>-config.js",
+              out: "<%= pkg.outputDir %>/<%= pkg.project %>.min.js",
+              name: "<%= pkg.project %>-config",
+              optimize: 'uglify2',
+              wrap: true
+            },
+            uglify2: {
+              compress: {
+                global_defs: {
+                  // scenery assertions
+                  sceneryAssert: false,
+                  sceneryExtraAssert: false,
+                  // scenery logging
+                  sceneryLayerLog: false,
+                  sceneryEventLog: false
+                },
+                dead_code: true
+              }
+            }
+          }
+        },
+
+        // configure the JSHint plugin
+        jshint: {
+          // source files that are specific to this simulation
+          simFiles: [ 'Gruntfile.js', 'js/**/*.js' ],
+          // source files for common-code dependencies
+          commonFiles: [
+            '../assert/js/**/*.js',
+            '../dot/js/**/*.js',
+            '../fort/js/**/*.js',
+            '../joist/js/**/*.js',
+            '../kite/js/**/*.js',
+            '../phet-core/js/**/*.js',
+            '../phetcommon/js/**/*.js',
+            '../scenery/js/**/*.js',
+            '../scenery-phet/js/**/*.js',
+            '../sun/js/**/*.js'
+          ],
+          options: {
+            // options documented at http://www.jshint.com/docs/#options
+
+            // enforcing options
+            curly: true, // require braces around blocks for loops and conditionals
+            eqeqeq: true, // prohibit == and !=, use === and !===
+            immed: true, // prohibits the use of immediate function invocations without wrapping them in parentheses
+            latedef: true, // prohibits the use of a variable before it was defined
+            newcap: true, // requires you to capitalize names of constructor functions
+            noarg: true, // prohibits the use of arguments.caller and arguments.callee
+            nonew: true, // prohibits calling new without assigning result to a variable
+            undef: true, // prohibits the use of explicitly undeclared variables
+
+            // relaxing options
+            expr: true, // suppresses warnings about the use of expressions where normally you would expect to see assignments or function calls, so we can use assert && assert( ... )
+            es5: true, // we use ES5 getters and setters for now
+            loopfunc: true, // suppresses warnings about defining functions inside of loops, but we know how not to shoot ourselves in the foot, and this is useful for _.each
+
+            // tell JSHint about global variables that are defined elsewhere. If value is false (default), JSHint will consider that variable as read-only.
+            globals: {
+
+              // writable globals ---------------------------------
+
+              sceneryAssert: true,  // for removal of scenery assertions
+              sceneryExtraAssert: true, // for removal of scenery assertions
+              sceneryLayerLog: true, // scenery logging levels
+              sceneryEventLog: true, // scenery logging levels
+              Float32Array: true, // we actually polyfill this, so allow it to be set
+
+              // read-only globals ---------------------------------
+
+              define: false, // require.js
+              require: false, // require.js
+              Uint16Array: false,
+              Uint32Array: false,
+              document: false,
+              window: false,
+              console: false,
+              HTMLImageElement: false,
+              HTMLCanvasElement: false,
+              Backbone: false, // backbone is currently run outside of requirejs
+              module: false, // as used in Gruntfile.js
+              $: false, // jQuery
+              _: false, // underscore, lodash
+              clearTimeout: false,
+              Image: false, // DOM.js
+              Blob: false,  // DOM.js
+              canvg: false
+            }
+          }
+        }
+      } );
+
+  // Default task ('grunt')
+  grunt.registerTask( 'default', [ 'lint', 'lint-common', 'build' ] );
+
+  // Other tasks ('grunt taskName')
+  grunt.registerTask( 'lint', [ 'jshint:simFiles' ] );
+  grunt.registerTask( 'lint-common', [ 'jshint:commonFiles' ] );
+  grunt.registerTask( 'build', [ 'requirejs:build' ] );
+
+  // Load tasks from grunt plugins that have been installed locally using npm.
+  // Put these in package.json and run 'npm install' before running grunt.
+  grunt.loadNpmTasks( 'grunt-requirejs' );
+  grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+};
---
+++ build.sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+#====================================================================================================
+#
+# Builds a simulation for deployment.
+# You must run this from the root level of your simulation repo.
+# Usage: ../chipper/build.sh
+#
+# Author: Chris Malley (PixelZoom, Inc.)
+#
+#====================================================================================================
+
+# variables that can be changed in config
+OUTPUT_DIR=./build
+
+# read the config file
+CONFIG_FILE=./build.config
+if [ ! -f $CONFIG_FILE ]; then
+    echo "missing $CONFIG_FILE"; exit 1
+fi
+source $CONFIG_FILE
+
+# check prerequisite config variables
+if [ -z "$PROJECT" ]; then
+   echo "PROJECT is not set in $CONFIG_FILE"; exit 1
+fi
+
+# variables that CANNOT be changed in config
+GRUNTFILE=../chipper/Gruntfile.js
+PACKAGE_JSON=./package.json
+HTML_FILE=${PROJECT}.html
+
+# check prerequisite files
+for file in $GRUNTFILE $PACKAGE_JSON $HTML_FILE; do
+  if [ ! -f $file ]; then
+    echo "missing $file"; exit 1
+  fi
+done
+
+echo "Building $PROJECT ..."
+
+echo "Cleaning ${OUTPUT_DIR} ..."
+rm -rf $OUTPUT_DIR
+mkdir $OUTPUT_DIR
+
+echo "Creating minified script ..."
+grunt --gruntfile $GRUNTFILE --base `pwd` || { echo 'grunt failed' ; exit 1; }
+
+echo "Copying 3rd-party libs ..."
+cp -rp lib $OUTPUT_DIR
+
+echo "Copying resources ..."
+if [ ! -z "$RESOURCE_DIRS" ]; then
+  for dir in $RESOURCE_DIRS; do
+    cp -rp $dir $OUTPUT_DIR
+  done
+fi
+
+echo "Consolidating CSS files ..."
+if [ -d ./css ]; then
+  cp -rp ./css $OUTPUT_DIR
+else
+  mkdir ${OUTPUT_DIR}/css
+fi
+if [ ! -z $COMMON_CSS ]; then
+  for css in $COMMON_CSS; do
+    cp -p $css ${OUTPUT_DIR}/css
+  done
+fi
+
+echo "Copying scripts that are loaded before RequireJS ..."
+if [ ! -z "$OUTPUT_DIR" ]; then
+  mkdir $OUTPUT_DIR/js
+  for script in $COMMON_SCRIPTS; do
+    cp -p $script ${OUTPUT_DIR}/js
+  done
+fi
+
+echo "Modifying HTML file..."
+
+BACKUP_SUFFIX=.bup
+cp $HTML_FILE $OUTPUT_DIR
+
+# change script tag to load minified script
+sed -i $BACKUP_SUFFIX "s/<script data-main=\"js\/${PROJECT}-config.js\" src=\".*\">/<script type=\"text\/javascript\" src=\"${PROJECT}.min.js\">/g" ${OUTPUT_DIR}/${HTML_FILE}
+
+# change the path of any CSS files that were consolidated
+for css in $COMMON_CSS; do
+  fromFile=`echo $css | sed -e 's/[\/]/\\&\//g' | tr '\&' '\'`
+  toFile=css\\/`basename $css`
+  sed -i $BACKUP_SUFFIX "s/${fromFile}/${toFile}/g" ${OUTPUT_DIR}/${HTML_FILE}
+done
+
+# change the path of any scripts that were copied
+for script in $COMMON_SCRIPTS; do
+  fromFile=`echo $script | sed -e 's/[\/]/\\&\//g' | tr '\&' '\'`
+  toFile=js\\/`basename $script`
+  sed -i $BACKUP_SUFFIX "s/${fromFile}/${toFile}/g" ${OUTPUT_DIR}/${HTML_FILE}
+done
+
+# delete sed backup files
+rm ${OUTPUT_DIR}/${HTML_FILE}${BACKUP_SUFFIX}
+
+echo "Done."
+
+#====================================================================================================
