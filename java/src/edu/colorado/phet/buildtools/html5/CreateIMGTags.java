1f2ad30fdd48089fb9d4f13db407d3f11b7ea7a7
Sam Reid <reids@colorado.edu>
2013-05-13 15:25:06 -0600

    Added Java build files from svn\trunk\simulations-html\build-tools\src to chipper\java\src

---
+++ java/src/edu/colorado/phet/buildtools/html5/CreateIMGTags.java
@@ -0,0 +1,23 @@
+package edu.colorado.phet.buildtools.html5;
+
+import java.io.File;
+
+/**
+ * Creates IMG tags for PNG, JPG and SVG for use with imagesloaded plugin
+ *
+ * @author Sam Reid
+ */
+public class CreateIMGTags {
+    public static void main( String[] args ) {
+        File dir = new File( args[0] );
+        for ( int i = 0; i < dir.listFiles().length; i++ ) {
+            File file = dir.listFiles()[i];
+            String lowername = file.getName().toLowerCase();
+            if ( lowername.endsWith( "png" ) ||
+                 lowername.endsWith( "jpg" ) ||
+                 lowername.endsWith( "svg" ) ) {
+                System.out.println( "<img src=\"images/" + file.getName() + "\"/>" );
+            }
+        }
+    }
+}
---
+++ java/src/edu/colorado/phet/buildtools/html5/XMLToProperties.java
@@ -0,0 +1,58 @@
+package edu.colorado.phet.buildtools.html5;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Properties;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ * Transform Flash XML string translations to RequireJSI18n format.
+ */
+public class XMLToProperties {
+    public static void main( String[] args ) throws IOException, ParserConfigurationException, SAXException {
+        //Source is the localization directory, such as "C:\workingcopy\phet\svn-1.7\trunk\simulations-java\simulations\energy-skate-park\data\energy-skate-park\localization"
+        File source = new File( args[0] );
+
+        //Destination is the target nls root, such as "C:\workingcopy\phet\svn-1.7\trunk\simulations-html5\simulations\energy-skate-park-easeljs-jquerymobile\src\app\nls"
+        File destination = new File( args[1] );
+        destination.mkdirs();
+
+        for ( File file : source.listFiles( new FilenameFilter() {
+            public boolean accept( File dir, String name ) {
+                return name.endsWith( ".xml" );
+            }
+        } ) ) {
+            Properties s = documentToProperties( DocumentBuilderFactory.newInstance().newDocumentBuilder().parse( new FileInputStream( file ) ) );
+            File destFile = new File( destination, file.getName().replace( ".xml", ".properties" ).replace( "_en.properties", ".properties" ) );
+            String header = "";
+            // write properties to file
+            OutputStream outputStream = new FileOutputStream( destFile );
+            s.store( outputStream, header );
+            outputStream.close();
+        }
+    }
+
+    public static final Properties documentToProperties( Document document ) {
+        Properties properties = new Properties();
+        NodeList elements = document.getElementsByTagName( "string" );
+        int numberOfNodes = elements.getLength();
+        for ( int i = 0; i < numberOfNodes; i++ ) {
+            Element element = (Element) elements.item( i );
+            String key = element.getAttribute( "key" );
+            String value = element.getAttribute( "value" );
+            properties.setProperty( key, value );
+        }
+        return properties;
+    }
+}
