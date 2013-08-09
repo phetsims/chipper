f4221105b46cffec0e7744040ef40876255fc839
Jonathan Olson <olsonsjc@gmail.com>
2013-08-08 20:11:41 -0600

    Adding script to build all active simulations, and a file listing the simulations

---
+++ bin/all-build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#====================================================================================================
+#
+# Builds all chipper-like simulations.
+# This must be run from the main "git" directory (the parent of all simulation/chipper repo directories)
+# Usage: chipper/bin/all-build.sh
+#                                     (named all-build because I like build.sh to
+#                                      be tab-completed easily. lazy? yes)
+#
+# Author: Jonathan Olson
+#
+#====================================================================================================
+
+for sim in `cat chipper/bin/data/active-sims | xargs`
+do
+  if [ -d "$sim" ]; then
+    cd $sim                 # build.sh needs to be run from the sim directory
+    npm install             # executes quickly when everything is up to date compared to build.sh
+    ../chipper/bin/build.sh # run the build
+    cd ..                   # and back to the original directory
+  fi
+done
