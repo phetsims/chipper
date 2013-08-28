59c5553499368d60e91de6d81a9595d51cbf707e
Jonathan Olson <olsonsjc@gmail.com>
2013-08-27 22:24:30 -0600

    First pass of build.sh => node.js/grunt

--- grunt/Gruntfile.js
+++ grunt/Gruntfile.js
@@ -1,3 +1,7 @@
+var assert = require( 'assert' );
+var fs = require( 'fs' );
+var child_process = require( 'child_process' );
+
 /**
  * Grunt configuration file for simulations.
  * Requires a package.json file containing project settings.
@@ -5,13 +9,44 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 module.exports = function( grunt ) {
+  function trimWhitespace( str ) {
+    return str.replace( /^\s\s*/, '' ).replace( /\s\s*$/, '' );
+  }
+
+  function padString( str, n ) {
+    while ( str.length < n ) {
+      str += ' ';
+    }
+    return str;
+  }
+  
+  function loadFileAsDataURI( filename ) {
+    var mimeType = {
+      'png': 'image/png',
+      'svg': 'image/svg+xml',
+      'jpg': 'image/jpeg'
+    }[filename.slice( -3 )];
+    assert( mimeType, 'Unknown mime type for filename: ' + filename );
+    
+    return 'data:' + mimeType + ';base64,' + Buffer( fs.readFileSync( filename ) ).toString( 'base64' );
+  }
+  
+  assert( fs.existsSync( 'package.json' ), 'We need to be in a sim directory with package.json' );
+  
+  var pkg = grunt.file.readJSON( 'package.json' );
+  
+  // TODO: eek, this is scary! we are importing from the sim dir. ideally we should just have uglify-js installed once in chipper?
+  var uglify = require( '../../' + pkg.name + '/node_modules/uglify-js' );
+  var requirejs = require( '../../' + pkg.name + '/node_modules/requirejs' );
+  
+  var preloadMapFilename = 'preload.js.map';
 
   // Project configuration.
   grunt.initConfig(
       {
         // Read in the project settings from the package.json file into the pkg property.
         // This allows us to refer to project settings from within this config file.
-        pkg: grunt.file.readJSON('package.json'),
+        pkg: pkg,
 
         // configure the RequireJS plugin
         requirejs: {
@@ -20,14 +55,17 @@ module.exports = function( grunt ) {
           build: {
             options: {
               almond: true,
-              mainConfigFile: "js/<%= pkg.name %>-config.js",
-              out: "build/<%= pkg.name %>.min.js",
-              name: "<%= pkg.name %>-config",
+              mainConfigFile: 'js/<%= pkg.name %>-config.js',
+              out: 'build/<%= pkg.name %>.min.js',
+              name: '<%= pkg.name %>-config',
               optimize: 'uglify2',
               wrap: true,
               // generateSourceMaps: true,
               // preserveLicenseComments: false,
               uglify2: {
+                output: {
+                  inline_script: true // escape </script
+                },
                 compress: {
                   global_defs: {
                     // scenery assertions
@@ -73,6 +111,141 @@ module.exports = function( grunt ) {
   grunt.registerTask( 'lint-sim', [ 'jshint:simFiles' ] );
   grunt.registerTask( 'lint-common', [ 'jshint:commonFiles' ] );
   grunt.registerTask( 'build', [ 'requirejs:build' ] );
+  
+  // creates a performance snapshot for profiling changes
+  grunt.registerTask( 'buildSim', 'Description', function() {
+    var done = this.async();
+    
+    grunt.log.writeln( 'Building simulation: ' + pkg.name + ' ' + pkg.version );
+    
+    assert( pkg.name, 'name required in package.json' );
+    assert( pkg.version, 'version required in package.json' );
+    assert( pkg.phetLibs, 'phetLibs required in package.json' );
+    assert( pkg.preload, 'preload required in package.json' );
+    
+    if ( fs.existsSync( 'build' ) ) {
+      grunt.log.writeln( 'Cleaning build directory' );
+      grunt.file.delete( 'build' );
+    }
+    grunt.file.mkdir( 'build' );
+    
+    grunt.log.writeln( 'Minifying preload scripts' );
+    var preloadResult = uglify.minify( pkg.preload.split( ' ' ), {
+      outSourceMap: preloadMapFilename,
+      output: {
+        inline_script: true // escape </script
+      },
+      compress: {
+        // here for now, for when we want to include options for the preloaded code
+        // global_defs: {
+        //   // scenery assertions
+        //   sceneryAssert: false,
+        //   sceneryAssertExtra: false,
+        //   // scenery logging
+        //   sceneryLayerLog: false,
+        //   sceneryEventLog: false,
+        //   sceneryAccessibilityLog: false
+        // },
+        // dead_code: true
+      }
+    } );
+    
+    var preloadJS = preloadResult.code; // minified output
+    var preloadMap = preloadResult.map; // map for minified output, use preloadMapFilename
+    
+    grunt.log.writeln( 'Copying preload source map' );
+    grunt.file.write( 'build/' + preloadMapFilename, preloadMap );
+    
+    grunt.log.writeln( 'Running Require.js optimizer' );
+    requirejs.optimize( {
+      almond: true,
+      mainConfigFile: 'js/' + pkg.name + '-config.js',
+      out: 'build/' + pkg.name + '.min.js',
+      name: pkg.name + '-config',
+      optimize: 'uglify2',
+      wrap: true,
+      generateSourceMaps: true,
+      preserveLicenseComments: false,
+      uglify2: {
+        output: {
+          inline_script: true // escape </script
+        },
+        compress: {
+          global_defs: {
+            // scenery assertions
+            sceneryAssert: false,
+            sceneryAssertExtra: false,
+            // scenery logging
+            sceneryLayerLog: false,
+            sceneryEventLog: false,
+            sceneryAccessibilityLog: false
+          },
+          dead_code: true
+        }
+      }
+    }, function( response ) {
+      
+      grunt.log.writeln( 'Copying changes.txt' );
+      grunt.file.copy( 'changes.txt', 'build/changes.txt' );
+      
+      // we ignore resourceDirs here, deprecated!
+      
+      var dependencies = pkg.phetLibs.split( ' ' );
+      dependencies.push( 'chipper' );
+      
+      var dependencyInfo = {};
+      
+      // git --git-dir ../scenery/.git rev-parse HEAD                 -- sha
+      // git --git-dir ../scenery/.git rev-parse --abbrev-ref HEAD    -- branch
+      function nextDependency() {
+        if ( dependencies.length ) {
+          var dependency = dependencies.pop();
+          
+          // get the SHA
+          child_process.exec( 'git --git-dir ../' + dependency + '/.git rev-parse HEAD', function( error, stdout, stderr ) {
+            assert( !error, error ? ( 'ERROR on git SHA attempt: code: ' + error.code + ', signal: ' + error.signal + ' with stderr:\n' + stderr ) : 'An error without an error? not good' );
+            
+            var sha = trimWhitespace( stdout );
+            
+            // get the branch
+            child_process.exec( 'git --git-dir ../' + dependency + '/.git rev-parse --abbrev-ref HEAD', function( error, stdout, stderr ) {
+              assert( !error, error ? ( 'ERROR on git branch attempt: code: ' + error.code + ', signal: ' + error.signal + ' with stderr:\n' + stderr ) : 'An error without an error? not good' );
+              
+              var branch = trimWhitespace( stdout );
+              
+              grunt.log.writeln( padString( dependency, 20 ) + branch + ' ' + sha );
+              dependencyInfo[dependency] = { sha: sha, branch: branch };
+              
+              
+              nextDependency();
+            } );
+          } );
+        } else {
+          // now continue on with the process! CALLBACK SOUP FOR YOU!
+          
+          // TODO: finish!
+          grunt.log.writeln( 'Writing dependencies.txt' );
+          grunt.file.write( 'build/dependencies.txt', '# ' + pkg.name + ' ' + pkg.version + ' ' + (new Date().toString()) + '\n' +
+                                                      JSON.stringify( dependencyInfo, null, 2 ) + '\n' );
+          
+          var splashDataURI = loadFileAsDataURI( '../joist/images/phet-logo-loading.svg' );
+          var mainInlineJavascript = grunt.file.read( 'build/' + pkg.name + '.min.js' );
+          
+          var html = grunt.file.read( '../chipper/templates/sim.html' )
+                               .replace( 'SPLASH_SCREEN_DATA_URI', splashDataURI )
+                               .replace( 'PRELOAD_INLINE_JAVASCRIPT', preloadJS )
+                               .replace( 'MAIN_INLINE_JAVASCRIPT', mainInlineJavascript );
+          
+          grunt.file.write( 'build/' + pkg.name + '_en.html', html );
+          
+          done();
+        }
+      }
+      
+      grunt.log.writeln( 'Scanning dependencies from:\n' + dependencies.toString() );
+      nextDependency();
+    } );
+  } );
 
   // Load tasks from grunt plugins that have been installed locally using npm.
   // Put these in package.json and run 'npm install' before running grunt.
