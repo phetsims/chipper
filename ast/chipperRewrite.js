e7de4c526a0d27455c7c54b7faabc95c456bc6e2
Jonathan Olson <olsonsjc@gmail.com>
2013-09-18 14:01:54 -0600

    Stub for testing AST rewriting to remove assertions (and modify any code)

---
+++ ast/chipperRewrite.js
@@ -0,0 +1,14 @@
+function chipperRewrite( contents ) {
+  var ast = esprima.parse( contents, { raw: true, tokens: true, range: true, comment: true } );
+  // ast = escodegen.attachComments( ast, ast.comments, ast.tokens );
+  var result = escodegen.generate( ast, {
+    comment: true,
+    format: {
+      indent: {
+        style: '  '
+      },
+      quotes: 'single'
+    }
+  } );
+  return result;
+}
