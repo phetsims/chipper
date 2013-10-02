2a23dfa398f4ba2ef330be1507fff4f32dec8476
samreid <reids@colorado.edu>
2013-10-02 08:23:34 -0600

    store strings in object literal instead of array

--- requirejs-plugins/string.js
+++ requirejs-plugins/string.js
@@ -107,8 +107,10 @@ define( function() {
         //Enumerate all of the strings used by the sim, with no false positives
         //TODO: A better way to do this without globals?  Perhaps the export value of this function?
         //TODO: if we stick with globals, make sure the globalStrings array is clean (undefined) when we start and delete it when we are done with it.
-        global.globalStrings = global.globalStrings || [];
-        global.globalStrings.push( {key: moduleName, value: value} );
+        global.globalStrings = global.globalStrings || {};
+
+        //TODO: make sure this string hasn't already been written (especially with a different value)
+        global.globalStrings[moduleName] = value;
       }
     }
   };
