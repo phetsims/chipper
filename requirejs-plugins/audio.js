bfdf42bee22873c617f725810d93a99f485892bd
samreid <reids@colorado.edu>
2013-10-08 09:56:24 -0600

    factored out loadFileAsDataURI.js

--- requirejs-plugins/audio.js
+++ requirejs-plugins/audio.js
@@ -8,33 +8,13 @@
  *
  * @author Sam Reid
  */
-define( function() {
+define( [
+
+  //Path is relative to the simulation directory where grunt is run from
+  '../../chipper/requirejs-plugins/loadFileAsDataURI'], function( loadFileAsDataURI ) {
 
   //Keep track of the images that are used during dependency resolution so they can be converted to base64 at compile time
   var buildMap = {};
-
-  //This function was taken from chipper/grunt/Gruntfile.js
-  //TODO: this code is an exact copy of the same code in the image! plugin
-  var loadFileAsDataURI = function( filename ) {
-    var mimeType = {
-      'png': 'image/png',
-      'svg': 'image/svg+xml',
-      'jpg': 'image/jpeg',
-      'cur': 'image/x-icon', // cursor files (used in build-a-molecule). x-win-bitmap gives off warnings in Chrome
-      'mp3': 'audio/mpeg',
-      'm4a': 'audio/mp4',
-      'ogg': 'audio/ogg',
-      'oga': 'audio/ogg',
-      'bma': 'audio/webm', // webma is the full extension
-      'wav': 'audio/wav'
-    }[filename.slice( -3 )];
-
-    //TODO: error check the mimeType
-//    assert( mimeType, 'Unknown mime type for filename: ' + filename );
-
-    var base64 = 'data:' + mimeType + ';base64,' + Buffer( fs.readFileSync( filename ) ).toString( 'base64' );
-    return base64;
-  };
 
   return {
     load: function( name, parentRequire, onload, config ) {
---
+++ requirejs-plugins/loadFileAsDataURI.js
@@ -0,0 +1,28 @@
+// Copyright 2002-2013, University of Colorado Boulder
+
+//This function was taken from chipper/grunt/Gruntfile.js.  It is for converting a resource (like an image or audio file) to base64
+define( function() {
+
+  return function( filename ) {
+    var mimeType = {
+      'png': 'image/png',
+      'svg': 'image/svg+xml',
+      'jpg': 'image/jpeg',
+      'cur': 'image/x-icon', // cursor files (used in build-a-molecule). x-win-bitmap gives off warnings in Chrome
+      'mp3': 'audio/mpeg',
+      'm4a': 'audio/mp4',
+      'ogg': 'audio/ogg',
+      'oga': 'audio/ogg',
+      'bma': 'audio/webm', // webma is the full extension
+      'wav': 'audio/wav'
+    }[filename.slice( -3 )];
+
+    //TODO: use asserts at build time
+    if ( !mimeType ) {
+      throw new Error( 'Unknown mime type for filename: ' + filename );
+    }
+
+    var base64 = 'data:' + mimeType + ';base64,' + Buffer( fs.readFileSync( filename ) ).toString( 'base64' );
+    return base64;
+  };
+} );
