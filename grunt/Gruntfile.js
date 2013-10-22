23be4e6b2c66a0d1d04addfc09c87bf54ab4269f
Jonathan Olson <olsonsjc@gmail.com>
2013-10-21 18:11:24 -0600

    Adding notes to see https://github.com/phetsims/chipper/issues/46

--- grunt/Gruntfile.js
+++ grunt/Gruntfile.js
@@ -513,9 +513,9 @@ module.exports = function( grunt ) {
           //Make the locale accessible at runtime (e.g., for changing layout based on RTL languages), see #40
           localeHTML = stringReplace( localeHTML, 'PHET_LOCALE', 'window.phetLocale=\'' + locale + '\';' );
           
-          // TODO: replace with namespace code once we determine how to get them (like MOLARITY and BAM)
+          // TODO: replace with namespace code once we determine how to get them (like MOLARITY and BAM) see https://github.com/phetsims/chipper/issues/46
           // this is less than ideal, and there's an off-chance it will pick up an incorrect string
-          // TODO: remove this sim-specific workaround!
+          // TODO: remove this sim-specific workaround! see https://github.com/phetsims/chipper/issues/46
           var simOverrides = {
             'concentration': 'tab.concentration',
             'balloons-and-static-electricity': 'balloons.name',
