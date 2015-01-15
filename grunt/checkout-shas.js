b46ab0105697d88ee2e1d5a35f332f11728a8ea4
Chris Malley <cmalley@pixelzoom.com>
2015-01-15 12:30:26 -0700

    #77 clean up checkout-shas.js, tomaster option can be checked once, command string can be less ugly

--- grunt/checkout-shas.js
+++ grunt/checkout-shas.js
@@ -6,7 +6,6 @@ var child_process = require( 'child_process' );
 /**
  * Check out the shas for a project as specified in a dependencies.json file in its top level.
  *
- * TODO: this is untested
  * TODO: should the parent project (the sim itself) also be checked out, ignored, or moved into its specified branch?  Right now it is ignored (i.e. left as it was when the task was started)
  * @param grunt
  * @param projectName
@@ -23,6 +22,9 @@ module.exports = function( grunt, projectName ) {
     }
   }
 
+  //Added an option to checkout master branch if you specify --tomaster=true
+  var toMaster = !!grunt.option( "tomaster" ) && grunt.option( "tomaster" ) === true;
+
   for ( property in dependencies ) {
     if ( property !== 'comment' && property !== projectName && dependencies.hasOwnProperty( property ) ) {
 
@@ -35,13 +37,7 @@ module.exports = function( grunt, projectName ) {
         //To execute something from a different directory:
         //cp.exec('foocommand', { cwd: 'path/to/dir/' }, callback);
         //http://stackoverflow.com/questions/14026967/calling-child-process-exec-in-node-as-though-it-was-executed-in-a-specific-folde
-
-        //Added an option to checkout master branch if you specify --tomaster=true
-        var toMaster = grunt.option( "tomaster" ) && grunt.option( "tomaster" ) === true;
-        var command = !toMaster ?
-                      'git checkout ' + dependencies[property].sha :
-                      'git checkout master';
-
+        var command = 'git checkout ' + ( toMaster ? 'master' : dependencies[property].sha );
         child_process.exec( command, {cwd: '../' + property}, function( error1, stdout1, stderr1 ) {
           assert( !error1, "error in " + command );
           console.log( 'Finished checkout.' );
