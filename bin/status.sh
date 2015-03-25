35853061189e9e6032b3f13433e5171c5ed1ffe6
Jonathan Olson <olsonsjc@gmail.com>
2015-03-25 15:41:17 -0600

    Improving status.sh output

--- bin/status.sh
+++ bin/status.sh
@@ -9,13 +9,52 @@
 #
 #====================================================================================================
 
+# ANSI escape sequences to move to the right (in the same line) or to apply or reset colors
+MOVE_RIGHT="\E[36G"
+RED="\E[31m"
+GREEN="\E[32m"
+YELLOW="\E[33m"
+RESET="\E[0m"
+
 for dir in *
 do
+  # ignore non-directory files
   if [ -d "${dir}" ]; then
     cd $dir
-    echo "${dir}"
-    git status | grep -v nothing | sed -e 's/# On branch master/                                    \x1BM\x1B\[32mmaster\x1B\[0m/' \
-                                       -e 's/# On branch \(.*\)$/                                    \x1BM\x1B\[31m\1\x1B\[0m/'
+    # ignore directory if it's not a git repo base
+    if [ -d .git ]; then
+      echo -n "${dir}" # -n for no newline
+
+      # current branch name OR an empty string (if detached head)
+      BRANCH=`git symbolic-ref -q HEAD | sed -e 's/refs\/heads\///'`
+
+      # current SHA
+      SHA=`git rev-parse HEAD`
+
+      # status (empty string if clean)
+      STATUS=`git status --porcelain`
+
+      # if no branch, print our SHA (detached head)
+      if [ -z "$BRANCH" ]; then
+        echo -e "${MOVE_RIGHT}${RED}${SHA}${RESET}"
+      else
+        # color branch name based on branch and status. RED for non-master, otherwise GREEN/YELLOW depending on status
+        if [ "$BRANCH" = "master" ]; then
+          if [ -z "$STATUS" ]; then
+            echo -e "${MOVE_RIGHT}${GREEN}master${RESET}"
+          else
+            echo -e "${MOVE_RIGHT}${YELLOW}master${RESET}"
+          fi
+        else
+          echo -e "${MOVE_RIGHT}${RED}${BRANCH}${RESET}"
+        fi
+      fi
+
+      # print status, if any
+      if [ ! -z "$STATUS" ]; then
+        echo "$STATUS"
+      fi
+    fi
     cd ..
   fi
 done
