0da445698777d85eb1abc797a1e4d377a2e67fca
Chris Malley <cmalley@pixelzoom.com>
2015-04-09 11:16:28 -0600

    remove extra space from copyright comment on line 1 of .js files

--- js/grunt/createSim.js
+++ js/grunt/createSim.js
@@ -1,4 +1,4 @@
-//  Copyright 2002-2014, University of Colorado Boulder
+// Copyright 2002-2014, University of Colorado Boulder
 
 /**
  * This grunt task creates a simulation based on the simula-rasa template.
--- js/requirejs-plugins/createMipmap.js
+++ js/requirejs-plugins/createMipmap.js
@@ -1,4 +1,4 @@
-//  Copyright 2002-2015, University of Colorado Boulder
+// Copyright 2002-2015, University of Colorado Boulder
 
 var fs = require( 'fs' );
 var jpeg = require( 'jpeg-js' );
