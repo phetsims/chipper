bc447ad0ee9bfda9e182c610b13e235b24a43508
Jonathan Olson <olsonsjc@gmail.com>
2015-04-23 13:53:37 -0600

    Windows test for print-missing-repos.sh

--- bin/print-missing-repos.sh
+++ bin/print-missing-repos.sh
@@ -11,5 +11,14 @@
 
 CHIPPER_BIN=`dirname "${BASH_SOURCE[0]}"`
 WORKING_DIR=${CHIPPER_BIN}/../..
-cd ${WORKING_DIR} ; comm -23 <(sort -u ./chipper/data/active-repos) <(/bin/ls -1 .)
+cd ${WORKING_DIR}
 
+# This used to be the simple line, but Git bash on windows doesn't include comm
+# comm -23 <(sort -u ./chipper/data/active-repos) <(/bin/ls -1 .)
+
+# A list of repo names that is either in active-repos OR is checked out (but not both).
+# uniq -u only includes non-duplicate lines
+ITEMS_IN_ONE_LIST=`cat <(cat ./chipper/data/active-repos) <(ls -1 .) | sort | uniq -u`
+
+# uniq -d only includes duplicate lines
+cat <(cat ./chipper/data/active-repos) <(echo "$ITEMS_IN_ONE_LIST") | sort | uniq -d
