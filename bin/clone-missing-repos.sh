8f26a66968a2d653cecf6839efdd523e4a2a3182
Chris Malley <cmalley@pixelzoom.com>
2015-04-27 11:38:52 -0600

    remove change for chipper, we're doing a cd to working dir

--- bin/clone-missing-repos.sh
+++ bin/clone-missing-repos.sh
@@ -12,13 +12,8 @@ CHIPPER_BIN=`dirname "${BASH_SOURCE[0]}"`
 WORKING_DIR=${CHIPPER_BIN}/../..
 cd ${WORKING_DIR}
 
-if [ -d ./chipper ];
-then
-  MISSING_REPOS=`./chipper/bin/print-missing-repos.sh`
-  for repo in `echo "${MISSING_REPOS}" | xargs`
-  do
-    git clone https://github.com/phetsims/"$repo".git
-  done
-else
-  echo "I don't see chipper. Are you running this script in your working directory?"
-fi
+MISSING_REPOS=`./chipper/bin/print-missing-repos.sh`
+for repo in `echo "${MISSING_REPOS}" | xargs`
+do
+  git clone https://github.com/phetsims/"$repo".git
+done
