e31560c7f4ee471f60030ab54fd6e0f9493e1858
Chris Malley <cmalley@pixelzoom.com>
2015-04-28 10:16:30 -0600

    #138 print a warning for missing repos, pass all command-line args to grunt

--- bin/build-all.sh
+++ bin/build-all.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 #====================================================================================================
 #
-# Builds all chipper-like simulations.
+# Runs grunt for all simulations.
+# Command-line args are passed through to grunt.
+# With no args, this runs the default grunt task (build).
 #
 # Author: Jonathan Olson
+# Author: Chris Malley
 #
 #====================================================================================================
 
@@ -17,7 +20,9 @@ do
     echo "Building" $sim
     cd $sim                 # build.sh needs to be run from the sim directory
     npm install             # executes quickly when everything is up to date compared to build.sh
-    grunt $1 # run the build
+    grunt $*                # run grunt with command-line args
     cd ..                   # and back to the original directory
+  else
+    echo ">>>>>>>>>>>>>>>> MISSING " $sim
   fi
 done
