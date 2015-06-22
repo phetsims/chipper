82f72406dc6ab3f034024487d08ca629ff7fcc44
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2015-06-22 11:37:41 -0600

    change dev deploy dir to ad-tests for build server test

--- js/grunt/deployDev.js
+++ js/grunt/deployDev.js
@@ -12,7 +12,7 @@
 var client = require( 'scp2' );
 
 // constants
-var DEV_DIRECTORY = '/htdocs/physics/phet/dev/html/';
+var DEV_DIRECTORY = '/htdocs/physics/phet/dev/html/ad-tests/';
 var HTACCESS_TEXT = 'IndexOrderDefault Descending Date\n';
 
 
