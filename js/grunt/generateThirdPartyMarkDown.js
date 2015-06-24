c907d0090ca2d6eeade862e0bf48c73ee20e5a72
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2015-06-24 09:16:12 -0600

    add commit and push to generate-markdown task, see https://github.com/phetsims/sherpa/issues/22

--- js/grunt/generateThirdPartyMarkDown.js
+++ js/grunt/generateThirdPartyMarkDown.js
@@ -6,19 +6,27 @@
  * @author Aaron Davis
  */
 
+// modules
+var child_process = require( 'child_process' );
+var assert = require( 'assert' );
+
+// constants
+var OUTPUT_FILE = 'info.md';
+var SHERPA = '../sherpa';
+
 /**
  * @param grunt the grunt instance
  */
 module.exports = function( grunt ) {
   'use strict';
 
-  var json = grunt.file.readJSON( '../sherpa/third-party-licenses.json' );
+  var json = grunt.file.readJSON( SHERPA + '/third-party-licenses.json' );
   var i;
 
-  var columnHeaders = [ 'Library', 'Website', 'License', 'License URL', 'Text', 'Notes' ];
+  var columnHeaders = [ 'Library', 'Website', 'License', 'License URL (if different than project website)', 'Text', 'How is it used in sims?' ];
   var headerDelimiters = [];
   for ( i = 0; i < columnHeaders.length; i++ ) {
-    headerDelimiters.push( '---------' );
+    headerDelimiters.push( '-----' );
   }
   var output = columnHeaders.join( '|' ) + '\n' + headerDelimiters.join( '|' );
 
@@ -38,6 +46,27 @@ module.exports = function( grunt ) {
     output += '\n' + row.join( '|' );
   }
 
-  grunt.log.writeln( output );
+  grunt.log.writeln( 'writing file ' + OUTPUT_FILE );
+  grunt.file.write( SHERPA + '/' + OUTPUT_FILE, output );
+
+  var done = grunt.task.current.async();
+
+  // exec a command in the sherpa directory
+  var exec = function( command, callback ) {
+    child_process.exec( command, { cwd: SHERPA }, function( err, stdout, stderr ) {
+      grunt.log.writeln( stdout );
+      grunt.log.writeln( stderr );
+      assert( err, 'assertion error running ' + command );
+      callback();
+    } );
+  };
+
+  exec( 'git add ' + OUTPUT_FILE, function() {
+    exec( 'git commit --message "updated info.md"', function() {
+      exec( 'git push', function() {
+        done();
+      } );
+    } );
+  } );
 
 };
