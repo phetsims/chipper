b13822181e0fd708b1617b6cee04cdd4c5e38b11
Sam Reid <reids@colorado.edu>
2015-07-28 10:30:29 -0600

    Created initial draft of composite report code, see #162

--- js/grunt/createCompositeThirdPartyReport.js
+++ js/grunt/createCompositeThirdPartyReport.js
@@ -4,6 +4,17 @@
  *
  * @author Sam Reid
  */
+
+// modules
+var child_process = require( 'child_process' );
+var assert = require( 'assert' );
+var fs = require( 'fs' );
+
+// constants
+var SHERPA = '../sherpa';  // The relative path to sherpa, from the chipper path
+var OUTPUT_FILE = 'third-party-licenses.md';
+var LICENSES_DIRECTORY = '../sherpa/licenses/'; // contains third-party licenses themselves.
+
 module.exports = function( grunt ) {
   'use strict';
 
@@ -28,7 +39,8 @@ module.exports = function( grunt ) {
   // TODO: Should there be a sim report as well as a non-sim report?
   // TODO: Add other 3rd party things from sherpa that are used in non-requirejs projects.
   // TODO: Should we parse the HTML and get the values from the HTML so that we can (a) run reports for published sims
-  // TODO:  instead of just master, and won't have to generate additional report files after the build? 
+  // TODO:  instead of just master, and won't have to generate additional report files after the build?
+  // TODO: At a minimum, let us fail the build if we didn't see a report for every active-sim
 
   // Iterate over all images and audio directories recursively
   grunt.file.recurse( rootdir, function( abspath, rootdir, subdir, filename ) {
@@ -78,4 +90,124 @@ module.exports = function( grunt ) {
   grunt.log.writeln( JSON.stringify( compositeCode, null, 2 ) );
   grunt.log.writeln( JSON.stringify( compositeImagesAndAudio, null, 2 ) );
   grunt.log.writeln( 'report completed for\n' + repositoryNames.join( ', ' ) );
+
+  // Convert everything to MD, see updateThirdPartyLicensesMD
+  var json = grunt.file.readJSON( SHERPA + '/lib/license.json' );
+
+  var entries = [];
+  var licensesUsed = [];
+
+  // Get a list of the library names
+  var libraries = [];
+  for ( var lib in json ) {
+    libraries.push( lib );
+  }
+
+  // Use a case insensitive sort, see http://stackoverflow.com/questions/8996963/how-to-perform-case-insensitive-sorting-in-javascript
+  libraries.sort( function( a, b ) {
+    return a.toLowerCase().localeCompare( b.toLowerCase() );
+  } );
+
+  // Add info for each library to the MD report
+  for ( var i = 0; i < libraries.length; i++ ) {
+    var library = libraries[ i ];
+
+    // check for existence of the license file
+    if ( !fs.existsSync( LICENSES_DIRECTORY + library + '.txt' ) ) {
+      grunt.log.error( 'license file not found for ' + library );
+    }
+
+    var lines = [
+      '**' + library + '**',
+      json[ library ].text.join( '<br>' ),
+      json[ library ].projectURL,
+      'License: [' + json[ library ].license + '](licenses/' + library + '.txt)',
+      'Notes: ' + json[ library ].notes
+    ];
+
+    if ( json[ library ].dependencies ) {
+      lines.push( 'Dependencies: **' + json[ library ].dependencies + '**' );
+    }
+
+    if ( compositeCode.hasOwnProperty( library ) && compositeCode[ library ].usedBy instanceof Array ) {
+      lines.push( 'Used by: ' + compositeCode[ library ].usedBy.join( ', ' ) );
+    }
+
+    // \n worked well when viewing GitHub markdown as an issue comment, but for unknown reasons <br> is necessary when 
+    // viewing from https://github.com/phetsims/sherpa/blob/master/third-party-licenses.md
+    entries.push( lines.join( '<br>' ) );
+
+    if ( licensesUsed.indexOf( json[ library ].license ) < 0 ) {
+      licensesUsed.push( json[ library ].license );
+    }
+  }
+
+  var imagesAndAudioOutput = [];
+  var imageAudioKeys = [];
+  for ( var imageAudioEntry in compositeImagesAndAudio ) {
+    if ( compositeImagesAndAudio.hasOwnProperty( imageAudioEntry ) ) {
+      imageAudioKeys.push( imageAudioEntry );
+    }
+  }
+  // Use a case insensitive sort, see http://stackoverflow.com/questions/8996963/how-to-perform-case-insensitive-sorting-in-javascript
+  imageAudioKeys.sort( function( a, b ) {
+    return a.toLowerCase().localeCompare( b.toLowerCase() );
+  } );
+  for ( i = 0; i < imageAudioKeys.length; i++ ) {
+    var imageAndAudioKey = imageAudioKeys[ i ];
+    if ( compositeImagesAndAudio[ imageAndAudioKey ].notes !== 'from Snow Day Math' ) {
+      var imageAudioEntryLines = [
+        '**' + imageAndAudioKey + '**',
+        compositeImagesAndAudio[ imageAndAudioKey ].text.join( '<br>' ),
+        compositeImagesAndAudio[ imageAndAudioKey ].projectURL,
+        'License: ' + compositeImagesAndAudio[ imageAndAudioKey ].license,
+        'Notes: ' + compositeImagesAndAudio[ imageAndAudioKey ].notes,
+        'Used by: ' + 'TODO'
+      ];
+      imagesAndAudioOutput.push( imageAudioEntryLines.join( '<br>' ) );
+    }
+  }
+
+  // Summarize licenses used
+  var output = '**Third-party Code:**<br>\n' +
+               entries.join( '\n\n' ) + '\n\n' +
+
+               '---\n' +
+
+               '**Third-party Code License Summary:**<br>\n' +
+               licensesUsed.join( '<br>' ) + '\n\n' +
+
+               '---\n' +
+
+               '**Third-party Images & Audio:**<br>\n' +
+               imagesAndAudioOutput.join( '\n\n' ) + '\n';
+
+  // It is sometimes convenient to iterate using GitHub issue preview rather than committing every time.
+  // In this case, you may want to comment out the commit below.
+  grunt.log.debug( '!!!!!! BEGIN LICENSES OUTPUT' );
+  grunt.log.debug( output );
+  grunt.log.debug( '!!!!!! END LICENSES OUTPUT' );
+
+  grunt.log.writeln( 'writing file ' + OUTPUT_FILE );
+  grunt.file.write( SHERPA + '/' + OUTPUT_FILE, output );
+
+  var done = grunt.task.current.async();
+
+  // exec a command in the sherpa directory
+  var exec = function( command, callback ) {
+    child_process.exec( command, { cwd: SHERPA }, function( err, stdout, stderr ) {
+      grunt.log.writeln( stdout );
+      grunt.log.writeln( stderr );
+      assert( !err, 'assertion error running ' + command );
+      callback();
+    } );
+  };
+
+  exec( 'git add ' + OUTPUT_FILE, function() {
+    exec( 'git commit --message "updated info.md"', function() {
+      exec( 'git push', function() {
+        done();
+      } );
+    } );
+  } );
 };
