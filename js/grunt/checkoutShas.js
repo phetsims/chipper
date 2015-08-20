c6b40c1bd425e2c07b77e1f3ac6b235cbe7c3ee7
Chris Malley <cmalley@pixelzoom.com>
2015-08-20 13:55:00 -0600

    #299 document that --buildServer is reserved for use by build-server

--- js/grunt/checkoutShas.js
+++ js/grunt/checkoutShas.js
@@ -15,7 +15,8 @@ var child_process = require( 'child_process' );
 module.exports = function( grunt, repositoryName, toMaster ) {
   'use strict';
 
-  var buildServer = !!grunt.option( 'buildServer' ); // whether this build is initiated by the build server
+  // This option should be used only by build-server, to indicate that the task was invoked from build-server.
+  var buildServer = !!grunt.option( 'buildServer' );
 
   var dependencies = grunt.file.readJSON( ( buildServer ) ? '../chipper/js/build-server/tmp/dependencies.json' : 'dependencies.json' );
   var done = grunt.task.current.async();
