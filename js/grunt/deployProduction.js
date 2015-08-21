cf69ec5e5bd65ec2f22aafa889cb9e8b3f184a25
Chris Malley <cmalley@pixelzoom.com>
2015-08-21 12:34:55 -0600

    deployProduction.js: revise use of grunt.file (#285), use pkg.name (#303)

--- js/grunt/deployProduction.js
+++ js/grunt/deployProduction.js
@@ -13,14 +13,13 @@
 // modules
 var querystring = require( 'querystring' );
 var request = require( 'request' );
-var fs = require( 'fs' );
 var assert = require( 'assert' );
 var ChipperConstants = require( '../../../chipper/js/common/ChipperConstants' );
 
 // constants
-var PREFERENCES_FILE = process.env.HOME + '/.phet/build-local.json';
 var PACKAGE_JSON = 'package.json';
 var DEPENDENCIES_JSON = 'build/dependencies.json';
+var PREFERENCES_FILE = process.env.HOME + '/.phet/build-local.json';
 var DEFAULT_PRODUCTION_SERVER_NAME = 'figaro.colorado.edu';
 var DEFAULT_PRODUCTION_SERVER_URL = 'https://phet.colorado.edu';
 
@@ -29,34 +28,28 @@ var DEFAULT_PRODUCTION_SERVER_URL = 'https://phet.colorado.edu';
  */
 module.exports = function( grunt ) {
 
-  assert( fs.existsSync( PREFERENCES_FILE ), 'missing preferences file ' + PREFERENCES_FILE );
+  // read package.json (required)
+  var packageJSON = grunt.file.readJSON( PACKAGE_JSON );
+  assert( packageJSON.name, 'missing name from ' + PACKAGE_JSON );
+  assert( packageJSON.version, 'missing name from ' + PACKAGE_JSON );
+
+  // read dependencies.json (required)
+  var dependencies = grunt.file.readJSON( DEPENDENCIES_JSON );
+
+  // read build-locale.json (required)
   var preferences = grunt.file.readJSON( PREFERENCES_FILE );
   assert( preferences.buildServerAuthorizationCode, 'buildServerAuthorizationCode is missing from ' + PREFERENCES_FILE );
-
-  var productionServerName = preferences.productionServerName || DEFAULT_PRODUCTION_SERVER_NAME;
-  var productionServerURL = preferences.productionServerURL || DEFAULT_PRODUCTION_SERVER_URL;
-
-  // get the sim name from the current directory
-  var directory = process.cwd();
-  var directoryComponents = directory.split( ( /^win/.test( process.platform ) ) ? '\\' : '/' );
-  var sim = directoryComponents[ directoryComponents.length - 1 ];
-
-  // check prerequisite files
-  assert( grunt.file.exists( PACKAGE_JSON ), 'Cannot find ' + PACKAGE_JSON );
-  assert( grunt.file.exists( DEPENDENCIES_JSON ), 'Cannot find ' + DEPENDENCIES_JSON );
-
-  var dependencies = grunt.file.readJSON( DEPENDENCIES_JSON );
-  var version = grunt.file.readJSON( PACKAGE_JSON ).version;
 
   var query = querystring.stringify( {
     'repos': JSON.stringify( dependencies ),
     'locales': JSON.stringify( [ ChipperConstants.FALLBACK_LOCALE ] ),
-    'simName': sim,
-    'version': version,
-    'serverName': productionServerName,
+    'simName': packageJSON.name,
+    'version': packageJSON.version,
+    'serverName': preferences.productionServerName || DEFAULT_PRODUCTION_SERVER_NAME,
     'authorizationCode': preferences.buildServerAuthorizationCode
   } );
 
+  var productionServerURL = preferences.productionServerURL || DEFAULT_PRODUCTION_SERVER_URL;
   var url = productionServerURL + '/deploy-html-simulation?' + query;
 
   var done = grunt.task.current.async();
