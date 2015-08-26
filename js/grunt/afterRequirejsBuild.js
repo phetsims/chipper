2c1828302926e0a2b34c5d1d9387ef720e57ed88
Chris Malley <cmalley@pixelzoom.com>
2015-08-26 14:47:33 -0600

    #292 revise doc for afterRequirejsBuild.js

--- js/grunt/afterRequirejsBuild.js
+++ js/grunt/afterRequirejsBuild.js
@@ -1,30 +1,8 @@
 // Copyright 2002-2015, University of Colorado Boulder
 
 /**
- * This grunt task does things after the requirejs:build step.
+ * This (asynchronous) grunt task does things after the requirejs:build step.
  * It is for internal use only, not intended to be called directly.
- *
- * Due to the asynchronous nature of determining shas and branch names for dependencies (via child_process.exec)
- * and creating scripts for mipmaps, the overall structure of this task is necessarily nested, relying on callbacks.
- *
- * Here's the basic flow of this task:
- *
- * createDependenciesJSON() {
- *
- *   var dependenciesJSON = ... // create JSON that describes dependencies
- *   write dependencies.json file;
- *
- *   createMipmaps( dependenciesJSON ) {
- *
- *     var mipmapsJavascript = ... // create script for mipmaps
- *
- *     createHTMLFiles( dependenciesJSON, mipmapsJavascript ) {
- *       replace tokens in HTML template;
- *       write locale-specific HMTL files;
- *       done;
- *     }
- *   }
- * }
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
@@ -37,7 +15,7 @@ var reportUnusedMedia = require( '../../../chipper/js/grunt/reportUnusedMedia' )
 
 /**
  * @param grunt - the grunt instance
- * @param {Object} buildConfig - see initBuildConfig.js
+ * @param {Object} buildConfig - see getBuildConfig.js
  */
 module.exports = function( grunt, buildConfig ) {
   'use strict';
@@ -49,8 +27,9 @@ module.exports = function( grunt, buildConfig ) {
   // After all media plugins have completed (which happens in requirejs:build), report which media files in the repository are unused.
   reportUnusedMedia( grunt, buildConfig.requirejsNamespace );
 
-  // The multi-step asynchronous task described above.
-  // For each step, a callback function invokes the next step.
+  // Since this is an asynchronous task, each step in the task uses a callback to advance to the next step,
+  // and propagate information to the subsequent step(s). The final step in the task calls 'done', to tell
+  // grunt that the task has completed.
   createDependenciesJSON( grunt, buildConfig, function( dependenciesJSON ) {
     createMipmapsJavaScript( grunt, buildConfig, dependenciesJSON, function( dependenciesJSON, mipmapsJavaScript ) {
       createHTMLFiles( grunt, buildConfig, dependenciesJSON, mipmapsJavaScript, done );
