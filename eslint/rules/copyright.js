8aae7309cfaab5ec5d5b28f836d846cb6937f956
Sam Reid <reids@colorado.edu>
2015-10-06 14:34:15 -0600

    Fixed the case where there are no comments, see #235

--- eslint/rules/copyright.js
+++ eslint/rules/copyright.js
@@ -18,17 +18,27 @@ module.exports = function( context ) {
       // Get the whole source code, not for node only.
       var comments = context.getSourceCode().getAllComments();
 
-      // sample comment:
-      //{"type":"Line","value":" Copyright 2002-2015, University of Colorado Boulder","range":[0,54],"loc":{"start":{"line":1,"column":0},"end":{"line":1,"column":54}}}
-      if ( comments[ 0 ].value !== ' Copyright 2002-2015, University of Colorado Boulder' &&
-           comments[ 0 ].value !== ' Copyright 2002-2014, University of Colorado Boulder' &&
-           comments[ 0 ].value !== ' Copyright 2002-2013, University of Colorado Boulder' ) {
-
+      if ( !comments || comments.length === 0 ) {
         context.report( {
           node: node,
-          loc: comments[ 0 ].loc.start,
+          loc: 1,
           message: 'Incorrect copyright statement in first comment'
         } );
+      }
+      else {
+
+        // sample comment:
+        //{"type":"Line","value":" Copyright 2002-2015, University of Colorado Boulder","range":[0,54],"loc":{"start":{"line":1,"column":0},"end":{"line":1,"column":54}}}
+        if ( comments[ 0 ].value !== ' Copyright 2002-2015, University of Colorado Boulder' &&
+             comments[ 0 ].value !== ' Copyright 2002-2014, University of Colorado Boulder' &&
+             comments[ 0 ].value !== ' Copyright 2002-2013, University of Colorado Boulder' ) {
+
+          context.report( {
+            node: node,
+            loc: comments[ 0 ].loc.start,
+            message: 'Incorrect copyright statement in first comment'
+          } );
+        }
       }
     }
   };
