e2b7d2448122df677549656a788e4cd4aa0915cb
Chris Malley <cmalley@pixelzoom.com>
2015-10-13 14:18:32 -0600

    #387 change output format: abbreviated sha, commit date, comitter name, commit message, '|' field separator

--- js/grunt/commitsSince.js
+++ js/grunt/commitsSince.js
@@ -47,7 +47,7 @@ module.exports = function( grunt, buildConfig ) {
 
       // 'git log' command
       child_process.exec(
-        'git --git-dir ../' + repository + '/.git log --since="' + dateString + '" --pretty=oneline',
+        'git --git-dir ../' + repository + '/.git log --since="' + dateString + '" --pretty=format:"%h | %cI | %cn | %s"',
         function( error, stdout, stderr ) {
           assert( !error, 'ERROR on git log attempt: ' + stderr );
           output += stdout;
