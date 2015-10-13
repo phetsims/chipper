6d1e713c5722824906e5a440292a66b86cdab446
Chris Malley <cmalley@pixelzoom.com>
2015-10-13 14:02:06 -0600

    #387 doc

--- js/grunt/commitsSince.js
+++ js/grunt/commitsSince.js
@@ -1,5 +1,18 @@
 // Copyright 2002-2015, University of Colorado Boulder
 
+/**
+ * Prints commits since a specified date, for all dependencies of the build target.
+ * The output is grouped by repository, and condensed to one line per commit.
+ *
+ * For example, to see all commits since Oct 1, 2015 at 3:52pm:
+ * grunt commits-since --date="2015-10-01 15:52"
+ *
+ * To count the number of commits, use the power of the shell:
+ * grunt commits-since --date="2015-10-01 15:52" | grep -v since | wc -l
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+
 // built-in node APIs
 var assert = require( 'assert' );
 var child_process = require( 'child_process' );
