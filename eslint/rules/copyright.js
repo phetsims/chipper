673b930fc943fc357c31c27a407e6f9a9172f4b4
Chris Malley <cmalley@pixelzoom.com>
2015-10-27 15:09:46 -0600

    #394 add special chars for begin/end of input

--- eslint/rules/copyright.js
+++ eslint/rules/copyright.js
@@ -27,7 +27,7 @@ module.exports = function( context ) {
       }
       else {
         // second year must be between 2010 and 2099, inclusive
-        if ( ! / Copyright 2002-20[1-9]\d, University of Colorado Boulder/.test( comments[ 0 ].value ) ) {
+        if ( ! /^ Copyright 2002-20[1-9]\d, University of Colorado Boulder$/.test( comments[ 0 ].value ) ) {
           context.report( {
             node: node,
             loc: comments[ 0 ].loc.start,
