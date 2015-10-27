151b028226c15f26e4cd7fa6ca59d4ea67f369e2
samreid <reids@colorado.edu>
2015-10-27 13:08:44 -0600

    Fixed link to sim iframe client, see https://github.com/phetsims/together/issues/205

--- js/grunt/createTogetherFiles.js
+++ js/grunt/createTogetherFiles.js
@@ -5,6 +5,7 @@
  */
 
 // modules
+var ChipperStringUtils = require( '../../../chipper/js/common/ChipperStringUtils' );
 
 /**
  * @param grunt - the grunt instance
@@ -16,7 +17,7 @@ module.exports = function( grunt, buildConfig ) {
 // TODO: chipper#101 eek, this is scary! we are importing from the repository dir. ideally we should just have uglify-js installed once in chipper?
   var uglify = require( '../../../' + buildConfig.name + '/node_modules/uglify-js' );
 
-  var minified = uglify.minify( [ '../together/js/SimIFrameAPI.js' ], {
+  var minified = uglify.minify( [ '../together/js/SimIFrameClient.js' ], {
     mangle: true,
     output: {
       inline_script: true, // escape </script
@@ -30,4 +31,18 @@ module.exports = function( grunt, buildConfig ) {
   var copyrightHeader = '// Copyright 2002-2015, University of Colorado Boulder\n' +
                         '// For licensing, please contact phethelp@colorado.edu';
   grunt.file.write( 'build/SimIFrameClient.js', copyrightHeader + '\n' + minified );
+
+  // Create a mirror-input-events.html file for testing.
+  var exampleFileText = grunt.file.read( '../together/examples/mirror-input-events.html' );
+  exampleFileText = ChipperStringUtils.replaceAll(
+    exampleFileText,
+    '// This URL is replaced with a built html file when this file is used as a template during the build process',
+    'URL = \'' + buildConfig.name + '_en.html\';'
+  );
+  exampleFileText = ChipperStringUtils.replaceAll(
+    exampleFileText,
+    '../js/SimIFrameClient.js',
+    'SimIFrameClient.js'
+  );
+  grunt.file.write( 'build/mirror-input-events.html', exampleFileText );
 };
