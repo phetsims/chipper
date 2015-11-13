a150c3e09fffe0b6dee906cfab055220e23a55e6
samreid <reids@colorado.edu>
2015-11-13 00:14:11 -0700

    Fixed templates for createTogetherFile

--- js/grunt/createTogetherFiles.js
+++ js/grunt/createTogetherFiles.js
@@ -30,30 +30,16 @@ module.exports = function( grunt, buildConfig ) {
 
   var copyrightHeader = '// Copyright 2002-2015, University of Colorado Boulder\n' +
                         '// For licensing, please contact phethelp@colorado.edu';
-  grunt.file.write( 'build/SimIFrameClient.js', copyrightHeader + '\n' + minified );
+  grunt.file.write( 'build/js/SimIFrameClient.js', copyrightHeader + '\n' + minified );
 
   // Create a mirror-input-events.html file for testing.
 
-  var filter = function( inputFilename, outputFilename ) {
-
-    // Default to the input filename if not specified
-    outputFilename = outputFilename || inputFilename;
-
-    var text = grunt.file.read( '../together/examples/' + inputFilename );
-    text = ChipperStringUtils.replaceAll( text,
-      '../js/SimIFrameClient.js',
-      'SimIFrameClient.js'
-    );
+  var filter = function( text ) {
 
     text = ChipperStringUtils.replaceAll(
       text,
-      '// $TEMPLATE$ This URL is replaced with a built html file when this file is used as a template during the build process',
-      'URL = \'' + buildConfig.name + '_en.html\';'
-    );
-    text = ChipperStringUtils.replaceAll(
-      text,
-      '../js/SimIFrameClient.js',
-      'SimIFrameClient.js'
+      '../../js/SimIFrameClient.js',
+      '../js/SimIFrameClient.js'
     );
     var camelCase = ChipperStringUtils.toCamelCase( buildConfig.name );
 
@@ -65,6 +51,11 @@ module.exports = function( grunt, buildConfig ) {
     );
     text = ChipperStringUtils.replaceAll(
       text,
+      '\'concentration.sim.active\'',
+      '\'' + camelCase + '.sim.active\''
+    );
+    text = ChipperStringUtils.replaceAll(
+      text,
       '\'faradaysLaw.sim',
       '\'' + camelCase + '.sim'
     );
@@ -73,13 +64,85 @@ module.exports = function( grunt, buildConfig ) {
       'src="../html/color-vision-together.html?brand=phet-io&togetherEvents.storeInitialMessages"',
       'src="' + buildConfig.name + '_en.html?togetherEvents.storeInitialMessages"'
     );
-    grunt.file.write( 'build/' + outputFilename, text );
-  };
-  filter( 'mirror-input-events.html' );
-  filter( 'index.html', 'together-index.html' );
-  filter( 'event-log.html' );
-  filter( 'state.html' );
 
-  // Include the logo
-  grunt.file.copy( '../together/examples/logo-on-white.png', 'build/logo-on-white.png' );
+    // Use a ?build so that use cases that require further query parameters don't have to distinguish
+    // between ? and &
+    text = ChipperStringUtils.replaceAll(
+      text,
+      '\'../../html/color-vision-together.html?brand=phet-io\'',
+      '\'../' + buildConfig.name + '_en.html?build\''
+    );
+    text = ChipperStringUtils.replaceAll(
+      text,
+      '\'../../html/faradays-law-together.html?brand=phet-io\'',
+      '\'../' + buildConfig.name + '_en.html?build\''
+    );
+    text = ChipperStringUtils.replaceAll(
+      text,
+      '\'../../html/concentration-together.html?brand=phet-io\'',
+      '\'../' + buildConfig.name + '_en.html?build\''
+    );
+    //
+    if ( camelCase === 'beaker' ) {
+      text = ChipperStringUtils.replaceAll(
+        text,
+        '\'../../html/beaker-together.html?brand=phet-io&together.values=',
+        '\'../' + buildConfig.name + '_en.html?build\''
+      );
+    }
+    else {
+      text = ChipperStringUtils.replaceAll(
+        text,
+        '<!--if camelcase===beaker-->',
+        '<!--if camelcase===beaker' // don't end this comment, so the next comment will end
+      );
+    }
+    return text;
+  };
+
+  var destinationPath = 'build/';
+
+  // Iterate over the file system and copy files, changing filenames and contents as we go.
+  grunt.file.recurse( '../together/doc/', function( abspath, rootdir, subdir, filename ) {
+      // skip these files
+      if ( abspath.indexOf( '../simula-rasa/README.md' ) === 0 ||
+           abspath.indexOf( '../simula-rasa/node_modules/' ) === 0 ||
+           abspath.indexOf( '../simula-rasa/.git/' ) === 0 ||
+           abspath.indexOf( '../simula-rasa/build/' ) === 0 ) {
+
+        // do nothing
+      }
+      else {
+        var contentsPath = subdir ? ( destinationPath + '/' + subdir + '/' + filename ) : ( destinationPath + '/' + filename );
+        if ( abspath.indexOf( '.png' ) >= 0 ) {
+          grunt.file.copy( abspath, contentsPath );
+        }
+        else {
+          var contents = grunt.file.read( abspath );
+          contents = filter( contents );
+
+          // Replace variations of the repository name
+          //contents = ChipperStringUtils.replaceAll( contents, 'simula-rasa', repositoryName );
+          //contents = ChipperStringUtils.replaceAll( contents, 'SIMULA_RASA', configPath );
+          //contents = ChipperStringUtils.replaceAll( contents, 'simulaRasa', lowerCamelCase );
+          //contents = ChipperStringUtils.replaceAll( contents, 'SimulaRasa', upperCamelCase );
+          //contents = ChipperStringUtils.replaceAll( contents, 'Simula Rasa', title );
+          //
+          //// Replace author
+          //contents = ChipperStringUtils.replaceAll( contents, 'Your Name (Your Affiliation)', author );
+          //
+          //// Replace names in the path where the contents will be written
+
+          //contentsPath = ChipperStringUtils.replaceFirst( contentsPath, 'simula-rasa', repositoryName );
+          //contentsPath = ChipperStringUtils.replaceFirst( contentsPath, 'simulaRasa', lowerCamelCase );
+          //contentsPath = ChipperStringUtils.replaceFirst( contentsPath, 'SimulaRasa', upperCamelCase );
+
+          // Write the file
+          grunt.file.write( contentsPath, contents );
+          grunt.log.writeln( 'wrote', contentsPath );
+        }
+
+      }
+    }
+  );
 };
