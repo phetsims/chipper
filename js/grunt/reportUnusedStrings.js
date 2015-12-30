8b7c6412d36a81cf7588ce3f19bd1c820c6e775f
Jesse <jesse.greenberg@colorado.edu>
2015-12-30 11:03:08 -0700

    Print error messages for unused strings, see phetsims/tasks#460

---
+++ js/grunt/reportUnusedStrings.js
@@ -0,0 +1,42 @@
+// Copyright 2015, University of Colorado Boulder
+
+/**
+ * Report which translatable strings from a sim were not used in the simulation with a require statement.
+ * 
+ * Each time a string is loaded by the plugin, it is added to a global list.  After all strings are loaded, 
+ * the global will contain the list of all strings that are actually used by the sim.  Comparing this list to 
+ * the strings in the translatable strings JSON file will identify which strings are unused.
+ * 
+ * See https://github.com/phetsims/tasks/issues/460
+ *
+ * @author Jesse Greenberg
+ */
+
+/**
+ * @param grunt - the grunt instance
+ * @param {object} buildConfig - see getBuildConfig.js
+ */
+module.exports = function( grunt, buildConfig ) {
+  'use strict';
+
+  // get the strings for this sim
+  var simulationRoot = process.cwd();
+  var jsStrings = grunt.file.readJSON( simulationRoot + '/' + buildConfig.name + '-strings_en.json' );
+
+	// iterate over the strings
+	for( var key in jsStrings ) {
+		if( jsStrings.hasOwnProperty( key ) ) {
+
+			var string = jsStrings[ key ].value;
+			var requireStringKey = buildConfig.requirejsNamespace + '/' + key;
+			
+			// global.phet.chipper.strings is initialized by the string plugin
+			var chipperStrings = global.phet.chipper.strings || {};
+
+      // If this string was not added to the global chipperStrings, it was not required in the sim
+      if ( !chipperStrings.hasOwnProperty( requireStringKey ) ) {
+        grunt.log.warn( 'Unused string: ' + requireStringKey + ', ' + string );
+      }
+		}
+	}
+};
