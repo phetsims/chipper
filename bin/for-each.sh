df27d17b2ef91d7725f65f00087f7072d0c655ee
samreid <reids@colorado.edu>
2016-01-27 14:30:29 -0700

    Added commands that iterate over any directory list, see #434

---
+++ bin/for-each.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#====================================================================================================
+#
+# Visits each directory in in a list and applies the same command to each.  The lists are drawn
+# from chipper/bin/data.  For example, to print the working directory of all instrumented simulations,
+# use:
+#
+# for-each.sh instrumented-sims pwd
+#
+# Author: Sam Reid
+# Author: Jonathan Olson (from work in grunt-all.sh)
+# Author: Chris Malley (from work in grunt-all.sh)
+#
+#====================================================================================================
+
+CHIPPER_BIN=`dirname "${BASH_SOURCE[0]}"`
+WORKING_DIR=${CHIPPER_BIN}/../..
+cd ${WORKING_DIR}
+
+array=( "$@" )
+unset "array[$1]"
+
+# Exit immediately on Ctrl-C
+trap "exit 1" SIGINT
+
+for sim in `cat $CHIPPER_BIN/../data/$1 | xargs`
+do
+  if [ -d "$sim" ]; then
+    echo $sim
+    cd $sim                 # build.sh needs to be run from the sim directory
+    ${@:2}                  # run command with remaining command-line args, skip the file list filename
+    cd ..                   # and back to the original directory
+  else
+    echo ">>>>>>>>>>>>>>>> MISSING " $sim
+  fi
+done
