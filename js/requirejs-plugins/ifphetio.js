fe17f34242781b4a6e36753c14d46bbea5d04e20
samreid <reids@colorado.edu>
2016-05-17 11:38:50 -0600

    Removed debug output, see https://github.com/phetsims/phet-io/issues/439

--- js/requirejs-plugins/ifphetio.js
+++ js/requirejs-plugins/ifphetio.js
@@ -7,9 +7,7 @@ define( function( module ) {
   return {
     load: function( id, require, load, config ) {
       storedID = id;
-      console.log( 'phetio loading id=' + id );
       if ( config.isBuild ) {
-        console.log( 'phetio plugin isbuild, id = ' + id );
 
         var url = require.toUrl( id );
 
@@ -21,7 +19,6 @@ define( function( module ) {
           content: global.phet.chipper.grunt.file.read( url ),
           attach: module.attach
         };
-        console.log( 'build map got ', buildMap[ id ].content );
 
         if ( global.phet.chipper.brand === 'phet-io' ) {
           require( [ id ], load );
@@ -32,7 +29,6 @@ define( function( module ) {
       }
       else {
         var brand = window.phet.chipper.brand;
-        console.log( 'found brand', brand );
         if ( brand === 'phet-io' ) {
           require( [ id ], load );
         }
@@ -42,26 +38,13 @@ define( function( module ) {
       }
     },
     write: function( pluginName, moduleName, write ) {
-      console.log( 'module content: ', module.content );
-      console.log( 'phetio.write, brand=', global.phet.chipper.brand );
-      console.log( 'storedid was ', storedID );
       var text = null;
       if ( global.phet.chipper.brand === 'phet-io' ) {
-
-        console.log( '########' );
-        console.log( 'Build Map was' );
-        console.log( buildMap[ moduleName ].content );
-
-        // text = 'define("' + pluginName + '!' + moduleName + '", function(){' + buildMap[ moduleName ].content + '});';
         text = 'define("' + moduleName + '", function(){' + buildMap[ moduleName ].content + '});';
-        console.log( '>>>>>WRITING MODULE\n' + text );
         write.asModule( moduleName, text, buildMap[ moduleName ].content );
-
-        console.log( '########' );
       }
       else {
         // It wasn't phet-io so load an empty module instead
-        // text = 'define("' + pluginName + '!' + moduleName + '", function(){});';
         text = 'define("' + moduleName + '", function(){});';
         write( text );
       }
