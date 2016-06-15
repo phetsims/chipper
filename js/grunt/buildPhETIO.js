ab2e82f9cb61316d8f50e5088fa0f55c15882462
samreid <reids@colorado.edu>
2016-06-15 13:21:50 -0600

    Fixed copying of api file for the build

--- js/grunt/buildPhETIO.js
+++ js/grunt/buildPhETIO.js
@@ -140,12 +140,11 @@ module.exports = function( grunt, buildConfig ) {
       copyDirectory( grunt, simSpecificPath, 'build/phet-io/protected/wrappers', filterWrapper, { failOnExistingFiles: failOnExistingFiles } );
     }
 
-    // Copy the API files
-    for ( i = 0; i < preload.length; i++ ) {
-      var lastSlash = preload[ i ].lastIndexOf( '/' );
-      var filename = preload[ i ].substring( lastSlash + 1 );
-      grunt.file.copy( preload[ i ], 'build/phet-io/protected/api/' + filename );
-    }
+    // Copy the API files.  The file is located in e.g., phet-io/js/simulations/concentration/concentration-api.js
+    var apiFilename = buildConfig.name + '-api.js';
+    var apiFilePath = '../phet-io/js/simulations/' + buildConfig.name + '/' + apiFilename;
+    grunt.file.copy( apiFilePath, 'build/phet-io/protected/api/' + apiFilename );
+    // TODO: How to copy other dependencies, such as beers-law-lab?
 
     // The .htaccess file
     grunt.file.copy( '../phet-io/templates/.htaccess', 'build/phet-io/protected/.htaccess' );
