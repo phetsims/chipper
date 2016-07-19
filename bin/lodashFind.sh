1f1e3e6fc71a237e6f9e907e0d2e75f07e89cc6d
Chris Malley <cmalley@pixelzoom.com>
2016-07-19 12:22:24 -0600

    script to identify uses of lodash, https://github.com/phetsims/sherpa/issues/58

---
+++ bin/lodashFind.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#====================================================================================================
+#
+# Finds usages of lodash in PhET JavaScript source code.
+# Prints a list with number of occurrences of each lodash function found.
+# This takes a long-ish time to run, so be patient.
+#
+# Author: Chris Malley (PixelZoom, Inc.)
+#
+#====================================================================================================
+
+CHIPPER_BIN=`dirname "${BASH_SOURCE[0]}"`
+ROOT_DIR=${CHIPPER_BIN}/../..
+cd ${ROOT_DIR}
+
+sourceFiles=`find . -name "*.js" -print | grep -v node_modules | grep -v sherpa | grep -v "scenery/snapshots" | grep -v "litmus/jsmol"`
+
+for sourceFile in ${sourceFiles}
+do
+  # Extract occurrences of _.TOKEN, then trim the leading '_.'
+  grep -Eo "_\.[a-zA-Z]*" ${sourceFile} | cut -c 3-
+done | sort | uniq -c | sort -bgr
