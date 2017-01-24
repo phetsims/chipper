066cc1587e7de8e9484e54a4173044d1396b2b19
zepumph <michael.kauzmann@colorado.edu>
2017-01-23 17:55:19 -0700

    added indicateUninstrumentedCode for scenery-phet, https://github.com/phetsims/chipper/issues/531

--- eslint/rules/require-tandem-support.js
+++ eslint/rules/require-tandem-support.js
@@ -13,11 +13,11 @@ module.exports = function( context ) {
   'use strict';
 
   // Whitelist of directories to check that Tandem has support.
-  // var directoriesToRequireTandemSupport = [ /sun[\/\\]js/, /scenery-phet[\/\\]js/ ];
-  var directoriesToRequireTandemSupport = [ /sun[\/\\]js/ ];
+  var directoriesToRequireTandemSupport = [ /sun[\/\\]js/, /scenery-phet[\/\\]js/ ];
+  // var directoriesToRequireTandemSupport = [ /sun[\/\\]js/ ];
 
   var requireTandemSupportBlackList = [ /[\/\\]demo[\/\\]/, /-main\.js$/, /-config\.js$/, /sun\.js/, /sunQueryParameters\.js/,
-    /[\/\\]ColorConstants\.js/ ];
+    /[\/\\]ColorConstants\.js/, /[\/\\][sS]ceneryPhet.*\.js/ ];
   return {
 
     Program: function requreTandemSupport( node ) {
