f72d7427fa5b4d7b814bb365024c7557800dd227
zepumph <michael.kauzmann@colorado.edu>
2017-02-07 17:19:58 -0700

    added a few shas, https://github.com/phetsims/phet-io/issues/972

--- js/documentation/generateDocs.js
+++ js/documentation/generateDocs.js
@@ -4,6 +4,9 @@
 'use strict';
 
 var fs = require( 'fs' );
+var child_process = require( 'child_process' );
+var execSync = child_process.execSync;
+
 
 /**
  * Returns the string of the query parameter schema that you are getting
@@ -11,7 +14,7 @@ var fs = require( 'fs' );
  * @param marker - the text to find the beginning of the query parameters
  * @returns {string} - query parameter string
  */
-function getQueryParameters( filename, marker) {
+function getQueryParameters( filename, marker ) {
 
   var initGlobalsText = fs.readFileSync( filename ).toString();
   var schemaIndex = initGlobalsText.indexOf( marker );
@@ -22,14 +25,39 @@ function getQueryParameters( filename, marker) {
 }
 
 
-var phetQueryParameters = getQueryParameters( '../../../chipper/js/initialize-globals.js', 'QUERY_PARAMETERS_SCHEMA = ');
-var phetioQueryParameters = getQueryParameters( '../phetio-query-parameters.js', 'QUERY_PARAMETERS_SCHEMA = ');
+/**
+ *
+ * @param repoName
+ * @returns {{branch: *, sha: *, repo: *}}
+ */
+function getGitInfo( repoName ) {
 
+  var sha = execSync( 'git rev-parse HEAD', { cwd: '../../../' + repoName, env: process.env } ).toString();
+  var branch = execSync( 'git rev-parse --abbrev-ref HEAD', { cwd: '../../../' + repoName, env: process.env } ).toString();
+
+  return { branch: branch.replace('\n', ''), sha: sha.replace('\n', ''), repo: repoName };
+}
+
+
+var phetQueryParameters = getQueryParameters( '../../../chipper/js/initialize-globals.js', 'QUERY_PARAMETERS_SCHEMA = ' );
+var phetioQueryParameters = getQueryParameters( '../phetio-query-parameters.js', 'QUERY_PARAMETERS_SCHEMA = ' );
+
+
+var phetioGitInfo = getGitInfo( 'phet-io' );
+var joistGitInfo = getGitInfo( 'joist' );
+var chipperGitInfo = getGitInfo( 'chipper' );
+
+var shaHTML = '';
+[ phetioGitInfo, joistGitInfo, chipperGitInfo ].forEach( function( info ) {
+  shaHTML += '<li>' + info.repo + ' on branch "' + info.branch + '" at sha + ' + info.sha + '</li><br>\n';
+} );
 
 // Replace the template strings with the api documentation
 var documentationFilename = 'phetioDocumentation';
 var phetioDocumentationTemplateText = fs.readFileSync( documentationFilename + '_template.html' ).toString();
 
+
+phetioDocumentationTemplateText = phetioDocumentationTemplateText.replace( '{{SHAS}}', shaHTML );
 phetioDocumentationTemplateText = phetioDocumentationTemplateText.replace( '{{PHET_QUERY_PARAMETERS}}', phetQueryParameters );
 phetioDocumentationTemplateText = phetioDocumentationTemplateText.replace( '{{PHETIO_QUERY_PARAMETERS}}', phetioQueryParameters );
 
