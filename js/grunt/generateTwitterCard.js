08cb32889f029407aa1b8a075fc49b96f9e82e61
mattpen <matthew.pennington@colorado.edu>
2017-04-05 11:03:35 -0600

    Added twitter card generation to production deploy process phetsims/website#583

---
+++ js/grunt/generateTwitterCard.js
@@ -0,0 +1,50 @@
+// Copyright 2017, University of Colorado Boulder
+
+/**
+ * This grunt task generates 128x84 and 600x394 thumbnails of the sim's screenshot in assets.
+ * Thumbnails are put in the build directory of the sim. If the directory doesn't exist, it is created.
+ * New grunt tasks can easily be created to generate different sized images by passing this function
+ * different heights and widths.
+ *
+ * @author Aaron Davis
+ */
+/* eslint-env node */
+'use strict';
+
+// modules
+var Jimp = require( 'jimp' ); // eslint-disable-line require-statement-match
+var assert = require( 'assert' );
+
+// constants
+var BUILD_DIRECTORY = 'build';
+
+/**
+ * @param grunt - the grunt instance
+ * @param {string} repositoryName - name of the repository
+ * @param {number} width of the resized image
+ * @param {number} height of the resized image
+ * @param {function} callback
+ */
+module.exports = function( grunt, repositoryName, callback ) {
+
+  var fullResImageName = 'assets/' + repositoryName + '-screenshot.png';
+  var destinationFile = BUILD_DIRECTORY + '/' + repositoryName + '-twitter-card.png';
+
+  if ( !grunt.file.exists( fullResImageName ) ) {
+    grunt.log.writeln( 'no image file exists: ' + fullResImageName + '. Not running task: generate-thumbnails' );
+    return;
+  }
+
+  if ( !grunt.file.exists( BUILD_DIRECTORY ) ) {
+    grunt.file.mkdir( BUILD_DIRECTORY );
+  }
+  assert( grunt.file.isDir( BUILD_DIRECTORY ), 'Error: "build" is not a directory' );
+
+  // The following creates an 800x400 image that is a letter-boxed version of the original size image and has small transparent padding on the top and bottom.
+  new Jimp( fullResImageName, function() { // eslint-disable-line
+    this.resize( 600, 394 ) // Preserve original dimensions
+      .contain( 585, 400 )  // Resize to allow padding on top/bottom
+      .contain( 800, 400 )  // Add padding on right/left
+      .write( destinationFile, callback );
+  } );
+};
