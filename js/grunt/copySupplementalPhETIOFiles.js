01bf4f8011f493e3b74e39491155aab19628c285
zepumph <michael.kauzmann@colorado.edu>
2017-04-10 15:38:31 -0600

    comment out documentation for now, https://github.com/phetsims/phet-io/issues/972

--- js/grunt/copySupplementalPhETIOFiles.js
+++ js/grunt/copySupplementalPhETIOFiles.js
@@ -12,7 +12,7 @@
 // modules
 var copyDirectory = require( '../../../chipper/js/grunt/copyDirectory' );
 var ChipperStringUtils = require( '../../../chipper/js/common/ChipperStringUtils' );
-var generatePhETIOAPIDocs = require( '../../../chipper/js/grunt/generatePhETIOAPIDocs' );
+// var generatePhETIOAPIDocs = require( '../../../chipper/js/grunt/generatePhETIOAPIDocs' );
 
 
 module.exports = function( grunt, buildConfig ) {
