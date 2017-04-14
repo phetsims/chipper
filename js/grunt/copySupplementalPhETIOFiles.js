4cf13d6c697458f21bc77195482656eebcd2b2df
zepumph <michael.kauzmann@colorado.edu>
2017-04-14 10:33:17 -0600

    documentation working with createType in phet-io, https://github.com/phetsims/phet-io/issues/972

--- js/grunt/copySupplementalPhETIOFiles.js
+++ js/grunt/copySupplementalPhETIOFiles.js
@@ -12,7 +12,7 @@
 // modules
 var copyDirectory = require( '../../../chipper/js/grunt/copyDirectory' );
 var ChipperStringUtils = require( '../../../chipper/js/common/ChipperStringUtils' );
-// var generatePhETIOAPIDocs = require( '../../../chipper/js/grunt/generatePhETIOAPIDocs' );
+var generatePhETIOAPIDocs = require( '../../../chipper/js/grunt/generatePhETIOAPIDocs' );
 
 
 module.exports = function( grunt, buildConfig ) {
@@ -169,5 +169,5 @@ module.exports = function( grunt, buildConfig ) {
   grunt.file.write( DESTINATION_PATH + '/' + OUTPUT_FILE, COPYRIGHT_HEADER + '\n\n' + minified );
 
   // Generate API Documentation
-  // generatePhETIOAPIDocs( grunt, buildConfig );
+  generatePhETIOAPIDocs( grunt, buildConfig );
 };
