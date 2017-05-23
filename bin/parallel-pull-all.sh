757bd717c624f9f809591513ba2c336067ae8bc1
samreid <reids@colorado.edu>
2017-05-22 19:07:29 -0600

    Added parallel-pull-all.sh, see https://github.com/phetsims/chipper/issues/576

---
+++ bin/parallel-pull-all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#====================================================================================================
+#
+# Does a 'git pull --rebase' on every active repo.
+#
+# Author: Jonathan Olson
+#
+#====================================================================================================
+
+CHIPPER_BIN=`dirname "${BASH_SOURCE[0]}"`
+WORKING_DIR=${CHIPPER_BIN}/../..
+cd ${WORKING_DIR}
+
+for repo in `cat chipper/data/active-repos | xargs | tr -d '\r'`
+do
+  if [ -d "$repo" ]; then
+    echo $repo
+    cd $repo
+    git pull --rebase &
+    cd ..
+  fi
+done
+wait
