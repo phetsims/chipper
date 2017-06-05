caaef10cbfec00db95d90e247cf147ef1d2348ff
Chris Malley <cmalley@pixelzoom.com>
2017-06-05 12:10:49 -0600

    rewrite pull-all.sh to use git-pull-all, https://github.com/phetsims/chipper/issues/576

--- bin/pull-all.sh
+++ bin/pull-all.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #====================================================================================================
 #
-# Does a 'git pull --rebase' on every active repo.
+# Does a 'git-pull-all' on every active repo.
+# Assumes that you have installed git-pull-all via: npm install -g git-pull-all
 #
 # Author: Jonathan Olson
+# Author: Chris Malley (PixelZoom, Inc.)
 #
 #====================================================================================================
 
@@ -11,12 +13,12 @@ CHIPPER_BIN=`dirname "${BASH_SOURCE[0]}"`
 WORKING_DIR=${CHIPPER_BIN}/../..
 cd ${WORKING_DIR}
 
-for repo in `cat chipper/data/active-repos | xargs | tr -d '\r'`
-do
-  if [ -d "$repo" ]; then
-    echo $repo
-    cd $repo
-    git pull --rebase
-    cd ..
-  fi
-done
+GIT_PULL_ALL=git-pull-all
+
+if ! type ${GIT_PULL_ALL} > /dev/null 2>&1;
+then
+  echo "${GIT_PULL_ALL} not found. Install it by running: npm install -g ${GIT_PULL_ALL}"
+else
+  ${GIT_PULL_ALL}
+fi
+
