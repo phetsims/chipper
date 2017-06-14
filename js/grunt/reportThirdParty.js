a1bb8afb8da3e4aef7724a77ed9b346c5f844aa0
Chris Malley <cmalley@pixelzoom.com>
2017-06-14 14:21:36 -0600

    line break

--- js/grunt/reportThirdParty.js
+++ js/grunt/reportThirdParty.js
@@ -11,7 +11,8 @@
  * Third party entries are parsed from the HTML files for all simulations published on the PhET website.
  * See getLicenseEntry.js for documentation of the fields in the entries.
  *
- * Copy the local-auth-code key value from phet-server:/etc/tomcat/context.xml into the value for websiteAuthorizationCode in ~/.phet/build-local.json
+ * Copy the local-auth-code key value from phet-server:/etc/tomcat/context.xml into the value for
+ * websiteAuthorizationCode in ~/.phet/build-local.json
  *
  * @author Sam Reid
  */
