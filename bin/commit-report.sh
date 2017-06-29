676d99d87080723ef18cc50abf591b8ac6d4f2e3
samreid <reids@colorado.edu>
2017-06-28 21:37:26 -0600

    Suppress commit sha

--- bin/commit-report.sh
+++ bin/commit-report.sh
@@ -6,4 +6,4 @@ if [ -z "$1" ]
     echo "Usage: commit-report.sh username"
     exit 1
 fi
-for-each.sh active-repos git log --all --remotes --since=7.days --author=$1 --pretty=format:"%h%x09%an%x09%ad%x09%s" --date=relative
+for-each.sh active-repos git log --all --remotes --since=7.days --author=$1 --pretty=format:"%an%x09%ad%x09%s" --date=relative
