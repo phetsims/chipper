c8c0d8ae31c7db5037d6184e6f5e3368de093cdf
samreid <reids@colorado.edu>
2017-10-04 15:41:20 -0600

    Added an ESLint rule that prevents Math.sign, see https://github.com/phetsims/circuit-construction-kit-dc/issues/163

---
+++ eslint/rules/no-math-sign.js
@@ -0,0 +1,33 @@
+// Copyright 2002-2015, University of Colorado Boulder
+/**
+ * @fileoverview Rule to guarantee we do not use Math.sign which is not supported on IE
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @copyright 2017 University of Colorado Boulder
+ */
+
+//------------------------------------------------------------------------------
+// Rule Definition
+//------------------------------------------------------------------------------
+
+module.exports = function( context ) {
+  'use strict';
+
+  return {
+
+    MemberExpression: function noMathSign( node ) {
+
+      // See https://astexplorer.net/ for an interactive tool that lets you explore ASTs
+      if ( node.object && node.object.name === 'Math' && node.property && node.property.name === 'sign' ) {
+        context.report( {
+          node: node,
+          loc: node.loc.start,
+          message: 'Illegal use of Math.sign'
+        } );
+      }
+    }
+  };
+};
+
+module.exports.schema = [
+  // JSON Schema for rule options goes here
+];
