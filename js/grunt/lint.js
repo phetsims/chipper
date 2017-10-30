6af83c188aa6720161c8b73a81d09ded7a236865
Jonathan Olson <olsonsjc@gmail.com>
2017-10-30 13:19:43 -0600

    Formatting fix

--- js/grunt/lint.js
+++ js/grunt/lint.js
@@ -85,7 +85,7 @@ module.exports = function( grunt, target, buildConfig ) {
   var report = cli.executeOnFiles( files );
 
   // pretty print results to console if any
-  (report.warningCount || report.errorCount) && grunt.log.write( cli.getFormatter()( report.results ) );
+  ( report.warningCount || report.errorCount ) && grunt.log.write( cli.getFormatter()( report.results ) );
 
   report.warningCount && grunt.fail.warn( report.warningCount + ' Lint Warnings' );
   report.errorCount && grunt.fail.fatal( report.errorCount + ' Lint Errors' );
