de20836e96e5eb75841a02ff273c7ec25d73fe35
Jonathan Olson <olsonsjc@gmail.com>
2017-11-16 21:28:02 -0700

    Patch for https://github.com/phetsims/chipper/issues/563

--- templates/chipper-string-setup.js
+++ templates/chipper-string-setup.js
@@ -7,6 +7,6 @@
 
   window.phet.chipper.strings.get = function( key ) {
     // override strings via the 'strings' query parameter
-    return stringOverrides[ key ] || window.phet.chipper.mapString( window.phet.chipper.strings[ key ], stringTest );
+    return stringOverrides[ key ] || window.phet.chipper.mapString( window.phet.chipper.strings[ window.phet.chipper.locale ][ key ], stringTest );
   };
 })();
