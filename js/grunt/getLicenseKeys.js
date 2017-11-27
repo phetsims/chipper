690a79b753fb2320c15c97066f48a432584aeca1
Jonathan Olson <jonathan.olson@colorado.edu>
2017-11-27 12:29:51 -0700

    getLicenseKeys

---
+++ js/grunt/getLicenseKeys.js
@@ -0,0 +1,49 @@
+// Copyright 2017, University of Colorado Boulder
+
+/**
+ * Gets the license keys for sherpa (third-party) libs that are used.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ * @author Jonathan Olson <jonathan.olson@colorado.edu>
+ */
+/* eslint-env node */
+'use strict';
+
+const _ = require( 'lodash' ); // eslint-disable-line require-statement-match
+const getPreloads = require( './getPreloads' );
+
+/**
+ * Gets the license keys for sherpa (third-party) libs that are used.
+ *
+ * @param {Object} grunt
+ * @param {string} repo
+ * @param {string} brand
+ * @returns {Array.<string>}
+ */
+module.exports = function( grunt, repo, brand ) {
+  const packageObject = grunt.file.readJSON( '../' + repo + '/package.json' );
+  const buildObject = grunt.file.readJSON( '../chipper/build.json' );
+  const preload = getPreloads( grunt, brand );
+
+  // start with package.json
+  var licenseKeys = packageObject.phet.licenseKeys || [];
+
+  // add common and brand-specific entries from build.json
+  [ 'common', brand ].forEach( id => {
+    if ( buildObject[ id ] && buildObject[ id ].licenseKeys ) {
+      licenseKeys = licenseKeys.concat( buildObject[ id ].licenseKeys );
+    }
+  } );
+
+  // Extract keys from preload for sherpa (third-party) dependencies
+  preload.forEach( path => {
+    if ( path.indexOf( '/sherpa/' ) !== -1 ) {
+      var lastSlash = path.lastIndexOf( '/' );
+      var key = path.substring( lastSlash + 1 );
+      licenseKeys.push( key );
+    }
+  } );
+
+  // sort and remove duplicates
+  return _.uniq( _.sortBy( licenseKeys, key => key.toUpperCase() ) );
+};
