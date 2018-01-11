c52ae6cbd6ef2901263a6595fe93c8841e3dd234
samreid <reids@colorado.edu>
2018-01-11 10:48:59 -0700

    Renamed toolBox=>toolbox and added lint rule, see https://github.com/phetsims/tasks/issues/909

---
+++ eslint/rules/bad-text.js
@@ -0,0 +1,38 @@
+// Copyright 2018, University of Colorado Boulder
+
+/**
+ * Lint detector for invalid text.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+module.exports = function( context ) {
+  'use strict';
+
+  var badTexts = [
+    'tool' + 'Box', // intentionally split to avoid getting caught in the lint, split manually because lint apparently rules cannot be disabled in their declarations
+    'Tool' + 'Box' // intentionally split to avoid getting caught in the lint, split manually because lint apparently rules cannot be disabled in their declarations
+  ];
+
+  var checkNode = function( node, text ) {
+    badTexts.forEach( function( badText ) {
+      if ( text.indexOf( badText ) >= 0 ) {
+        context.report( {
+          node: node,
+          message: 'Bad text: ' + badText + ' in ' + node.type
+        } );
+      }
+    } );
+  };
+  return {
+    Identifier: function checkCopyright( node ) {
+      checkNode( node, node.name );
+    },
+    Literal: function checkLiteral( node ) {
+      checkNode( node, node.raw );
+    }
+  };
+};
+
+module.exports.schema = [
+  // JSON Schema for rule options goes here
+];
