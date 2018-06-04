7d12527d4489798ede76256734ca6c88ba755dd1
denz1994 <denz1994@gmail.com>
2018-06-04 13:06:29 -0400

    Added lint rule to 'Use Array.isArray() instead of instanceof Array'. https://github.com/phetsims/chipper/issues/659.

---
+++ eslint/rules/no-instanceof-array.js
@@ -0,0 +1,34 @@
+// Copyright 2002-2015, University of Colorado Boulder
+/**
+ * @fileoverview Rule to guarantee we do not use instanceof Array
+ * @author Denzell Barnett (PhET Interactive Simulations)
+ * @copyright 2018 University of Colorado Boulder
+ */
+
+//------------------------------------------------------------------------------
+// Rule Definition
+//------------------------------------------------------------------------------
+
+module.exports = function( context ) {
+  'use strict';
+
+  {
+    return {
+      BinaryExpression: function( node ) {
+        if ( node.operator === 'instanceof' && node.right.type === 'Identifier' && node.right.name === 'Array' ) {
+          context.report( {
+            node: node,
+            message: 'Use Array.isArray() instead of instanceof Array',
+            data: {
+              identifier: node.name
+            }
+          } );
+        }
+      }
+    };
+  }
+};
+
+module.exports.schema = [
+  // JSON Schema for rule options goes here
+];
