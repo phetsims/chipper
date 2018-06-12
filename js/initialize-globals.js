a674320a74f7a985244bc7a6665f8c8227f51ce0
Jonathan Olson <jonathan.olson@colorado.edu>
2018-06-12 17:03:38 -0600

    Additional doc note for https://github.com/phetsims/phetcommon/issues/46

--- js/initialize-globals.js
+++ js/initialize-globals.js
@@ -159,6 +159,9 @@
     /**
      * Used for providing a external Google Analytics property for tracking, see
      * https://github.com/phetsims/phetcommon/issues/46 for more information.
+     *
+     * This is useful for various users/clients that want to embed simulations, or direct users to simulations. For
+     * example, if a sim is included in an epub, the sim HTML won't have to be modified to include page tracking.
      */
     ga: {
       type: 'string',
