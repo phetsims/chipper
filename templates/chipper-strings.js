ce11efb1c943cc2fed9cfb0fa3779ad4e78c17a5
Jonathan Olson <jonathan.olson@colorado.edu>
2018-07-26 12:36:44 -0600

    Support locale fallback for _all.html (or general) built sim usage, see https://github.com/phetsims/chipper/issues/694

--- templates/chipper-strings.js
+++ templates/chipper-strings.js
@@ -9,6 +9,19 @@
 
   window.phet.chipper.strings.get = function( key ) {
     // override strings via the 'strings' query parameter
-    return stringOverrides[ key ] || window.phet.chipper.mapString( window.phet.chipper.strings[ window.phet.chipper.locale ][ key ], stringTest );
+    if ( stringOverrides[ key ] ) {
+      return stringOverrides[ key ];
+    }
+    var stringMap = window.phet.chipper.strings[ window.phet.chipper.locale ];
+    // Don't fail out on unsupported locales, see https://github.com/phetsims/chipper/issues/694
+    if ( !stringMap ) {
+      // See if there's a translation for just the language code
+      stringMap = window.phet.chipper.strings[ window.phet.chipper.locale.slice( 0, 2 ) ];
+
+      if ( !stringMap ) {
+        stringMap = window.phet.chipper.strings.en;
+      }
+    }
+    return window.phet.chipper.mapString( stringMap[ key ], stringTest );
   };
 })();
