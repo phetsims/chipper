419aa10e673cb13c0535d346e1c905ca083a0c43
jbphet <john.blanco@colorado.edu>
2018-08-01 11:03:15 -0600

    minor code cleanup, see https://github.com/phetsims/chipper/issues/694

--- templates/chipper-strings.js
+++ templates/chipper-strings.js
@@ -1,3 +1,5 @@
+// Copyright 2018, University of Colorado Boulder
+
 (function() {
   'use strict';
 
@@ -8,13 +10,16 @@
                    null;
 
   window.phet.chipper.strings.get = function( key ) {
+
     // override strings via the 'strings' query parameter
     if ( stringOverrides[ key ] ) {
       return stringOverrides[ key ];
     }
     var stringMap = window.phet.chipper.strings[ window.phet.chipper.locale ];
+
     // Don't fail out on unsupported locales, see https://github.com/phetsims/chipper/issues/694
     if ( !stringMap ) {
+
       // See if there's a translation for just the language code
       stringMap = window.phet.chipper.strings[ window.phet.chipper.locale.slice( 0, 2 ) ];
 
