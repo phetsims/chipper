7a398a5209aef6140df05e1e7807c13741aa72ab
Chris Malley <cmalley@pixelzoom.com>
2018-09-18 17:12:55 -0600

    fix a bogus comment
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/grunt/reportMedia.js
+++ js/grunt/reportMedia.js
@@ -52,7 +52,7 @@ module.exports = function() {
 
     const searchDir = rootdir + repo + '/' + directory;
 
-    // Not all projects have an sounds/ directory
+    // Not all projects have all media-related directories
     if ( grunt.file.exists( searchDir ) ) {
 
       // Iterate over all media directories, such as images and sounds recursively
