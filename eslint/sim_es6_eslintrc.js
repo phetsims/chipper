9eba984770816d333828f7be7eb846382fbcaeee
zepumph <michael.kauzmann@colorado.edu>
2018-12-27 12:23:01 -0900

    Add es6 sim specific config, https://github.com/phetsims/tasks/issues/972

---
+++ eslint/sim_es6_eslintrc.js
@@ -0,0 +1,15 @@
+// Copyright 2018, University of Colorado Boulder
+// @author Michael Kauzmann
+
+/* eslint-env node */
+'use strict';
+
+/**
+ * Eslint config applied only to sims that are completely written in es6, with no es5 code.
+ */
+module.exports = {
+  extends: './.eslintrc.js',
+  rules: {
+    'no-var': 2
+  }
+};
