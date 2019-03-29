508d009ca7f2f81efa592efc2552e55066392df4
zepumph <michael.kauzmann@colorado.edu>
2019-03-28 16:02:30 -0800

    add "phetio element" to bad-text, https://github.com/phetsims/phet-io/issues/1422

--- eslint/rules/bad-text.js
+++ eslint/rules/bad-text.js
@@ -31,6 +31,7 @@ module.exports = function( context ) {
 
     // For phet-io use PHET_IO in constants
     'PHETIO',
+    'phetio element', // use "phet-io element" or "PhET-iO element"
     '@return '
   ];
 
