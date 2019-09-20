ab147053ffc275116ab07d59a21161918b6ed548
Jesse <jesse.greenberg@colorado.edu>
2019-09-20 19:55:29 -0400

    remuve unused function, see #791

--- eslint/rules/getBadTextTester.js
+++ eslint/rules/getBadTextTester.js
@@ -10,28 +10,6 @@
 module.exports = ( badTexts, context ) => {
   'use strict';
 
-  /**
-   * Returns true if the valueText is "good" (not an entry of badTexts).
-   *
-   * @param {string} valueText - the string to test
-   * @param {string|{name:string, regex:RegExp}} badText entry of badTexts
-   * @returns {boolean}
-   */
-  const reportBadText = ( valueText, badText, tokenLocation ) => {
-    let failedText = null;
-    if ( badText.regex instanceof RegExp && badText.regex.test( valueText ) ) {
-      failedText = badText.name;
-    }
-    if ( valueText.indexOf( badText ) >= 0 ) {
-      failedText = badText;
-    }
-
-    failedText && context.report( {
-      loc: tokenLocation.start,
-      message: 'File contains bad text: \'' + failedText + '\''
-    } );
-  };
-
   return node => {
     const sourceCode = context.getSourceCode();
     const codeTokens = sourceCode.getTokens( node );
