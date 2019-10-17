a44b724da62c1df1e3fcb75a13349a6031b84c32
zepumph <michael.kauzmann@colorado.edu>
2019-10-17 15:02:35 -0800

    revert merge in string plugin, https://github.com/phetsims/phet-core/issues/71

--- js/requirejs-plugins/string.js
+++ js/requirejs-plugins/string.js
@@ -182,7 +182,7 @@ define( require => {
             getWithCache( localeSpecificPath, parsed => {
 
                 // Combine the primary and fallback strings into one object hash.
-                const parsedStrings = merge( parsedFallbackStrings, parsed );
+                const parsedStrings = _.extend( parsedFallbackStrings, parsed ); // extend to avoid the phet-core dependency
                 if ( parsedStrings[ key ] !== undefined ) {
                   onload( window.phet.chipper.mapString( parsedStrings[ key ].value, stringTest ) );
                 }
