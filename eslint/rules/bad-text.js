8e72946c43e8d3096f4dd0285d3484cf55be22c0
Jesse <jesse.greenberg@colorado.edu>
2019-11-08 13:44:21 -0500

    uncomment ' => { return ' in bad text, see phetsims/energy-skate-park#140

--- eslint/rules/bad-text.js
+++ eslint/rules/bad-text.js
@@ -51,11 +51,11 @@ module.exports = function( context ) {
     'phetio element', // use "phet-io element" or "PhET-iO element"
     'Phet-iO',
 
-    '@return '
+    '@return ',
 
     // TODO: uncomment once ESP(B) lint errors fixed, see https://github.com/phetsims/energy-skate-park/issues/140
     // if on a one line arrow function returning something, prefer instead `() => theReturn`, see https://github.com/phetsims/chipper/issues/790
-    // ' => { return '
+    ' => { return '
   ];
 
   return {
