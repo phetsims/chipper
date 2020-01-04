a7f923a8e8e554d71954b9898390f200fbd00eab
phet-dev <phetadmin@gmail.com>
2020-01-04 03:07:55 -0700

    update copyright dates from daily grunt work

--- js/initialize-globals.js
+++ js/initialize-globals.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2019, University of Colorado Boulder
+// Copyright 2015-2020, University of Colorado Boulder
 
 /**
  * Initializes phet globals that are used by all simulations, including assertions and query-parameters.
