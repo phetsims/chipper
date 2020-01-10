3496baa8e95a32a63bcad7c167a949bf7c0ccbf9
samreid <reids@colorado.edu>
2020-01-10 13:45:50 -0700

    Added notes for string plugin, see https://github.com/phetsims/chipper/issues/820

--- js/webpack/LocalizedStringBundle.js
+++ js/webpack/LocalizedStringBundle.js
@@ -4,10 +4,20 @@ export default class LocalizedStringBundle {
     // @private
     this.data = data;
 
-
     // TODO: Determine optimal API for this, see https://github.com/phetsims/chipper/issues/820
-    // TODO: This API allows you to do REPO_strings.localized.myKey.  But we would probably often need to also do
+    // TODO: Support an API like REPO_strings.localized.myKey.subkey.
+    // TODO: For non-code keys, like so: REPO_strings.localized['simName-something'].
+
+    // TODO: @jonathanolson and @samreid like this API
+    // acidBaseSolutionStrings.acidBaseSolutionTitle;
+    // acidBaseSolutionStrings.a11y.keyboardTitle;
+    // acidBaseSolutionStrings['acidBaseSolutionTitle-with-a-hyphen'];
+
+    //  But we would probably often need to also do
     // TODO: REPO_strings.localized['my.key']
+    // TODO: We prefer an API where you
+
+    // TODO: fill in the fallbacks here.
     this.localized = _.mapValues( this.data.en, 'value' ); // TODO: select other locales https://github.com/phetsims/chipper/issues/820
   }
 }
