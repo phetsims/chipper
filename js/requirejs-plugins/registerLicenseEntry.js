4e68f4105bc51099e394119a9126cb3c97295d23
phet-dev <phetadmin@gmail.com>
2020-02-17 03:14:50 -0700

    update copyright dates from daily grunt work

--- js/requirejs-plugins/registerLicenseEntry.js
+++ js/requirejs-plugins/registerLicenseEntry.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2019, University of Colorado Boulder
+// Copyright 2015-2020, University of Colorado Boulder
 
 /**
  * Registers the license entry for a media file by adding it to global.phet.chipper.licenseEntries.
