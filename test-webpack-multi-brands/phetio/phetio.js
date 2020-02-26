55cf37f7f2cbdda8cfe0cb76d18da1e029dd8c55
chrisklus <christopher.klusendorf@colorado.edu>
2020-02-26 14:02:11 -0500

    Delete test-webpack-multi-brands, see #844

--- test-webpack-multi-brands/phetio/phetio.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable */
-
-import '../centralDependency.js';
-console.log( 'phetio' );
-export default 'phetio';
--- test-webpack-multi-brands/phetioStub-loader.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-// ignore the source input and return a stub
-module.exports = function( source ) {
-
-  console.log( 'in loader' );
-  return '()=>"hi";';
-};
