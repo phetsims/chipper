3b7f87eec12f1c3bf41bf08919be1d8fbd6f4775
Jonathan Olson <jonathan.olson@colorado.edu>
2020-03-30 19:34:19 -0600

    Some getStringMap cleanup and docs, see https://github.com/phetsims/chipper/issues/922

--- js/grunt/getStringMap.js
+++ js/grunt/getStringMap.js
@@ -29,6 +29,12 @@ const getStringFilesContents = ( reposWithUsedStrings, locales ) => {
   reposWithUsedStrings.forEach( repo => {
     stringFilesContents[ repo ] = {};
 
+    /**
+     * Adds a locale into our stringFilesContents map.
+     *
+     * @param {string} locale
+     * @param {boolean} isRTL
+     */
     const addLocale = ( locale, isRTL ) => {
       // Read optional string file
       const stringsFilename = path.normalize( `../${locale === ChipperConstants.FALLBACK_LOCALE ? '' : 'babel/'}${repo}/${repo}-strings_${locale}.json` );
@@ -51,6 +57,7 @@ const getStringFilesContents = ( reposWithUsedStrings, locales ) => {
       assert( localeInfo[ locale ], `unsupported locale: ${locale}` );
       const isRTL = localeInfo[ locale ].direction === 'rtl';
 
+      // Handle fallback locales
       addLocale( locale, isRTL );
       if ( locale.length > 2 ) {
         const middleLocale = locale.slice( 0, 2 );
@@ -75,6 +82,15 @@ module.exports = function( locales, phetLibs, usedModules ) {
 
   assert( locales.indexOf( ChipperConstants.FALLBACK_LOCALE ) !== -1, 'fallback locale is required' );
 
+  /**
+   * For a given locale, return an array of specific locales that we'll use as fallbacks, e.g.
+   * 'zh_CN' => [ 'zh_CN', 'zh', 'en' ]
+   * 'es' => [ 'es', 'en' ]
+   * 'en' => [ 'en' ]
+   *
+   * @param {string} locale
+   * @returns {Array.<string>}
+   */
   const localeFallbacks = locale => {
     return [
       ...( locale !== ChipperConstants.FALLBACK_LOCALE ? [ locale ] : [] ), // e.g. 'zh_CN'
@@ -83,8 +99,11 @@ module.exports = function( locales, phetLibs, usedModules ) {
     ];
   };
 
+  // Load the file contents of every single JS module that used any strings
   const usedFileContents = usedModules.map( usedModule => fs.readFileSync( `../${usedModule}`, 'utf-8' ) );
 
+  // Compute which repositories contain one more more used strings (since we'll need to load string files for those
+  // repositories).
   let reposWithUsedStrings = [];
   usedFileContents.forEach( fileContent => {
     const allImportStatements = fileContent.match( /import [a-zA-Z_$][a-zA-Z0-9_$]*Strings from '[^\n\r]+-strings.js';/g );
@@ -94,6 +113,8 @@ module.exports = function( locales, phetLibs, usedModules ) {
   } );
   reposWithUsedStrings = _.uniq( reposWithUsedStrings );
 
+  // Compute a map of {repo:string} => {requirejsNamepsace:string}, so we can construct full string keys from strings
+  // that would be accessing them, e.g. `joistStrings.ResetAllButton.name` => `JOIST/ResetAllButton.name`.
   const requirejsNamespaceMap = {};
   reposWithUsedStrings.forEach( repo => {
     const packageObject = JSON.parse( fs.readFileSync( `../${repo}/package.json`, 'utf-8' ) );
@@ -104,6 +125,7 @@ module.exports = function( locales, phetLibs, usedModules ) {
   // maps [repositoryName][locale] => contents of locale string file
   const stringFilesContents = getStringFilesContents( reposWithUsedStrings, locales );
 
+  // Initialize our full stringMap object (which will be filled with results and then returned as our string map).
   const stringMap = {};
   locales.forEach( locale => {
     stringMap[ locale ] = {};
@@ -113,29 +135,49 @@ module.exports = function( locales, phetLibs, usedModules ) {
   // strings, this data structure doesn't nest. Instead it gets nested string values, and then sets them with the
   // flat key string like `"FRICTION/a11y.some.string.here": { value: 'My Some String' }`
   reposWithUsedStrings.forEach( repo => {
+
+    // Scan all of the files with string module references, scanning for anything that looks like a string access for
+    // our repo. This will include the string module reference, e.g. `joistStrings.ResetAllButton.name`, but could also
+    // include slightly more (since we're string parsing), e.g. `joistStrings.ResetAllButton.name.length` would be
+    // included, even though only part of that is a string access.
     let stringAccesses = [];
-    const prefix = `${_.camelCase( repo )}Strings`;
+    const prefix = `${_.camelCase( repo )}Strings`; // e.g. joistStrings
     usedFileContents.forEach( ( fileContent, i ) => {
+      // Only scan files where we can identify an import for it
       if ( fileContent.includes( `import ${prefix} from` ) ) {
+
+        // Look for normal matches, e.g. `joistStrings.` followed by one or more chunks like:
+        // .somethingValuely_alphaNum3r1c
+        // [ 'aStringInBracketsBecauseOfSpecialCharacters' ]
+        //
+        // It will also then end on anything that doesn't look like another one of those chunks
         const matches = fileContent.match( new RegExp( `${prefix}(\\.[a-zA-Z_$][a-zA-Z0-9_$]*|\\[ '[^']+' \\])+[^\\.\\[]`, 'g' ) );
         if ( matches ) {
           stringAccesses.push( ...matches.map( match => match.slice( 0, match.length - 1 ) ).filter( m => m !== `${prefix}.get` ) );
         }
 
+        // Look for backup matches, e.g. `joistStrings.get( 'fullString' )`
         const workaroundMatches = fileContent.match( new RegExp( `${prefix}\\.get\\( '([^']+)' \\)`, 'g' ) );
         if ( workaroundMatches ) {
           stringAccesses.push( ...workaroundMatches.map( match => `${prefix}.${match.slice( match.indexOf( '\'' ) + 1, match.lastIndexOf( '\'' ) )}` ) );
         }
       }
     } );
+
+    // Strip off our prefixes, so our stringAccesses will have things like `'ResetAllButton.name'` inside.
     stringAccesses = _.uniq( stringAccesses ).map( str => str.slice( prefix.length ) );
 
+    // Turn each string access into an array of parts, e.g. '.ResetAllButton.name' => [ 'ResetAllButton', 'name' ]
+    // or '[ \'A\' ].B[ \'C\' ]' => [ 'A', 'B', 'C' ]
     const stringKeysByParts = stringAccesses.map( access => access.match( /\.[a-zA-Z_$][a-zA-Z0-9_$]*|\[ '[^']+' \]/g ).map( token => {
       return token.startsWith( '.' ) ? token.slice( 1 ) : token.slice( 3, token.length - 3 );
     } ) );
 
+    // Concatenate the string parts for each access into something that looks like a partial string key, e.g.
+    // [ 'ResetAllButton', 'name' ] => 'ResetAllButton.name'
     const partialStringKeys = _.uniq( stringKeysByParts.map( parts => parts.join( '.' ) ) );
 
+    // For each string key and locale, we'll look up the string entry and fill it into the stringMap
     partialStringKeys.forEach( partialStringKey => {
       locales.forEach( locale => {
         let stringValue = null;
