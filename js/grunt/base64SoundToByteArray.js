195c5df66a901f97411d39f4f3686e257ba98193
jbphet <john.blanco@colorado.edu>
2020-05-13 13:08:13 -0600

    minor comment alignment fix

--- js/grunt/base64SoundToByteArray.js
+++ js/grunt/base64SoundToByteArray.js
@@ -17,12 +17,12 @@ const base64SoundToByteArray = ( audioContext, base64Sound ) => {
   const byteArray = new Uint8Array( byteChars.length );
   for ( let j = 0; j < byteArray.length; j++ ) {
 
-    // A note to future maintainers of this code: The line below, strictly speaking, seems to disregard the highest 8 bits
-    // of the character code, since strings in JavaScript are UTF-16.  This seems a little odd, but testing showed that
-    // the decoded base64 data never had values above 255, so it worked.  This code was leveraged from examples found on
-    // the web, and has worked through all our testing, so perhaps the upper bits are simply never used in this decoding
-    // process.  That seems like the best explanation.  However, if we one day run into issues with sound decoding, this
-    // might warrant further scrutiny.
+    // A note to future maintainers of this code: The line below, strictly speaking, seems to disregard the highest 8
+    // bits of the character code, since strings in JavaScript are UTF-16.  This seems a little odd, but testing showed
+    // that the decoded base64 data never had values above 255, so it worked.  This code was leveraged from examples
+    // found on the web, and has worked through all our testing, so perhaps the upper bits are simply never used in this
+    // decoding process.  That seems like the best explanation.  However, if we one day run into issues with sound
+    // decoding, this might warrant further scrutiny.
     byteArray[ j ] = byteChars.charCodeAt( j );
   }
   return byteArray;
