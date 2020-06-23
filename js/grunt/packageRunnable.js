5374f150f52b9c104edee8ca46bfe2975e5be1f7
chrisklus <christopher.klusendorf@colorado.edu>
2020-06-23 14:11:00 -0400

    Add dialog for no IE support and prevent scripts from running in IE, see https://github.com/phetsims/chipper/issues/954

--- js/grunt/packageRunnable.js
+++ js/grunt/packageRunnable.js
@@ -39,6 +39,8 @@ module.exports = function( config ) {
   assert( typeof locale === 'string', 'Requires locale' );
   assert( typeof htmlHeader === 'string', 'Requires htmlHeader' );
 
+  const simIEDetection = grunt.file.read( '../chipper/templates/sim-ie-detection.html' );
+
   const localizedTitle = stringMap[ locale ][ getTitleStringKey( repo ) ];
 
   // Directory on the PhET website where the latest version of the sim lives
@@ -48,7 +50,10 @@ module.exports = function( config ) {
     PHET_CARRIAGE_RETURN: '\r',
     PHET_SIM_TITLE: encoder.htmlEncode( localizedTitle ),
     PHET_HTML_HEADER: htmlHeader,
-    PHET_SIM_SCRIPTS: scripts.map( script => `<script type="text/javascript">${script}</script>` ).join( '\n' ),
+    PHET_IE_DETECTION: simIEDetection,
+
+    // wrap scripts in global check for IE
+    PHET_SIM_SCRIPTS: scripts.map( script => `<script type="text/javascript">if(!window.isIE){${script}}</script>` ).join( '\n' ),
 
     // metadata for Open Graph protocol, see phet-edmodo#2
     OG_TITLE: encoder.htmlEncode( localizedTitle ),
