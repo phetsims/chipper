844a6051a583f93e1a0e2d1720e12f916113f4ab
phet-dev <phetadmin@gmail.com>
2020-08-20 03:48:34 -0600

    update copyright dates from daily grunt work

--- js/scripts/ie-detection.js
+++ js/scripts/ie-detection.js
@@ -1,4 +1,4 @@
-// Copyright 2020, University of Colorado Boulder
+// Copyright 2018-2020, University of Colorado Boulder
 
 /**
  * Detects if the browser in use is Internet Explorer, and shows an error page if so.
