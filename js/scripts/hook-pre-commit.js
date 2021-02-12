9db8fb580a68cc2b3cec8d255a6bbcfcc1969239
samreid <reids@colorado.edu>
2021-02-12 14:08:00 -0700

    Rename generatePhetioAPI => generateMacroAPI, see generatePhetioAPI

--- js/scripts/hook-pre-commit.js
+++ js/scripts/hook-pre-commit.js
@@ -111,7 +111,7 @@ catch( e ) {
 // If there is a PhET-iO API Safety Net, compare to it
 try {
 
-  const generatePhetioAPI = require( '../../../chipper/js/phet-io/generatePhetioAPI' );
+  const generateMacroAPI = require( '../../../chipper/js/phet-io/generateMacroAPI' );
   const compareMacroAPIs = require( '../../../chipper/js/phet-io/compareMacroAPIs' );
   const REFERENCE_MACRO_API_PATH = '../perennial/build-phet-io/reference-macro-api.json';
   const exists = fs.existsSync( REFERENCE_MACRO_API_PATH );
@@ -127,7 +127,7 @@ try {
 
     const chunkSize = 4;
     ( async () => {
-      const results = await generatePhetioAPI( repos, {
+      const results = await generateMacroAPI( repos, {
         chunkSize: chunkSize,
         showMessagesFromSim: false // must be pure JSON
       } );
