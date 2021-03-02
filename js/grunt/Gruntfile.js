7421cba9392cac8447b61137d69131cf62087fc1
NickCrews <nicholas.b.crews@gmail.com>
2021-03-02 10:19:11 -0700

    Fix lint task help message, https://github.com/phetsims/phet-info/issues/150
    Before it
    - Was missing info on the --format option
    - Incorrectly said that it applied only to the current repo, even
      though the --patterns option was added
    - Was poorly formatted so that each option did not start on a new line.
      Now it follows the structure of the `build` task help message.
    - Was overly wordy on the --patterns option.

--- js/grunt/Gruntfile.js
+++ js/grunt/Gruntfile.js
@@ -260,12 +260,14 @@ module.exports = function( grunt ) {
   grunt.registerTask( 'build-for-server', 'meant for use by build-server only',
     [ 'build' ]
   );
-  grunt.registerTask( 'lint', 'lint js files that are specific to this repository.  Options:' +
-                              '' +
-                              '--disable-eslint-cache: cache will not be read or written' +
-                              '--fix: autofixable changes will be written to disk' +
-                              '--patterns: comma-separated list of directory/file patterns.  ' +
-                              'If not supplied, it will default to the repo where the command was run from.', wrapTask( async () => {
+  
+  grunt.registerTask( 'lint',
+    `lint js files. Options:
+--disable-eslint-cache: cache will not be read or written
+--fix: autofixable changes will be written to disk
+--format: Append an additional set of rules for formatting
+--patterns: comma-separated list of directory/file patterns. Default: repo where the command was run.`,
+    wrapTask( async () => {
 
     // --disable-eslint-cache disables the cache, useful for developing rules
     const cache = !grunt.option( 'disable-eslint-cache' );
