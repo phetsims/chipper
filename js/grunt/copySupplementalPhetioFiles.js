d5259cc9d3192989259804ad407e013d911615a4
Jesse <jesse.greenberg@colorado.edu>
2021-03-23 22:02:13 -0400

    Move keyboard event forwarding listener script to others, add to supplemental files, see phetsims/phet-io-wrappers/issues#402

--- js/grunt/copySupplementalPhetioFiles.js
+++ js/grunt/copySupplementalPhetioFiles.js
@@ -45,7 +45,8 @@ const LIB_FILES = [
   '../tandem/js/PhetioIDUtils.js',
   `../${WRAPPER_COMMON_FOLDER}/js/Client.js`,
   `../${WRAPPER_COMMON_FOLDER}/js/readFile.js`,
-  `../${WRAPPER_COMMON_FOLDER}/js/loadWrapperTemplate.js`
+  `../${WRAPPER_COMMON_FOLDER}/js/loadWrapperTemplate.js`,
+  `../${WRAPPER_COMMON_FOLDER}/js/addPanAndZoomKeyboardEventForwardingListener.js`
 ];
 
 const LIB_OUTPUT_FILE = 'phet-io.js';
