eb52d9b4f9d1aac0812becb3e94f6d6ab1f2bfb0
samreid <reids@colorado.edu>
2021-05-12 09:02:04 -0600

    Add docs, see https://github.com/phetsims/phet-io/issues/1761

--- test/generalTests.js
+++ test/generalTests.js
@@ -4,6 +4,10 @@
  * launch point to load any tests located around the chipper repo. This is to support running `qunit` with no args
  * from the top level of chipper, as is the recommended way to run chipper tests.
  *
+ * On May 12, 2021, @samreid ran tests like so:
+ * npm install --save-dev qunit
+ * node node_modules/qunit/bin/qunit.js test/generalTests.js
+ *
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
 
