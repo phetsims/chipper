b6e70e4f4630d9b97cb1e073937b13e190e79a72
Jesse <jesse.greenberg@colorado.edu>
2021-06-23 12:05:03 -0600

    hotkey (one word) instead of hot key, and add to bad-text.js, see https://github.com/phetsims/scenery/issues/1242

--- eslint/rules/bad-text.js
+++ eslint/rules/bad-text.js
@@ -42,6 +42,11 @@ module.exports = function( context ) {
     // event.keyCode according to spec, rather than event.keycode
     'keycode',
 
+    // prefer hotkey (one word)
+    'hot key',
+    'hotKey',
+    'HotKey',
+
     // In ES6, extending object causes methods to be dropped
     { id: 'extends Object ', codeTokens: [ 'extends', 'Object' ] },
 
