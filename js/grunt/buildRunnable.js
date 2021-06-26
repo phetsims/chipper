f0a241d3341b16e5ad3a23b0fd311fd5d25044f5
samreid <reids@colorado.edu>
2021-06-26 16:57:29 -0600

    Use the sim repo if a usedModule doesn't have a directory indicated, see https://github.com/phetsims/chipper/issues/1056

--- js/grunt/buildRunnable.js
+++ js/grunt/buildRunnable.js
@@ -126,7 +126,11 @@ module.exports = async function( repo, minifyOptions, instrument, allHTML, brand
   const dependencies = await getDependencies( repo );
 
   webpackResult.usedModules.forEach( moduleDependency => {
-    const moduleRepo = moduleDependency.slice( 0, moduleDependency.indexOf( path.sep ) );
+
+    // The first part of the path is the repo.  Or if no directory is specified, the file is in the sim repo.
+    const pathSeparatorIndex = moduleDependency.indexOf( path.sep );
+    const moduleRepo = pathSeparatorIndex >= 0 ? moduleDependency.slice( 0, pathSeparatorIndex ) :
+                       repo;
     assert( Object.keys( dependencies ).includes( moduleRepo ), `repo ${moduleRepo} missing from package.json's phetLibs for ${moduleDependency}` );
   } );
 
