b272b82f549d674a0e7e798e7565a6dbc97109a3
Sam Reid <reids@colorado.edu>
2021-10-25 23:08:15 -0600

    Add an eslint bad-sim-text rule that prevents "chipper/dist", see https://github.com/phetsims/chipper/issues/1124

--- eslint/rules/bad-sim-text.js
+++ eslint/rules/bad-sim-text.js
@@ -47,6 +47,9 @@ module.exports = function( context ) {
     // https://github.com/phetsims/scenery/issues/1156 and feel free to discuss if there are usages you want to support.
     ' instanceof Touch ',
 
+    // Prevent accidental importing of files from the TypeScript build output directory
+    'chipper/dist',
+
     // In sims, don't allow setTimout and setInterval calls coming from window, see https://github.com/phetsims/phet-info/issues/59
     {
       id: 'setTimeout(',
