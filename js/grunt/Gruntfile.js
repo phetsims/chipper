5a1f0a0242d2bb7358fd78a8f6f04ff6899813e0
Sam Reid <reids@colorado.edu>
2021-12-06 18:13:56 -0700

    Only run the type checker if phet-io repo is checked out, see https://github.com/phetsims/chipper/issues/1167

--- js/grunt/Gruntfile.js
+++ js/grunt/Gruntfile.js
@@ -213,9 +213,12 @@ module.exports = function( grunt ) {
 
       const repoPackageObject = grunt.file.readJSON( `../${repo}/package.json` );
 
-      // Run the type checker first
-      const results = await tsc( `../${repo}`, [ '--build' ] );
-      reportTscResults( results, grunt );
+      // Run the type checker first.  But since the phet-io repos are listed in tsconfig, we can only run the type
+      // checker if phet-io repos are checked out.
+      if ( fs.existsSync( '../phet-io' ) ) {
+        const results = await tsc( `../${repo}`, [ '--build' ] );
+        reportTscResults( results, grunt );
+      }
 
       // If that succeeds, then convert the code to JS
       new Transpiler().transpileRepos( getPhetLibs( repo ) );
