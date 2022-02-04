1d6f2d55fa0ad8c976e655633ecedba207cba2bf
zepumph <michael.kauzmann@colorado.edu>
2022-02-04 09:53:08 -0700

    add sim-tests eslintrc

---
+++ js/sim-tests/.eslintrc.js
@@ -0,0 +1,11 @@
+// Copyright 2022, University of Colorado Boulder
+// @author Michael Kauzmann
+
+
+module.exports = {
+  extends: '../../../chipper/eslint/sim_eslintrc.js',
+
+  rules: {
+    'bad-sim-text': 'off'
+  }
+};
