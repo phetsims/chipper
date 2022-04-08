1ce8c7cc2e58951de1a94800277b5c57cdcace05
Sam Reid <reids@colorado.edu>
2022-04-08 16:45:46 -0600

    Improve linting and type checking in phet-types.d.ts, see https://github.com/phetsims/chipper/issues/1219

--- phet-types.d.ts
+++ phet-types.d.ts
@@ -1,6 +1,4 @@
 // Copyright 2021, University of Colorado Boulder
-/* eslint-disable */
-// @ts-nocheck
 
 /**
  * Ambient type declarations for PhET code.  Many of these definitions can be moved/disabled once the common code is
@@ -9,17 +7,12 @@
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
-// See https://stackoverflow.com/a/51114250/3408502
-RoundButtonOptions = import('../sun/buttons/RoundButton.ts').RoundButtonOptions;
+declare var assert: undefined | ( ( x: any, s?: string ) => void ); // eslint-disable-line no-unused-vars, no-var
+declare var assertSlow: undefined | ( ( x: any, s?: string ) => void ); // eslint-disable-line no-unused-vars, no-var
+declare var sceneryLog: undefined | any; // eslint-disable-line no-unused-vars, no-var
+declare var phet: any; // eslint-disable-line no-unused-vars, no-var
 
-declare var assert: undefined | ( ( x: any, s?: string ) => void );
-declare var assertSlow: undefined | ( ( x: any, s?: string ) => void );
-declare var sceneryLog: undefined | any;
-declare var phet: any;
-
-type QSMType = {
-  getAll: ( a: any ) => any,
-  containsKey: ( key: string ) => boolean
+declare var QueryStringMachine: { // eslint-disable-line no-unused-vars, no-var
+  getAll: ( a: any ) => any;
+  containsKey: ( key: string ) => boolean;
 };
-
-declare var QueryStringMachine: QSMType;
