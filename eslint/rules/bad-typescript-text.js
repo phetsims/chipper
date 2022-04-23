8fb23e08c62aeedbbebc68dbd9a411245cd4fdb1
zepumph <michael.kauzmann@colorado.edu>
2022-04-22 18:51:45 -0600

    update doc, https://github.com/phetsims/chipper/issues/1237

--- eslint/rules/bad-typescript-text.js
+++ eslint/rules/bad-typescript-text.js
@@ -20,11 +20,12 @@ module.exports = function( context ) {
 
     // Typescript handles this for us, please refrain from providing visibility annotations via jsdoc (unless you have
     // to, disabling this rule).
+    // For @protected see "no-protected-jsdoc" rule, and https://github.com/phetsims/chipper/issues/1237#issuecomment-1105863226
     '@public',
 
     'const simOptions = {'
     // '@private',
-    // //
+    //
     // {
     //   id: '@returns with type and/or without extra doc',
     //   regex: /(@returns \{)|(@returns$)/
