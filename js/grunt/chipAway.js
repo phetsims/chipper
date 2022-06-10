71ae36e22f53f531adffc35dda067723b4e1a9b0
phet-dev <phetadmin@gmail.com>
2022-06-10 03:33:44 -0600

    update copyright dates from daily grunt work

--- js/grunt/chipAway.js
+++ js/grunt/chipAway.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2022, University of Colorado Boulder
+// Copyright 2022, University of Colorado Boulder
 
 /**
  * Produces an assignment list of responsible devs. Run from lint.js
