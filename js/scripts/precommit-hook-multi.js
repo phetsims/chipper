f06c43d8662e5f356ec6b836bbef4072d95276fb
Sam Reid <reids@colorado.edu>
2022-08-01 14:12:38 -0600

    Move precommit hook script to chipper and rename precommit-hook-changed-repos.js to precommit-hook-changed-repos.js, see https://github.com/phetsims/perennial/issues/278

---
+++ js/scripts/precommit-hook-multi.js
@@ -0,0 +1,95 @@
+// Copyright 2022, University of Colorado Boulder
+const startTime = Date.now();
+
+const execute = require( '../../../perennial-alias/js/common/execute' );
+const _ = require( 'lodash' ); // eslint-disable-line
+const fs = require( 'fs' );
+
+// constants
+// Don't use getActiveRepos() since it cannot be run from the root
+const contents = fs.readFileSync( 'perennial/data/active-repos', 'utf8' ).trim();
+const repos = contents.split( '\n' ).map( sim => sim.trim() );
+
+const args = process.argv.slice( 2 );
+const all = args.includes( '--all' );
+
+/**
+ * Identify all repos with uncommitted changes, and run the precommit hooks on them.
+ *
+ * USAGE:
+ * cd ${root containing all repos}
+ * node perennial/js/scripts/precommit-hook-changed-repos.js
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+( async () => {
+
+  let reposToTest = repos;
+
+  if ( !all ) {
+
+    // Detect uncommitted changes in each repo:
+    // https://stackoverflow.com/questions/3878624/how-do-i-programmatically-determine-if-there-are-uncommitted-changes
+    // git update-index --refresh
+    // git diff-index --quiet HEAD --
+
+    const updateIndexPromises = repos.map( repo => execute( 'git', 'update-index --refresh'.split( ' ' ), `${repo}`, {
+
+      // resolve errors so Promise.all doesn't fail on first repo that cannot pull/rebase
+      errors: 'resolve'
+    } ) );
+    await Promise.all( updateIndexPromises );
+
+    const diffIndexPromises = repos.map( repo => execute( 'git', 'diff-index --quiet HEAD --'.split( ' ' ), `${repo}`, {
+
+      // resolve errors so Promise.all doesn't fail on first repo that cannot pull/rebase
+      errors: 'resolve'
+    } ) );
+    const results = await Promise.all( diffIndexPromises );
+
+    // Find out which repos have uncommitted changes
+    const changedRepos = [];
+    for ( let i = 0; i < results.length; i++ ) {
+      const repo = repos[ i ];
+      const result = results[ i ];
+
+      if ( result.code === 0 ) {
+
+        // was up-to-date
+      }
+      else {
+
+        // needs to push
+        changedRepos.push( repo );
+      }
+    }
+    console.log( 'detected changed repos: ' + changedRepos.join( ', ' ) );
+
+    reposToTest = changedRepos;
+  }
+
+  // This is done sequentially so we don't spawn a bunch of uncached tsc at once, but in the future we may want to optimize
+  // to run one sequentially then the rest in parallel
+  for ( let i = 0; i < reposToTest.length; i++ ) {
+
+    process.stdout.write( reposToTest[ i ] + ': ' );
+
+    const result = await execute( 'node', [ '../chipper/js/scripts/hook-pre-commit.js' ], `${reposToTest[ i ]}`, {
+
+      // resolve errors so Promise.all doesn't fail on first repo that cannot pull/rebase
+      errors: 'resolve'
+    } );
+    if ( result.code === 0 ) {
+
+      console.log( 'Success' );
+    }
+    else {
+      console.log();
+      result.stdout.trim().length > 0 && console.log( result.stdout.trim() );
+      result.stderr.trim().length > 0 && console.log( result.stderr.trim() );
+    }
+  }
+
+  const endTime = Date.now();
+  console.log( 'Done in ' + ( endTime - startTime ) + 'ms' );
+} )();
