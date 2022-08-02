c3573e668d9b5b2c7f3f570dbe252052dc28af90
phet-dev <phetadmin@gmail.com>
2022-08-02 04:25:57 -0600

    update copyright dates from daily grunt work

--- js/scripts/hook-pre-commit.js
+++ js/scripts/hook-pre-commit.js
@@ -1,4 +1,4 @@
-// Copyright 2020, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 /**
  * Runs tasks for pre-commit, including lint and qunit testing.  Avoids the overhead of grunt and Gruntfile.js for speed
