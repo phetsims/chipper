55f01f582ff42427c5b2f12a070edbe0c47f0706
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-12 21:39:47 -0600

    Adding ROUGH DRAFT locale switcher for development purposes, see https://github.com/phetsims/chipper/issues/1302

--- js/grunt/getStringMap.js
+++ js/grunt/getStringMap.js
@@ -174,7 +174,7 @@ module.exports = function( mainRepo, locales, phetLibs, usedModules ) {
         // this must support JS code and minified TypeScript code
         const matches = fileContent.match( new RegExp( `${prefix}(\\.[a-zA-Z_$][a-zA-Z0-9_$]*|\\[\\s*['"][^'"]+['"]\\s*\\])+[^\\.\\[]`, 'g' ) );
         if ( matches ) {
-          stringAccesses.push( ...matches.map( match => match.slice( 0, match.length - 1 ) ).filter( m => m !== `${prefix}.get` ) );
+          stringAccesses.push( ...matches.map( match => match.slice( 0, match.length - 1 ).replace( /Property$/, '' ) ).filter( m => m !== `${prefix}.get` ) );
         }
 
         // Look for backup matches, e.g. `joistStrings.get( 'fullString' )`
