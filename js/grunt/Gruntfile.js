ca7595f0836d7626b8535e86c29837c33135bb69
Sam Reid <reids@colorado.edu>
2022-09-26 21:18:35 -0600

    Add npm install step, see https://github.com/phetsims/chipper/issues/1308

--- js/grunt/Gruntfile.js
+++ js/grunt/Gruntfile.js
@@ -432,6 +432,7 @@ Updates the normal automatically-generated files for this repository. Includes:
 
   // This is not run in grunt update because it affects dependencies and outputs files outside of the repo.
   // Run on all repos via:
+  // for-each.sh perennial/data/active-repos npm install
   // for-each.sh perennial/data/active-repos grunt generate-development-strings
   grunt.registerTask( 'generate-development-strings',
     wrapTask( async () => {
