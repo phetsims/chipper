c01b32007a9f96f92e9f523f07eb69b281e12c67
Jonathan Olson <jonathan.olson@colorado.edu>
2022-11-23 18:00:58 -0700

    Resolve after cleanup, see https://github.com/phetsims/perennial/issues/268

--- js/phet-io/generatePhetioMacroAPI.js
+++ js/phet-io/generatePhetioMacroAPI.js
@@ -112,6 +112,7 @@ const generatePhetioMacroAPI = async ( repos, options ) => {
 
             // page.close() (in cleanup) will potentially trigger a pageerror race condition, so guard on that with a flag.
             resolved = true;
+            await cleanup();
             resolve( {
 
               // to keep track of which repo this is for
@@ -120,7 +121,6 @@ const generatePhetioMacroAPI = async ( repos, options ) => {
               // For machine readability
               api: JSON.parse( fullAPI )
             } );
-            await cleanup();
           }
 
           else if ( msg.type() === 'error' ) {
