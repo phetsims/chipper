711145ca1c564ab46ef38210f7601ef3e28cb115
Jonathan Olson <jonathan.olson@colorado.edu>
2022-11-23 18:20:00 -0700

    Resolve after cleanup, see https://github.com/phetsims/perennial/issues/268

--- js/phet-io/generatePhetioMacroAPI.js
+++ js/phet-io/generatePhetioMacroAPI.js
@@ -93,6 +93,7 @@ const generatePhetioMacroAPI = async ( repos, options ) => {
 
             const fullAPI = messageText;
             clearTimeout( id );
+            await page.close();
             resolve( {
 
               // to keep track of which repo this is for
@@ -101,7 +102,6 @@ const generatePhetioMacroAPI = async ( repos, options ) => {
               // For machine readability
               api: JSON.parse( fullAPI )
             } );
-            await page.close();
           }
 
           else if ( msg.type() === 'error' ) {
