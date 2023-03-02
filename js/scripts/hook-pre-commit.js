43316d5f9e377d256fcf15ce5f9606bb20f78fbc
Jesse <jesse.greenberg@colorado.edu>
2023-03-02 11:13:10 -0500

    Revert "Revert "Revert "Only run precommit hooks when master branch is checked out, see https://github.com/phetsims/perennial/issues/276"""
    This reverts commit 94338c7fd7a793f2d00397681bd19cd9b85b219b.

--- js/scripts/hook-pre-commit.js
+++ js/scripts/hook-pre-commit.js
@@ -29,64 +29,57 @@ const phetTimingLog = require( '../../../perennial-alias/js/common/phetTimingLog
 
   // Identify the current repo
   const repo = process.cwd().split( path.sep ).pop();
-  const branch = ( await execute( 'git', [ 'branch', '--show-current' ], `../${repo}` ) ).trim();
 
-  if ( branch === 'master' ) {
+  const precommitSuccess = await phetTimingLog.startAsync( `hook-pre-commit repo="${repo}"`, async () => {
 
-    const precommitSuccess = await phetTimingLog.startAsync( `hook-pre-commit repo="${repo}"`, async () => {
+    // Console logging via --console
+    const commandLineArguments = process.argv.slice( 2 );
+    const outputToConsole = commandLineArguments.includes( '--console' );
 
-      // Console logging via --console
-      const commandLineArguments = process.argv.slice( 2 );
-      const outputToConsole = commandLineArguments.includes( '--console' );
-
-      const promises = [ 'lint', 'report-media', 'tsc', 'qunit', 'phet-io-api-compare' ].map( task => {
-        return phetTimingLog.startAsync( task, async () => {
-          const results = await execute( 'node', [
-            '../chipper/js/scripts/hook-pre-commit-task.js',
-            `--command=${task}`,
-            `--repo=${repo}`,
-            outputToConsole ? '--console' : '' ], '../chipper', {
-            errors: 'resolve'
-          } );
-          results.stdout && results.stdout.trim().length > 0 && console.log( results.stdout );
-          results.stderr && results.stderr.trim().length > 0 && console.log( results.stderr );
-
-          if ( results.code === 0 ) {
-            return 0;
-          }
-          else {
-            let message = 'Task failed: ' + task;
-            if ( results.stdout && results.stdout.trim().length > 0 ) {
-              message = message + ', ' + results.stdout;
-            }
-            if ( results.stderr && results.stderr.trim().length > 0 ) {
-              message = message + ', ' + results.stderr;
-            }
-            throw new Error( message );
-          }
-        }, {
-          depth: 1
+    const promises = [ 'lint', 'report-media', 'tsc', 'qunit', 'phet-io-api-compare' ].map( task => {
+      return phetTimingLog.startAsync( task, async () => {
+        const results = await execute( 'node', [
+          '../chipper/js/scripts/hook-pre-commit-task.js',
+          `--command=${task}`,
+          `--repo=${repo}`,
+          outputToConsole ? '--console' : '' ], '../chipper', {
+          errors: 'resolve'
         } );
+        results.stdout && results.stdout.trim().length > 0 && console.log( results.stdout );
+        results.stderr && results.stderr.trim().length > 0 && console.log( results.stderr );
+
+        if ( results.code === 0 ) {
+          return 0;
+        }
+        else {
+          let message = 'Task failed: ' + task;
+          if ( results.stdout && results.stdout.trim().length > 0 ) {
+            message = message + ', ' + results.stdout;
+          }
+          if ( results.stderr && results.stderr.trim().length > 0 ) {
+            message = message + ', ' + results.stderr;
+          }
+          throw new Error( message );
+        }
+      }, {
+        depth: 1
       } );
-
-      try {
-        await Promise.all( promises );
-        console.log( 'All tasks succeeded' );
-        return true;
-      }
-      catch( e ) {
-
-        // Exit as soon as any one promise fails
-        // Each task is responsible for outputting its error to the console, so the console should already
-        // be showing the error by now
-        return false;
-      }
     } );
 
-    // generatePhetioMacroAPI is preventing exit for unknown reasons, so manually exit here
-    phetTimingLog.close( () => process.exit( precommitSuccess ? 0 : 1 ) );
-  }
-  else {
-    console.log( 'Skipping pre-commit hook because not on master branch. Detected branch: ' + ( branch.length === 0 ? '(none)' : branch ) );
-  }
+    try {
+      await Promise.all( promises );
+      console.log( 'All tasks succeeded' );
+      return true;
+    }
+    catch( e ) {
+
+      // Exit as soon as any one promise fails
+      // Each task is responsible for outputting its error to the console, so the console should already
+      // be showing the error by now
+      return false;
+    }
+  } );
+
+  // generatePhetioMacroAPI is preventing exit for unknown reasons, so manually exit here
+  phetTimingLog.close( () => process.exit( precommitSuccess ? 0 : 1 ) );
 } )();
