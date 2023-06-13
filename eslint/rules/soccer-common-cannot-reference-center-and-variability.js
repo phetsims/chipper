4d484f79aee4c836f6290ea055511448ad78effc
Sam Reid <reids@colorado.edu>
2023-06-12 21:56:36 -0600

    Add lint rule to prevent usage of CAV code in soccer-common directory, see https://github.com/phetsims/center-and-variability/issues/222

---
+++ eslint/rules/soccer-common-cannot-reference-center-and-variability.js
@@ -0,0 +1,35 @@
+// Copyright 2023, University of Colorado Boulder
+module.exports = {
+  create( context ) {
+    const filename = context.getFilename();
+
+    // Array of forbidden directories and files
+    const forbiddenImports = [
+      '../common/',
+      '../mean-and-median/',
+      '../median/',
+      '../variability/',
+      '../center-and-variability-main.js',
+      '../center-and-variability-phet-io-overrides.js',
+      '../center-and-variability-tests.js',
+      '../centerAndVariability.js',
+      '../CenterAndVariabilityStrings.js'
+    ];
+
+    if ( filename.includes( '/soccer-common/' ) ) {
+      return {
+        ImportDeclaration( node ) {
+          const importPath = node.source.value;
+          console.log( importPath );
+          if ( forbiddenImports.some( forbiddenPath => importPath.includes( forbiddenPath ) ) ) {
+            context.report( {
+              node: node,
+              message: 'Files in \'soccer-common\' directory should not import from the forbidden directories or files.'
+            } );
+          }
+        }
+      };
+    }
+    return {};
+  }
+};
