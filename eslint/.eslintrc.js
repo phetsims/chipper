1ce14910bc4b7c55831a8d07cd1d362d94a150aa
Jonathan Olson <jonathan.olson@colorado.edu>
2023-06-28 12:04:43 -0600

    Blit shader for when we can't output directly to the Canvas texture

--- eslint/.eslintrc.js
+++ eslint/.eslintrc.js
@@ -1602,6 +1602,8 @@ module.exports = {
     GPUTexture: 'readonly',
     GPUCanvasContext: 'readonly',
     GPUTextureFormat: 'readonly',
-    GPUImageCopyExternalImageSource: 'readonly'
+    GPUImageCopyExternalImageSource: 'readonly',
+    GPUPipelineLayout: 'readonly',
+    GPURenderPipeline: 'readonly'
   }
 };
