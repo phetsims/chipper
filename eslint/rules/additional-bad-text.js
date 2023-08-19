60e6097c6b2046c80833087d6768a83f7d3d7c84
Sam Reid <reids@colorado.edu>
2023-08-18 18:23:06 -0600

    Add lint rule additional-bad-text.js, see https://github.com/phetsims/center-and-variability/issues/463

---
+++ eslint/rules/additional-bad-text.js
@@ -0,0 +1,59 @@
+// Copyright 2019-2022, University of Colorado Boulder
+
+/**
+ * Lint detector for invalid text, where repo-specific text can be identified. We were unable to find a way to combine
+ * this with bad-sim-text, so we just left it separate. Only supports string literals (not predicates or regex).
+ *
+ *  @author Sam Reid (PhET Interactive Simulations)
+ */
+// Sample usage:
+// "eslintConfig": {
+//   "extends": "../chipper/eslint/sim_eslintrc.js",
+//     "overrides": [
+//     {
+//       "files": [
+//         "**/*.ts"
+//       ],
+//       "rules": {
+//         "additional-bad-text": [
+//           "error",
+//           {
+//             "forbiddenTextObjects": [
+//               "dispose"
+//             ]
+//           }
+//         ]
+//       }
+//     }
+//   ]
+// }
+
+module.exports = {
+  meta: {
+    type: 'problem',
+    docs: {
+      description: 'Lint detector for invalid text.'
+    },
+    schema: [
+      {
+        type: 'object',
+        properties: {
+          forbiddenTextObjects: {
+            type: 'array',
+            items: {
+              type: 'string'
+            }
+          }
+        }
+      }
+    ]
+  },
+  create: function( context ) {
+    const getBadTextTester = require( './getBadTextTester' );
+    const options = context.options[ 0 ];
+
+    return {
+      Program: getBadTextTester( 'bad-sim-text', options.forbiddenTextObjects, context )
+    };
+  }
+};
