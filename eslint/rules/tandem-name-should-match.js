1ad6520ba5c3faaf8e16d92b372e84ecb083c1ee
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-09-05 12:40:14 -0600

    disallow the lint rule from `options.myVar =` declarations, add TODOs and doc, https://github.com/phetsims/phet-io/issues/1947

--- eslint/rules/tandem-name-should-match.js
+++ eslint/rules/tandem-name-should-match.js
@@ -1,6 +1,8 @@
 // Copyright 2023, University of Colorado Boulder
 
 /**
+ * tandem-name-should-match
+ *
  * This module exports a rule for ESLint. The rule checks that when calling the createTandem method,
  * the argument passed to createTandem should match the name of the variable or property it's being assigned to.
  *
@@ -8,24 +10,24 @@
  * checking the value passed to the createTandem method and comparing it with the name of the variable or property.
  * If the names do not match, it reports an error.
  *
+ * Certain behaviors of this rule that may seem confusing in certain cases are labelled inline with `QUIRK:`
+ *
+ * Cases that are not supported by this rule:
+ *
+ * 1. New directives onto options or providedOptions properties:
+ * options.titleNode = new RichText( . . . )
+ *
+ * 2. Templated variables:
+ * tandem.createTandem( 'myArray${index}` )
+ *
+ * // TODO: support this case, https://github.com/phetsims/phet-io/issues/1947
+ * 3. createTandem as an argument instead of in options:
+ * const x = new Something( tandem.createTandem( 'notX' ) )
+ *
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
-const upperSnakeToCamel = str => {
-  return str.toLowerCase().replace( /(_\w)/g, m => m[ 1 ].toUpperCase() );
-};
-
-/**
- * Allow cases like MY_TANDEM matching myTandem.
- */
-const matchesNamingConvention = ( tandemName, variableName ) => {
-  const variableNameCamel = upperSnakeToCamel( variableName );
-
-  return ( tandemName === variableNameCamel ) ||
-         ( tandemName === variableName ) ||
-         ( tandemName === 'tandemName' ) ||
-         ( variableName === '_' + tandemName );
-};
+const _ = require( 'lodash' );
 
 module.exports = {
   create( context ) {
@@ -34,14 +36,24 @@ module.exports = {
         const { declarations } = node;
 
         declarations.forEach( declaration => {
-          const variableName = declaration.id.name;
-          const tandemName = getCreateTandemCallArgument( declaration.init );
+          let variableName = declaration.id.name;
 
-          if ( tandemName && !matchesNamingConvention( tandemName, variableName ) ) {
-            context.report( {
-              node: node,
-              message: `The variable name "${variableName}" does not match the argument passed to createTandem "${tandemName}"`
-            } );
+          if ( variableName ) {
+
+            // QUIRK: If creating an options object outside the new operator, don't require it to be `myTypeOptions`,
+            // but instead `myType`
+            if ( variableName.endsWith( 'Options' ) ) {
+              variableName = variableName.replace( 'Options', '' );
+            }
+
+            const tandemName = getCreateTandemCallArgument( declaration.init );
+
+            if ( tandemName && !matchesNamingConvention( tandemName, variableName ) ) {
+              context.report( {
+                node: node,
+                message: `The variable name "${variableName}" does not match the argument passed to createTandem "${tandemName}"`
+              } );
+            }
           }
         } );
       },
@@ -49,86 +61,111 @@ module.exports = {
       AssignmentExpression( node ) {
         if ( node.left.type === 'MemberExpression' ) {
           const propertyName = node.left.property.name;
-          const isThisExpression = node.left.object.type === 'ThisExpression';
+          const isOptionsObject = node.left.object.name && ( node.left.object.name === 'options' || node.left.object.name.endsWith( 'Options' ) );
 
-          if ( propertyName ) {
+          if ( propertyName && !isOptionsObject ) {
 
             // If the property is a part of 'this', we remove 'this.' from the property name
-            const variableName = isThisExpression ? propertyName : propertyName.replace( 'this.', '' );
             const tandemName = getCreateTandemCallArgument( node.right );
 
-            if ( tandemName && !matchesNamingConvention( tandemName, variableName ) ) {
+            if ( tandemName && !matchesNamingConvention( tandemName, propertyName ) ) {
               context.report( {
                 node: node,
-                message: `The property name "${variableName}" does not match the argument passed to createTandem "${tandemName}"`
+                message: `The property name "${propertyName}" does not match the argument passed to createTandem "${tandemName}"`
               } );
             }
           }
         }
       }
     };
+  }
+};
 
-    function getFullCallerName( memberExpressionNode ) {
-      if ( memberExpressionNode.type === 'Identifier' ) {
-        return memberExpressionNode.name;
-      }
-      else if ( memberExpressionNode.type === 'MemberExpression' ) {
-        return getFullCallerName( memberExpressionNode.object ) + '.' + memberExpressionNode.property.name;
-      }
-      return '';
-    }
+/**
+ * QUIRK: We map certain spellings into camel case for comparison.
+ * For example, allow cases like MY_TANDEM or _myTandem matching myTandem.
+ */
+const matchesNamingConvention = ( tandemName, variableName ) => {
+  const variableNameCamel = _.camelCase( variableName );
 
-    /**
-     * This function analyzes the given node and retrieves the argument passed to the createTandem method.
-     * It expects the node to be either a NewExpression or a CallExpression.
-     * If it finds a 'tandem' property, it checks the callee to see if it's a call to createTandem,
-     * then retrieves and returns the first argument of the call.
-     */
-    function getCreateTandemCallArgument( node ) {
-      if ( node && ( node.type === 'NewExpression' || node.type === 'CallExpression' ) ) {
-        const { arguments: args } = node;
-        const lastArgument = args[ args.length - 1 ];
+  return ( tandemName === variableNameCamel ) ||
+         ( tandemName === variableName ) ||
+         ( tandemName === 'tandemName' ) || // TODO: this is very strange, https://github.com/phetsims/phet-io/issues/1947
+         ( '_' + tandemName === variableName );
+};
 
-        if ( lastArgument && lastArgument.type === 'ObjectExpression' ) {
-          const tandemProperty = lastArgument.properties.find(
-            prop => prop.key.name === 'tandem'
-          );
 
-          if ( tandemProperty ) {
-            const createTandemCall = tandemProperty.value;
+/**
+ *
+ * @param memberExpressionNode - an AST node
+ * @returns {string}
+ */
+function getFullCallerName( memberExpressionNode ) {
+  if ( memberExpressionNode.type === 'Identifier' ) {
+    return memberExpressionNode.name;
+  }
+  else if ( memberExpressionNode.type === 'MemberExpression' ) {
+    return getFullCallerName( memberExpressionNode.object ) + '.' + memberExpressionNode.property.name;
+  }
+  return '';
+}
 
-            if (
-              createTandemCall &&
-              createTandemCall.callee && // We are now accessing the callee of the call
-              createTandemCall.callee.property &&
-              createTandemCall.callee.property.name === 'createTandem' &&
-              createTandemCall.arguments && // Check if arguments array exists
-              createTandemCall.arguments.length > 0 // Check if the array is not empty
-            ) {
+/**
+ * This function analyzes the given node and retrieves the argument passed to the createTandem method.
+ * It expects the node to be either a NewExpression or a CallExpression.
+ * If it finds a 'tandem' property, it checks the callee to see if it's a call to createTandem,
+ * then retrieves and returns the first argument of the call.
+ * @returns {string|null}
+ */
+function getCreateTandemCallArgument( node ) {
+  if ( node && ( node.type === 'NewExpression' || node.type === 'CallExpression' ) ) {
+    const { arguments: args } = node;
+    const lastArgument = args[ args.length - 1 ];
 
-              const callee = getFullCallerName( createTandemCall.callee.object );
+    // An object literal as a parameter to the `new Type()` or `myFunction()` call
+    if ( lastArgument && lastArgument.type === 'ObjectExpression' ) {
 
-              // If the tandem is something like: myAccordionBoxTandem.createTandem('theChild') then the const variable may have
-              // a name like myAccordionBoxChild, so we need to remove the 'Tandem' part from the name before checking.
-              if ( callee === 'tandem' || callee === 'options.tandem' || callee === 'providedOptions.tandem' ) {
+      // Find if "tandem" is the name of a key in the object
+      const tandemProperty = lastArgument.properties.find(
+        prop => prop.key.name === 'tandem'
+      );
 
-                const argument = createTandemCall.arguments[ 0 ];
+      if ( tandemProperty ) {
+        const createTandemCall = tandemProperty.value;
 
-                switch( argument.type ) {
-                  case 'Literal':
-                    return argument.value;
-                  case 'Identifier':
-                    return argument.name;
-                  default:
-                    return null;
-                }
-              }
+        if (
+          createTandemCall &&
+          createTandemCall.callee && // We are now accessing the callee of the call
+          createTandemCall.callee.property &&
+          createTandemCall.callee.property.name === 'createTandem' &&
+          createTandemCall.arguments && // Check if arguments array exists
+          createTandemCall.arguments.length > 0 // Check if the array is not empty
+        ) {
+
+          const callee = getFullCallerName( createTandemCall.callee.object );
+
+          // QUIRK: If the tandem is something like: myAccordionBoxTandem.createTandem('theChild') then the const variable may have
+          // a name like myAccordionBoxChild, so we need to remove the 'Tandem' part from the name before checking.
+          if ( callee === 'tandem' || callee === 'options.tandem' || callee === 'providedOptions.tandem' ) {
+
+            const argument = createTandemCall.arguments[ 0 ];
+
+            // createTandem( 'myString' ) <-- "Literal"
+            // createTandem( myVar ) <-- "Identifier"
+            // createTandem( `my${template}Var` ) <-- "TemplateElement"
+            switch( argument.type ) {
+              case 'Literal':
+                return argument.value;
+              case 'Identifier':
+                return argument.name;
+              default:
+                return null;
             }
           }
         }
       }
-
-      return null;
     }
   }
-};
+
+  return null;
+}
