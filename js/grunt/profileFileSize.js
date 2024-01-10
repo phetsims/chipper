3af8b686e5f7152d553e2f7dcb227b3e568f29d8
Jonathan Olson <jonathan.olson@colorado.edu>
2024-01-10 10:50:17 -0700

    Improved report for `grunt profile-file-size`

--- js/grunt/profileFileSize.js
+++ js/grunt/profileFileSize.js
@@ -3,6 +3,11 @@
 /**
  * Analyzes the file size of a built file (that has been built with --profileFileSize), and prints out the results.
  *
+ * To profile a sim, go to the sim directory and run:
+ *
+ * grunt --allHTML --locales=* --brands=phet --profileFileSize
+ * grunt profile-file-size
+ *
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
 
@@ -15,9 +20,38 @@ const _ = require( 'lodash' );
  * @param {string} repo
  */
 module.exports = async function( repo ) {
-  const file = fs.readFileSync( `../${repo}/build/phet/${repo}_en_phet.html`, 'utf-8' );
+  const file = fs.readFileSync( `../${repo}/build/phet/${repo}_all_phet.html`, 'utf-8' );
 
-  console.log( parseToSections( file ).toReportString( true ) );
+  const rootSection = parseToSections( file );
+
+  const size = rootSection.getSize();
+  const gzippedSize = rootSection.getGzippedSize();
+
+  const printString = ( name, string ) => {
+    console.log( `${name}: ${getSizeString( string, size, gzippedSize )}` );
+  };
+
+  const printFilter = ( name, filter ) => {
+    printString( name, rootSection.getApproxFilteredString( filter ) );
+  };
+
+  console.log( 'summary:\n' );
+  printString( 'TOTAL', rootSection.getApproxString() );
+  console.log( '' );
+  printString( 'images', rootSection.getApproxImagesString() );
+  printString( 'sounds', rootSection.getApproxSoundsString() );
+  printFilter( 'webpack (includes assets)', section => section.type === 'WEBPACK' );
+  printFilter( 'preload', section => section.type === 'PRELOAD' );
+  printFilter( 'strings', section => section.type === 'STRINGS' );
+  printFilter( 'license', section => section.type === 'LICENSE' );
+
+  for ( const repo of rootSection.getRepos().sort() ) {
+    printString( `js ${repo}`, rootSection.getApproxRepoString( repo ) );
+  }
+
+  console.log( '\ndetails:\n' );
+
+  console.log( rootSection.toReportString( true, size, gzippedSize ) );
 };
 
 class TagMatch {
@@ -39,13 +73,12 @@ class TaggedSection {
 
   // @public
   getSize() {
-    return Buffer.from( this.getApproxString(), 'utf-8' ).length;
+    return getUtf8Length( this.getApproxString() );
   }
 
   // @public
   getGzippedSize() {
-    const buffer = Buffer.from( this.getApproxString(), 'utf-8' );
-    return zlib.gzipSync( buffer ).length;
+    return getGzippedLength( this.getApproxString() );
   }
 
   // @public
@@ -61,15 +94,69 @@ class TaggedSection {
   }
 
   // @public
-  toReportString( sort, indent = '' ) {
+  getApproxFilteredString( filter ) {
+    if ( filter( this ) ) {
+      return this.getApproxString();
+    }
+    else {
+      return this.children.map( child => {
+        if ( typeof child === 'string' ) {
+          return '';
+        }
+        else {
+          return child.getApproxFilteredString( filter );
+        }
+      } ).join( '' );
+    }
+  }
+
+  // @public
+  getApproxRepoString( repo ) {
+    return this.getApproxFilteredString( section => section.type === 'MODULE' && section.name && section.name.includes( `chipper/dist/js/${repo}/` ) );
+  }
+
+  // @public
+  getApproxImagesString() {
+    return this.getApproxFilteredString( section => section.type === 'MODULE' && section.name && /chipper\/dist\/js\/[^/]+\/(images|mipmaps)\//.test( section.name ) );
+  }
+
+  // @public
+  getApproxSoundsString() {
+    return this.getApproxFilteredString( section => section.type === 'MODULE' && section.name && /chipper\/dist\/js\/[^/]+\/sounds\//.test( section.name ) );
+  }
+
+  // @public
+  getRepos() {
+    let repo = null;
+
+    if ( this.type === 'MODULE' && this.name && this.name.includes( 'chipper/dist/js/' ) ) {
+      const index = this.name.indexOf( 'chipper/dist/js/' ) + 'chipper/dist/js/'.length;
+      repo = this.name.slice( index ).split( '/' )[ 0 ];
+    }
+
+    return _.uniq( [
+      ...( repo ? [ repo ] : [] ),
+      ...this.children.flatMap( child => {
+        if ( typeof child === 'string' ) {
+          return [];
+        }
+        else {
+          return child.getRepos();
+        }
+      } )
+    ] );
+  }
+
+  // @public
+  toReportString( sort, size, gzippedSize, indent = '' ) {
     // TOD: sort by gzipped size?
-    const children = sort ? _.sortBy( this.children, child => -( typeof child === 'string' ? Buffer.from( child ).length : child.getSize() ) ) : this.children;
-    return `${this.getSize()} ${this.getGzippedSize()} ${indent}${this.type}${this.name ? ' ' + this.name : ''}\n${children.map( child => {
+    const children = sort ? _.sortBy( this.children, child => -( typeof child === 'string' ? getUtf8Length( child ) : child.getSize() ) ) : this.children;
+    return `${getSizeString( this.getApproxString(), size, gzippedSize )} ${indent}${this.type}${this.name ? ' ' + this.name : ''}\n${children.map( child => {
       if ( typeof child === 'string' ) {
         return '';
       }
       else {
-        return child.toReportString( sort, `${indent}  ` );
+        return child.toReportString( sort, size, gzippedSize, `${indent}  ` );
       }
     } ).join( '' )}`;
   }
@@ -84,6 +171,40 @@ const findNextMatch = ( string, startIndex ) => {
   else {
     return null;
   }
+};
+
+const getUtf8Length = string => Buffer.from( string, 'utf-8' ).length;
+const getGzippedLength = string => zlib.gzipSync( Buffer.from( string, 'utf-8' ) ).length;
+const getSizeString = ( string, size, gzippedSize ) => {
+  const ourSize = getUtf8Length( string );
+  const ourGzippedSize = getGzippedLength( string );
+
+  let sizeString = '' + ourSize;
+  let gzippedSizeString = '' + ourGzippedSize;
+
+
+  const sizePercentage = Math.round( ourSize / size * 1000 ) / 10;
+  if ( sizePercentage !== 0 ) {
+    sizeString += ` (${sizePercentage}%)`;
+  }
+
+  const gzippedSizePercentage = Math.round( ourGzippedSize / gzippedSize * 1000 ) / 10;
+  if ( gzippedSizePercentage !== 0 ) {
+    gzippedSizeString += ` (${gzippedSizePercentage}%)`;
+  }
+
+
+  const megabytes = Math.round( ourSize / 1024 / 1024 * 100 ) / 100;
+  if ( megabytes !== 0 ) {
+    sizeString += ` ${megabytes} MB`;
+  }
+
+  const gzippedMegabytes = Math.round( ourGzippedSize / 1024 / 1024 * 100 ) / 100;
+  if ( gzippedMegabytes !== 0 ) {
+    gzippedSizeString += ` ${gzippedMegabytes} MB`;
+  }
+
+  return `utf-8: ${sizeString} gzip: ${gzippedSizeString}`;
 };
 
 const parseToSections = string => {
