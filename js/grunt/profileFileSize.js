49a99430b999fa4491f7868df91b4b46b9abf563
phet-dev <phetadmin@gmail.com>
2024-01-10 03:11:07 -0700

    update copyright dates from daily grunt work

--- js/grunt/profileFileSize.js
+++ js/grunt/profileFileSize.js
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2024, University of Colorado Boulder
 
 /**
  * Analyzes the file size of a built file (that has been built with --profileFileSize), and prints out the results.
