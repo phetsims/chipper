8a06c3b63698211bb3548ffbb38461087e05fa7d
phet-dev <phetadmin@gmail.com>
2024-01-23 03:11:35 -0700

    update copyright dates from daily grunt work

--- js/phet-io/output-apis.js
+++ js/phet-io/output-apis.js
@@ -1,4 +1,4 @@
-// Copyright 2021-2022, University of Colorado Boulder
+// Copyright 2021-2024, University of Colorado Boulder
 
 const fs = require( 'fs' );
 const generatePhetioMacroAPI = require( './generatePhetioMacroAPI' );
