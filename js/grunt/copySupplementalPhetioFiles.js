c0dc01315a3c021b7bbbc226fc6207c94f7c911d
phet-dev <phetadmin@gmail.com>
2024-01-25 03:11:27 -0700

    update copyright dates from daily grunt work

--- js/grunt/copySupplementalPhetioFiles.js
+++ js/grunt/copySupplementalPhetioFiles.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2023, University of Colorado Boulder
+// Copyright 2016-2024, University of Colorado Boulder
 
 /**
  * Copies all supporting PhET-iO files, including wrappers, indices, lib files, etc.
