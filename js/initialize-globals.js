9b02f59d12ca335f2f80a7f9f29aacb9c28c3cdc
Jonathan Olson <jonathan.olson@colorado.edu>
2024-03-15 18:07:29 -0600

    Behavior when ?yotta=false, see https://github.com/phetsims/phetcommon/issues/65

--- js/initialize-globals.js
+++ js/initialize-globals.js
@@ -207,7 +207,8 @@
      */
     ga4: {
       type: 'string',
-      defaultValue: null
+      defaultValue: null,
+      public: true
     },
 
     /**
@@ -505,6 +506,15 @@
     webgl: {
       type: 'boolean',
       defaultValue: true
+    },
+
+    /**
+     * Indicates whether yotta analytics are enabled.
+     */
+    yotta: {
+      type: 'boolean',
+      defaultValue: true,
+      public: true
     }
   };
 
