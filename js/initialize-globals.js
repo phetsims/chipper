c3350befa730711a3ffa18ddeceabb654dc9ee48
Jonathan Olson <jonathan.olson@colorado.edu>
2024-03-18 13:05:47 -0600

    Removing public: true for yotta-related query parameters, see https://github.com/phetsims/phetcommon/issues/65

--- js/initialize-globals.js
+++ js/initialize-globals.js
@@ -208,8 +208,7 @@
      */
     ga4: {
       type: 'string',
-      defaultValue: null,
-      public: true
+      defaultValue: null
     },
 
     /**
@@ -561,8 +560,7 @@
      */
     yotta: {
       type: 'boolean',
-      defaultValue: true,
-      public: true
+      defaultValue: true
     }
   };
 
