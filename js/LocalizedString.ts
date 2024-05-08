99db72aebc36d23746195af69b459232fe01bfce
Jonathan Olson <jonathan.olson@colorado.edu>
2024-05-08 11:40:17 -0600

    LocalizedString reworked to use TinyProperties without the "override" tree (and the new fallback). Adjusting unbuilt loading, so we load localeData BEFORE loading translations. see https://github.com/phetsims/joist/issues/963

--- js/LocalizedString.ts
+++ js/LocalizedString.ts
@@ -7,9 +7,7 @@
  */
 
 import TinyProperty from '../../axon/js/TinyProperty.js';
-import TinyOverrideProperty from '../../axon/js/TinyOverrideProperty.js';
 import { Locale } from '../../joist/js/i18n/localeProperty.js';
-import localeOrderProperty from '../../joist/js/i18n/localeOrderProperty.js';
 import Tandem from '../../tandem/js/Tandem.js';
 import chipper from './chipper.js';
 import TProperty from '../../axon/js/TProperty.js';
@@ -17,9 +15,12 @@ import { localizedStrings } from './getStringModule.js';
 import arrayRemove from '../../phet-core/js/arrayRemove.js';
 import { PhetioID } from '../../tandem/js/TandemConstants.js';
 import LocalizedStringProperty from './LocalizedStringProperty.js';
+import fallbackLocalesProperty from '../../joist/js/i18n/fallbackLocalesProperty.js';
 
 // constants
 const FALLBACK_LOCALE = 'en';
+
+fallbackLocalesProperty.value; // TODO: waiting until we see if fallbackLocalesProperty can be removed, see https://github.com/phetsims/joist/issues/963
 
 // for readability/docs
 type TranslationString = string;
@@ -28,42 +29,31 @@ export type LocalizedStringStateDelta = Partial<Record<Locale, TranslationString
 // Where "string" is a phetioID
 export type StringsStateStateObject = { data: Record<PhetioID, LocalizedStringStateDelta> };
 
+const localeData = phet.chipper.localeData;
+assert && assert( localeData );
+
 class LocalizedString {
 
   // Public-facing IProperty<string>, used by string modules
   public readonly property: LocalizedStringProperty;
 
-  // Holds our non-Override Property at the root of everything
-  private readonly englishProperty: TinyProperty<TranslationString>;
-
   // Uses lazy creation of locales
-  private readonly localePropertyMap = new Map<Locale, TinyOverrideProperty<TranslationString>>();
-
-  private readonly localeOrderListener: ( locales: Locale[] ) => void;
+  private readonly localePropertyMap = new Map<Locale, TinyProperty<TranslationString>>();
 
   // Store initial values, so we can handle state deltas
   private readonly initialValues: LocalizedStringStateDelta = {};
 
-  public constructor( public readonly stringKey: string, englishValue: TranslationString, tandem: Tandem, metadata?: Record<string, unknown> ) {
-
-    this.englishProperty = new TinyProperty( englishValue );
-    this.initialValues[ FALLBACK_LOCALE ] = englishValue;
-
-    this.localeOrderListener = this.onLocaleOrderChange.bind( this );
-    localeOrderProperty.lazyLink( this.localeOrderListener );
-
+  public constructor(
+    public readonly stringKey: string,
+    // Store initial values, so we can handle state deltas
+    private readonly localeToTranslationMap: LocalizedStringStateDelta,
+    tandem: Tandem,
+    metadata?: Record<string, unknown>
+  ) {
     this.property = new LocalizedStringProperty( this, tandem, metadata );
 
     // Add to a global list to support PhET-iO serialization and internal testing
     localizedStrings.push( this );
-  }
-
-  /**
-   * Sets the initial value of a translated string (so that there will be no fallback for that locale/string combo)
-   */
-  public setInitialValue( locale: Locale, value: TranslationString ): void {
-    this.initialValues[ locale ] = value;
-    this.getLocaleSpecificProperty( locale ).value = value;
   }
 
   /**
@@ -74,9 +64,11 @@ class LocalizedString {
     const result: LocalizedStringStateDelta = {};
 
     this.usedLocales.forEach( locale => {
-      const rawString = this.getRawStringValue( locale );
-      if ( rawString !== null && rawString !== this.initialValues[ locale ] ) {
-        result[ locale ] = rawString;
+      const initialValue: string = this.initialValues[ locale ]!;
+      const currentValue = this.getLocaleSpecificProperty( locale ).value;
+
+      if ( currentValue !== initialValue ) {
+        result[ locale ] = currentValue;
       }
     } );
 
@@ -93,122 +85,51 @@ class LocalizedString {
 
     this.usedLocales.forEach( locale => {
       const localeSpecificProperty = this.getLocaleSpecificProperty( locale );
-      const initialValue: string | null = this.initialValues[ locale ] !== undefined ? this.initialValues[ locale ]! : null;
+      const initialValue: string = this.initialValues[ locale ]!;
+      assert && assert( initialValue !== undefined );
+
       const stateValue: string | null = state[ locale ] !== undefined ? state[ locale ]! : null;
 
-      // If not specified in the state
-      if ( stateValue === null ) {
-
-        // If we have no initial value, we'll want to set it to fall back
-        if ( initialValue === null ) {
-          ( localeSpecificProperty as TinyOverrideProperty<string> ).clearOverride();
-        }
-        else {
-          localeSpecificProperty.value = initialValue;
-        }
-      }
-      else {
-        localeSpecificProperty.value = stateValue;
-      }
+      localeSpecificProperty.value = stateValue ?? initialValue;
     } );
   }
 
-  /**
-   * Returns the specific translation for a locale (no fallbacks), or null if that string is not translated in the
-   * exact locale
-   */
-  private getRawStringValue( locale: Locale ): string | null {
-    const property = this.getLocaleSpecificProperty( locale );
-    if ( property instanceof TinyOverrideProperty ) {
-      return property.isOverridden ? property.value : null;
-    }
-    else {
-      // english
-      return property.value;
-    }
-  }
-
   private get usedLocales(): Locale[] {
-    // NOTE: order matters, we want the fallback to be first so that in onLocaleOrderChange we don't run into infinite
-    // loops.
-    return [ FALLBACK_LOCALE, ...this.localePropertyMap.keys() ];
-  }
-
-  private onLocaleOrderChange( localeOrder: Locale[] ): void {
-
-    // Do this in reverse order to AVOID infinite loops.
-    // For example, if localeOrder1=ar,es localeOrder2=es,ar) then we could run into the case temporarily where the
-    // TinyOverrideProperty for ar has its target as es, and the TinyOverrideProperty for es has its target as ar.
-    // This would then trigger an infinite loop if you try to read the value of either of them, as it would ping
-    // back-and-forth.
-    const locales: Locale[] = [
-      ...this.usedLocales,
-
-      // Yes, this duplicates some, but it should be a no-op and saves code length
-      ...localeOrder
-    ];
-    for ( let i = locales.length - 1; i >= 0; i-- ) {
-      const locale = locales[ i ];
-      const localeProperty = this.getLocaleSpecificProperty( locale );
-      if ( localeProperty instanceof TinyOverrideProperty ) {
-        localeProperty.targetProperty = this.getLocaleSpecificProperty( LocalizedString.getFallbackLocale( locale ) );
-      }
-    }
+    return [ ...this.localePropertyMap.keys() ];
   }
 
   /**
    * Returns the locale-specific Property for any locale (lazily creating it if necessary)
    */
   public getLocaleSpecificProperty( locale: Locale ): TProperty<string> {
-    if ( locale === 'en' ) {
-      return this.englishProperty;
-    }
-
     // Lazy creation
     if ( !this.localePropertyMap.has( locale ) ) {
-      this.localePropertyMap.set( locale, new TinyOverrideProperty( this.getLocaleSpecificProperty( LocalizedString.getFallbackLocale( locale ) ) ) );
+      // Locales in order of fallback
+      const orderedLocales: Locale[] = [
+        locale,
+        ...( localeData[ locale ].fallbackLocales || [] ),
+        FALLBACK_LOCALE
+      ];
+
+      // Find the first-defined value
+      let initialValue: string | null = null;
+      for ( const locale of orderedLocales ) {
+        if ( this.localeToTranslationMap[ locale ] !== undefined ) {
+          initialValue = this.localeToTranslationMap[ locale ]!;
+          break;
+        }
+      }
+      // Should be guaranteed because of `en` as a fallback
+      assert && assert( initialValue !== undefined, 'no initial value found for', locale );
+
+      this.initialValues[ locale ] = initialValue!;
+      this.localePropertyMap.set( locale, new TinyProperty( initialValue! ) );
     }
 
     return this.localePropertyMap.get( locale )!;
   }
 
-  /**
-   * What should be the next-most fallback locale for a given locale. Our global localeOrder is used, and otherwise it
-   * defaults to our normal fallback mechanism.
-   */
-  public static getFallbackLocale( locale: Locale ): Locale {
-    if ( locale === 'en' ) {
-      return 'en'; // can be its own fallback
-    }
-
-    const localeOrder = localeOrderProperty.value;
-
-    const index = localeOrder.indexOf( locale );
-    if ( index >= 0 ) {
-      assert && assert( localeOrder[ localeOrder.length - 1 ] === 'en' );
-      assert && assert( index + 1 < localeOrder.length );
-      return localeOrder[ index + 1 ];
-    }
-    else {
-      if ( phet.chipper.localeData[ locale ] ) {
-        // Pick either the first fallback locale from our localeData, or English
-        return ( phet.chipper.localeData[ locale ].fallbackLocales || [ 'en' ] )[ 0 ];
-      }
-      else {
-        // doesn't exist in those
-        if ( locale.includes( '_' ) ) {
-          return locale.slice( 0, 2 ) as Locale; // zh_CN => zh
-        }
-        else {
-          return 'en';
-        }
-      }
-    }
-  }
-
   public dispose(): void {
-    localeOrderProperty.unlink( this.localeOrderListener );
-
     this.property.dispose();
     arrayRemove( localizedStrings, this );
   }
--- js/load-unbuilt-strings.js
+++ js/load-unbuilt-strings.js
@@ -30,15 +30,9 @@
   window.phet.chipper.strings = {};
   window.phet.chipper.stringMetadata = {};
 
-  // Prefixes, ideally a better way of accessing localeData on startup would exist. We have localeData, however it's
-  // in the form of a module, and we can't use that at this point.
-  // NOTE: For built forms, we will always have this data. For unbuilt forms, we may conditionally have this data,
-  // depending on the load order
-  const rtlLocales = phet.chipper.localeData ? Object.keys( phet.chipper.localeData ).filter( locale => {
-    return phet.chipper.localeData[ locale ].direction === 'rtl';
-  } ) : [ 'ac', 'ar', 'ar_AE', 'ar_BH', 'ar_DJ', 'ar_DZ', 'ar_EG', 'ar_EH', 'ar_ER', 'ar_IQ', 'ar_JO', 'ar_KM', 'ar_KW', 'ar_LB', 'ar_LY', 'ar_MA', 'ar_MR', 'ar_OM', 'ar_QA', 'ar_SA', 'ar_SD', 'ar_SO', 'ar_SY', 'ar_TD', 'ar_TN', 'ar_YE', 'bc', 'bx', 'de_AT', 'de_CH', 'de_LI', 'de_LU', 'di', 'es_AR', 'es_BO', 'es_CL', 'es_DO', 'es_EC', 'es_GQ', 'es_GT', 'es_HN', 'es_NI', 'es_PA', 'es_PR', 'es_PY', 'es_SV', 'es_US', 'es_UY', 'es_VE', 'fa', 'fa_DA', 'fr_BE', 'fr_BF', 'fr_BI', 'fr_BJ', 'fr_CA', 'fr_CD', 'fr_CF', 'fr_CG', 'fr_CH', 'fr_CI', 'fr_CM', 'fr_DJ', 'fr_EH', 'fr_GA', 'fr_GN', 'fr_GQ', 'fr_KM', 'fr_LU', 'fr_MC', 'fr_MG', 'fr_ML', 'fr_NE', 'fr_RW', 'fr_SC', 'fr_SN', 'fr_TD', 'fr_TG', 'it_CH', 'iw', 'jr', 'kq', 'lh', 'ly', 'ms_MY', 'nl_BE', 'nq', 'sr_BA', 'sv_FI', 'sy', 'ur', 'zh_MO', 'zh_SG' ];
+  // Will be initialized after we have loaded localeData (below)
+  let rtlLocales;
 
-  const localeQueryParam = new window.URLSearchParams( window.location.search ).get( 'locale' );
   const localesQueryParam = new window.URLSearchParams( window.location.search ).get( 'locales' );
 
   let remainingFilesToProcess = 0;
@@ -187,34 +181,6 @@
     window.phet.chipper.loadModules();
   };
 
-  let locales = [ FALLBACK_LOCALE ];
-
-  if ( localesQueryParam === '*' ) {
-    locales = 'aa,ab,ae,af,ak,am,an,ar,ar_MA,ar_SA,as,av,ay,az,ba,be,bg,bh,bi,bm,bn,bo,br,bs,ca,ce,ch,co,cr,cs,cu,cv,cy,da,de,dv,dz,ee,el,en,en_CA,en_GB,eo,es,es_CO,es_CR,es_ES,es_MX,es_PE,et,eu,fa,ff,fi,fj,fo,fr,fu,fy,ga,gd,gl,gn,gu,gv,ha,hi,ho,hr,ht,hu,hy,hz,ia,ie,ig,ii,ik,in,io,is,it,iu,iw,ja,ji,jv,ka,kg,ki,kj,kk,kl,km,kn,ko,kr,ks,ku,ku_TR,kv,kw,ky,la,lb,lg,li,lk,ln,lo,lt,lu,lv,mg,mh,mi,mk,ml,mn,mo,mr,ms,mt,my,na,nb,nd,ne,ng,nl,nn,nr,nv,ny,oc,oj,om,or,os,pa,pi,pl,ps,pt,pt_BR,qu,rm,rn,ro,ru,rw,ry,sa,sc,sd,se,sg,sh,si,sk,sl,sm,sn,so,sq,sr,ss,st,su,sv,sw,ta,te,tg,th,ti,tk,tl,tn,to,tr,ts,tt,tw,ty,ug,uk,ur,uz,ve,vi,vo,wa,wo,xh,yo,za,zh_CN,zh_HK,zh_TW,zu'.split( ',' );
-  }
-  else {
-    // Load other locales we might potentially need (keeping out duplicates)
-    [
-      localeQueryParam,
-      ...( localesQueryParam ? localesQueryParam.split( ',' ) : [] )
-    ].forEach( locale => {
-      if ( locale ) {
-        // NOTE: this is UNABLE to follow the normal fallback mechanisms from https://github.com/phetsims/joist/issues/963,
-        // as we don't have that data loaded yet.
-
-        // e.g. 'zh_CN'
-        if ( !locales.includes( locale ) ) {
-          locales.push( locale );
-        }
-        // e.g. 'zh'
-        const shortLocale = locale.slice( 0, 2 );
-        if ( locale.length > 2 && !locales.includes( shortLocale ) ) {
-          locales.push( shortLocale );
-        }
-      }
-    } );
-  }
-
   // Check for phet.chipper.stringPath. This should be set to ADJUST the path to the strings directory, in cases
   // where we're running this case NOT from a repo's top level (e.g. sandbox.html)
   const getStringPath = ( repo, locale ) => `${phet.chipper.stringPath ? phet.chipper.stringPath : ''}../${locale === FALLBACK_LOCALE ? '' : 'babel/'}${repo}/${repo}-strings_${locale}.json`;
@@ -237,15 +203,17 @@
   // } );
 
   // Load locale data
+  remainingFilesToProcess++;
   requestJSONFile( '../babel/localeData.json', json => {
     phet.chipper.localeData = json;
 
     // Because load-unbuilt-strings' "loading" of the locale data might not have happened BEFORE initialize-globals
     // runs (and sets phet.chipper.locale), we'll attempt to handle the case where it hasn't been set yet.
     phet.chipper.checkAndRemapLocale && phet.chipper.checkAndRemapLocale();
-  } );
 
-  if ( localesQueryParam === '*' ) {
+    rtlLocales = Object.keys( phet.chipper.localeData ).filter( locale => {
+      return phet.chipper.localeData[ locale ].direction === 'rtl';
+    } );
 
     // Load the conglomerate files
     requestJSONFile( `../babel/_generated_development_strings/${ourRepo}_all.json`, json => {
@@ -273,22 +241,7 @@
         }
       } );
     } );
-  }
-  else {
 
-    // Load just the specified locales
-    locales.forEach( locale => {
-      requestJSONFile( getStringPath( ourRepo, locale ), json => {
-        processStringFile( json, ourRequirejsNamespace, locale );
-        phet.chipper.stringRepos.forEach( stringRepoData => {
-          const repo = stringRepoData.repo;
-          if ( repo !== ourRepo ) {
-            requestJSONFile( getStringPath( repo, locale ), json => {
-              processStringFile( json, stringRepoData.requirejsNamespace, locale );
-            } );
-          }
-        } );
-      } );
-    } );
-  }
+    remainingFilesToProcess--;
+  } );
 } )();
