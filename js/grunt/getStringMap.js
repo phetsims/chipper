ee01133e0037012818de207abcc6ced2fbf093bd
Jonathan Olson <jonathan.olson@colorado.edu>
2024-05-13 11:25:01 -0600

    Adding phet.chipper.localeData to built versions, see https://github.com/phetsims/joist/issues/963

--- js/grunt/getStringMap.js
+++ js/grunt/getStringMap.js
@@ -11,12 +11,45 @@
 // built-in node APIs
 const assert = require( 'assert' );
 const path = require( 'path' );
+const fs = require( 'fs' );
 
-// modules
 const ChipperConstants = require( '../common/ChipperConstants' );
 const ChipperStringUtils = require( '../common/ChipperStringUtils' );
 const grunt = require( 'grunt' );
-const localeInfo = require( '../data/localeInfo' ); // Locale information
+
+const localeData = JSON.parse( fs.readFileSync( '../babel/localeData.json', 'utf8' ) );
+
+/**
+ * For a given locale, return an array of specific locales that we'll use as fallbacks, e.g.
+ * 'ar_AE' => [ 'ar_AE', 'ar', 'ar_MA', 'en' ]   (note, changed from zh_CN example, which does NOT use 'zh' as a fallback anymore)
+ * 'es' => [ 'es', 'en' ]
+ * 'en' => [ 'en' ]
+ *
+ * @param {string} locale
+ * @returns {Array.<string>}
+ */
+const localeFallbacks = function ( locale ) {
+  const fallbackLocales = [];
+
+  // Add the locale itself
+  if ( locale !== ChipperConstants.FALLBACK_LOCALE ) {
+    fallbackLocales.push( locale );
+  }
+
+  // Add the fallback locales
+  if ( localeData[ locale ].fallbackLocales ) {
+    localeData[ locale ].fallbackLocales.forEach( function ( fallbackLocale ) {
+      if ( fallbackLocales.indexOf( fallbackLocale ) < 0 ) {
+        fallbackLocales.push( fallbackLocale );
+      }
+    } );
+  }
+
+  // Add the fallback locale
+  fallbackLocales.push( ChipperConstants.FALLBACK_LOCALE );
+
+  return fallbackLocales;
+};
 
 /**
  * @param {Array.<string>} locales
@@ -49,15 +82,24 @@ module.exports = function( locales, phetLibs ) {
     }
   }
 
+  const allLocales = [];
+  locales.forEach( function( locale ) {
+    localeFallbacks( locale ).forEach( function( fallbackLocale ) {
+      if ( allLocales.indexOf( fallbackLocale ) < 0 ) {
+        allLocales.push( fallbackLocale );
+      }
+    } );
+  } );
+
   // Load all the required string files into memory, so we don't load them multiple times (for each usage)
   const repoStringMap = {}; // maps [repositoryName][locale] => contents of locale string file
   stringRepositories.forEach( function( repository ) {
     repoStringMap[ repository.name ] = {};
 
-    locales.forEach( function( locale ) {
+    allLocales.forEach( function( locale ) {
 
-      assert( localeInfo[ locale ], `unsupported locale: ${locale}` );
-      const isRTL = localeInfo[ locale ].direction === 'rtl';
+      assert( localeData[ locale ], `unsupported locale: ${locale}` );
+      const isRTL = localeData[ locale ].direction === 'rtl';
 
       let basePath;
       // pick a location that is in the repo, or babel
@@ -105,19 +147,22 @@ module.exports = function( locales, phetLibs ) {
       // English fallback
       assert( repoStringMap[ repositoryName ][ fallbackLocale ][ stringKey ] !== undefined,
         `Missing string: ${stringKey} in ${repositoryName} for fallback locale: ${fallbackLocale}` );
-      const fallbackString = repoStringMap[ repositoryName ][ fallbackLocale ][ stringKey ].value;
-      stringMap[ locale ][ stringKey ] = fallbackString;
 
       // Extract 'value' field from non-fallback (babel) strings file, and overwrites the default if available.
-      if ( locale !== fallbackLocale &&
-           repoStringMap[ repositoryName ] &&
-           repoStringMap[ repositoryName ][ locale ] &&
-           repoStringMap[ repositoryName ][ locale ][ stringKey ] &&
-
-           // if the string in rosetta is empty we want to use the fallback english string
-           repoStringMap[ repositoryName ][ locale ][ stringKey ].value.length > 0 ) {
-        stringMap[ locale ][ stringKey ] = repoStringMap[ repositoryName ][ locale ][ stringKey ].value;
-      }
+      // NOTE: fallback locales INCLUDES 'en', so we don't need a special case for it.
+      let value = null;
+      localeFallbacks( locale ).forEach( function( candidateLocale ) {
+        if (
+          value === null &&
+          repoStringMap[ repositoryName ] &&
+          repoStringMap[ repositoryName ][ candidateLocale ] &&
+          repoStringMap[ repositoryName ][ candidateLocale ][ stringKey ] &&
+          repoStringMap[ repositoryName ][ candidateLocale ][ stringKey ].value.length > 0
+        ) {
+          value = repoStringMap[ repositoryName ][ candidateLocale ][ stringKey ].value;
+        }
+      } );
+      stringMap[ locale ][ stringKey ] = value;
     }
   } );
 
