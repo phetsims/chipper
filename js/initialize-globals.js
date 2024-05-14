640f320dff63dce9924505adde1b3b9612eeee4f
Jonathan Olson <jonathan.olson@colorado.edu>
2024-05-14 12:47:54 -0600

    Pre-ES5 fixes, see https://github.com/phetsims/joist/issues/963

--- js/initialize-globals.js
+++ js/initialize-globals.js
@@ -423,7 +423,7 @@
 
     // We will need to check for locale validity (once we have localeData loaded, if running unbuilt), and potentially
     // either fall back to `en`, or remap from 3-character locales to our locale keys.
-    phet.chipper.checkAndRemapLocale = () => {
+    phet.chipper.checkAndRemapLocale = function() {
       // We need both to proceed. Provided as a global, so we can call it from load-unbuilt-strings
       // (IF initialize-globals loads first)
       if ( !phet.chipper.localeData || !phet.chipper.locale ) {
@@ -439,14 +439,14 @@
         else {
           locale = locale.replace( /-/, '_' );
 
-          const parts = locale.split( '_' );
+          var parts = locale.split( '_' );
           if ( parts.length === 2 ) {
             locale = parts[ 0 ].toLowerCase() + '_' + parts[ 1 ].toUpperCase();
           }
         }
 
         if ( locale.length === 3 ) {
-          const potentialLocales = Object.keys( phet.chipper.localeData );
+          var potentialLocales = Object.keys( phet.chipper.localeData );
           for ( var i = 0; i < potentialLocales.length; i++ ) {
             var candidateLocale = potentialLocales[ i ];
             if ( phet.chipper.localeData[ candidateLocale ].locale3 === locale ) {
