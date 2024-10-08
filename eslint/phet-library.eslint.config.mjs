0663954626853cff9b72d0c51db79a5cd54b34c9
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-10-08 15:44:15 -0600

    minor changes, https://github.com/phetsims/chipper/issues/1451

--- eslint/phet-library.eslint.config.mjs
+++ eslint/phet-library.eslint.config.mjs
@@ -1,8 +1,7 @@
 // Copyright 2023, University of Colorado Boulder
 // @author Michael Kauzmann
 
-// eslint-disable-next-line phet/default-import-match-filename
-import simConfig from './sim.eslint.config.mjs';
+import simEslintConfig from './sim.eslint.config.mjs';
 
 /**
  * Eslint config applied only to "library" code. This means code that is potentially meant to run outside of the context
@@ -10,9 +9,10 @@ import simConfig from './sim.eslint.config.mjs';
  * joist's nav bar or preferences) to correctly run.
  */
 export default [
-  ...simConfig,
+  ...simEslintConfig,
   {
     rules: {
       'phet/bad-phet-library-text': 'error'
     }
-  } ];
+  }
+];
