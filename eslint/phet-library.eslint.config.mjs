099d0d46814014d47e25cdb8b3e64e3b093400cc
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-10-14 15:33:05 -0600

    expand browser-and-node repo linting config pattern to phet library config, https://github.com/phetsims/chipper/issues/1464

--- eslint/phet-library.eslint.config.mjs
+++ eslint/phet-library.eslint.config.mjs
@@ -2,20 +2,26 @@
 // @author Michael Kauzmann
 
 import rootEslintConfig from './root.eslint.config.mjs';
-import simEslintConfig from './sim.eslint.config.mjs';
+import { getSimConfiguration } from './sim.eslint.config.mjs';
+
+export const getPhetLibraryConfiguration = ( pattern = {} ) => {
+  return [
+    ...getSimConfiguration( pattern ),
+    {
+      rules: {
+        'phet/bad-phet-library-text': 'error'
+      },
+      ...pattern
+    }
+  ];
+};
 
 /**
  * Eslint config applied only to "library" code. This means code that is potentially meant to run outside of the context
  * of phetsims. For example, a SUN/Checkbox shouldn't require a hard dependency on the phetsim environment (for example
  * joist's nav bar or preferences) to correctly run.
  */
-
 export default [
   ...rootEslintConfig,
-  ...simEslintConfig,
-  {
-    rules: {
-      'phet/bad-phet-library-text': 'error'
-    }
-  }
+  ...getPhetLibraryConfiguration()
 ];
--- eslint/sim.eslint.config.mjs
+++ eslint/sim.eslint.config.mjs
@@ -1,25 +1,51 @@
-// Copyright 2018, University of Colorado Boulder
-// @author Michael Kauzmann
+// Copyright 2018-2024, University of Colorado Boulder
 
+import assert from 'assert';
 import { getBrowserConfiguration } from './browser.eslint.config.mjs';
 import rootEslintConfig from './root.eslint.config.mjs';
 
+// Ensure the pattern only applies to HTML files.
+const getHTMLPatterns = pattern => {
+
+  return {
+    ...pattern,
+    files: !pattern.files ? [ '*.html' ] : pattern.files.map( filePattern => {
+
+      // This is likely only going to be this horrible until we complete https://github.com/phetsims/chipper/issues/1483
+      assert( filePattern.endsWith( '/*' ), 'only limited support for files patterns right now to append "html" file suffix' );
+
+      return `${filePattern}.html`;
+    } )
+  };
+};
+
+export const getSimConfiguration = ( pattern = {} ) => {
+  const patternsForHTML = getHTMLPatterns( pattern );
+
+  return [
+    ...getBrowserConfiguration( pattern ),
+    {
+      rules: {
+        'phet/bad-sim-text': 'error'
+      },
+      ...pattern
+    },
+    {
+      // Most html files don't need to behave like sims
+      rules: {
+        'phet/bad-sim-text': 'off'
+      },
+      ...patternsForHTML
+    }
+  ];
+};
+
 /**
- * Eslint config applied only to sims.
+ * Eslint config applied only to simulations.
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ * @author Sam Reid (PhET Interactive Simulations)
  */
 export default [
   ...rootEslintConfig,
-  ...getBrowserConfiguration(),
-  {
-    rules: {
-      'phet/bad-sim-text': 'error'
-    }
-  },
-  {
-    // Most html files don't need to behave like sims
-    files: [ '*.html' ],
-    rules: {
-      'phet/bad-sim-text': 'off'
-    }
-  }
+  ...getSimConfiguration()
 ];
