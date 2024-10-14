7d4ea75ddc8f93f26da91fd9c29c8447d153c3dc
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-10-14 14:25:24 -0600

    minor formatting, https://github.com/phetsims/chipper/issues/1464

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,9 +20,7 @@ export default [
   ...rootEslintConfig,
   ...getBrowserConfiguration( { files: browserFiles } ),
   ...getNodeConfiguration( {
-    files: [
-      '**/*'
-    ],
+    files: [ '**/*' ],
     ignores: browserFiles
   } ),
   {
