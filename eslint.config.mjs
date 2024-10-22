f932885b9369d193037d44dc50a1247a5fa0edb3
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-10-22 13:52:20 -0600

    point to perennial-alias eslint config, https://github.com/phetsims/perennial/issues/364
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,9 +7,9 @@
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
 
-import { getBrowserConfiguration } from './eslint/browser.eslint.config.mjs';
-import getNodeConfiguration from './eslint/getNodeConfiguration.mjs';
-import rootEslintConfig from './eslint/root.eslint.config.mjs';
+import { getBrowserConfiguration } from '../perennial-alias/js/eslint/browser.eslint.config.mjs';
+import getNodeConfiguration from '../perennial-alias/js/eslint/getNodeConfiguration.mjs';
+import rootEslintConfig from '../perennial-alias/js/eslint/root.eslint.config.mjs';
 
 const browserFiles = [
   'js/*', // not recursive
