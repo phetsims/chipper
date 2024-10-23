adee7401dde45dde3d1f3feed9808025c79c5c31
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-10-23 15:01:30 -0600

    remove todo, https://github.com/phetsims/chipper/issues/1499

--- js/grunt/tasks/transpile.ts
+++ js/grunt/tasks/transpile.ts
@@ -5,7 +5,6 @@ import transpile, { getTranspileOptions } from '../transpile.js';
 /**
  * Main entrypoint for transpiling the PhET codebase to js. See transpile() for API. Outputs JS just
  * for the specified repo by default.
- * TODO: Rename this to "transpile", it is ok that we keep output-js-project for backwards compatibility. https://github.com/phetsims/chipper/issues/1499
  *
  * @author Sam Reid (PhET Interactive Simulations)
  */
