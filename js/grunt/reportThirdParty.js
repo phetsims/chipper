0f33a8428b1f894b3a403a20e8888b2e1c8e9dd5
phet-dev <phetadmin@gmail.com>
2024-10-29 03:17:24 -0600

    update copyright dates from daily grunt work

--- js/grunt/reportThirdParty.js
+++ js/grunt/reportThirdParty.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2023, University of Colorado Boulder
+// Copyright 2015-2024, University of Colorado Boulder
 
 /**
  * Creates a composite report of all of the 3rd party images, code, sounds and other media used by all of the published
