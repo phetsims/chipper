2151d71a78067387189f057ce3cbb14111a9b4ff
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-11-07 16:49:48 -0700

    inline module, https://github.com/phetsims/perennial/issues/370

--- js/scripts/commits-since.ts
+++ js/scripts/commits-since.ts
@@ -1,22 +1,39 @@
 // Copyright 2013-2024, University of Colorado Boulder
 
 /**
- * Shows commits since a specified date. Use --date=<date> to specify the date.
- * TODO: move to chipper/js/scripts/ https://github.com/phetsims/perennial/issues/370
- * TODO: inline the module into this script https://github.com/phetsims/perennial/issues/370
- * @author Sam Reid (PhET Interactive Simulations)
+ * Prints commits since a specified date, for all dependencies of the build target.
+ * The output is grouped by repository, and condensed to one line per commit.
+ * The date is in ISO 8601 format
+ *
+ * For example, to see all commits since Oct 1, 2015 at 3:52pm:
+ * grunt commits-since --date="2015-10-01 15:52"
+ *
+ * To count the number of commits, use the power of the shell:
+ * grunt commits-since --date="2015-10-01 15:52" | grep -v since | wc -l
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
  */
 
 import assert from 'assert';
+import execute from '../../../perennial-alias/js/common/execute.js';
 import getOption from '../../../perennial-alias/js/grunt/tasks/util/getOption.js';
 import getRepo from '../../../perennial-alias/js/grunt/tasks/util/getRepo.js';
-
-const commitsSince = require( '../grunt/commitsSince.js' );
-
-const repo = getRepo();
-const dateString = getOption( 'date' );
-assert( dateString, 'missing required option: --date={{DATE}}' );
+import getPhetLibs from '../grunt/getPhetLibs.js';
 
 ( async () => {
-  await commitsSince( repo, dateString );
+
+  const repo = getRepo();
+  const dateString = getOption( 'date' );
+  assert( dateString, 'missing required option: --date={{DATE}}' );
+
+  let output = '';
+  for ( const dependency of getPhetLibs( repo ) ) {
+    output += `${dependency} since ${dateString} ----------------------------------------------\n`;
+
+    // TODO: https://github.com/phetsims/perennial/issues/403
+    // eslint-disable-next-line @typescript-eslint/no-base-to-string
+    output += await execute( 'git', [ 'log', `--since="${dateString}"`, '--pretty=tformat:"%h | %ci | %cn | %s"' ], `../${dependency}` );
+  }
+
+  console.log( output );
 } )();
