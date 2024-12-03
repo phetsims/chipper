b1538b4209e210421eddfba19cd3040608979961
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-12-03 12:56:29 -0700

    new eslint config for browser-and-node, https://github.com/phetsims/perennial/issues/418

---
+++ js/browser-and-node/eslint.config.mjs
@@ -0,0 +1,11 @@
+// Copyright 2024, University of Colorado Boulder
+
+import browserAndNodeEslintConfig from '../../../perennial-alias/js/eslint/config/browser-and-node.eslint.config.mjs';
+
+/**
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
+export default [
+  ...browserAndNodeEslintConfig
+];
