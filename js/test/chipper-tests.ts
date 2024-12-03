f292194a96c1d8461b904da4213ced48fc47071b
phet-dev <phetadmin@gmail.com>
2024-12-03 03:23:19 -0700

    update copyright dates from daily grunt work

--- js/test/chipper-tests.ts
+++ js/test/chipper-tests.ts
@@ -1,4 +1,4 @@
-// Copyright 2019, University of Colorado Boulder
+// Copyright 2019-2024, University of Colorado Boulder
 
 /**
  * launch point to load any tests located around the chipper repo. This is to support running `qunit` with no args
