f8019234aa041865a14d09af2d6dc8c4b6425f98
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-12-04 11:48:35 -0700

    workarounds to support nesting linting configs in WebStorm, https://github.com/phetsims/chipper/issues/1541

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,5 +15,12 @@ export default [
     rules: {
       'phet/bad-chipper-text': 'error'
     }
+  },
+  {
+    // TODO: Use eslint/config/util/allowFloatingPromises instead, see https://github.com/phetsims/chipper/issues/1541
+    files: [ 'js/scripts/**/*', 'js/grunt/tasks/**/*' ],
+    rules: {
+      '@typescript-eslint/no-floating-promises': 'off'
+    }
   }
 ];
