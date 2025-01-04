a19de3b54d3ecd900b98ca83718924c8758dc664
phet-dev <phetadmin@gmail.com>
2025-01-04 03:37:11 -0700

    update copyright dates from daily grunt work

--- js/grunt/tasks/transpile.ts
+++ js/grunt/tasks/transpile.ts
@@ -1,4 +1,4 @@
-// Copyright 2013-2024, University of Colorado Boulder
+// Copyright 2013-2025, University of Colorado Boulder
 
 import getOption, { isOptionKeyProvided } from '../../../../perennial-alias/js/grunt/tasks/util/getOption.js';
 import { combineOptions } from '../../../../phet-core/js/optionize.js';
