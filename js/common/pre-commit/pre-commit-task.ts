4cd9fe6c91a7de704404a88c939236e7c998d064
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-02-21 10:12:12 -0700

    minor log, https://github.com/phetsims/chipper/issues/1557

--- js/common/pre-commit/pre-commit-task.ts
+++ js/common/pre-commit/pre-commit-task.ts
@@ -47,7 +47,7 @@ const repo = getArg( 'repo' );
     // Run lint tests if they exist in the checked-out SHAs.
     // lint() automatically filters out non-lintable repos
     const lintSuccess = await lint( [ repo ] );
-    outputToConsole && console.log( `Linting had ${lintSuccess ? 'no ' : ''}errors.` );
+    outputToConsole && console.log( `lint: ${lintSuccess ? 'no ' : ''}errors.` );
     process.exit( lintSuccess ? 0 : 1 );
   }
 
