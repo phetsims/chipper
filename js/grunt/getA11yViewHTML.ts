9f1775e5cd4b8a03bbf15882f6bb2488975f5e07
phet-dev <phetadmin@gmail.com>
2025-03-01 03:33:30 -0700

    update copyright dates from daily grunt work

--- js/grunt/getA11yViewHTML.ts
+++ js/grunt/getA11yViewHTML.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2024, University of Colorado Boulder
+// Copyright 2016-2025, University of Colorado Boulder
 
 import fixEOL from '../../../perennial-alias/js/common/fixEOL.js';
 import grunt from '../../../perennial-alias/js/npm-dependencies/grunt.js';
