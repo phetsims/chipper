f555bdb1499ae30033b81bf5672f1aaeff74ec9d
phet-dev <phetadmin@gmail.com>
2025-03-21 03:17:23 -0600

    update copyright dates from daily grunt work

--- js/grunt/tasks/default.ts
+++ js/grunt/tasks/default.ts
@@ -1,4 +1,4 @@
-// Copyright 2024, University of Colorado Boulder
+// Copyright 2024-2025, University of Colorado Boulder
 
 /**
  * Default command which runs lint-project, report-media, clean, and build.
