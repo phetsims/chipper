8d83d9c1434d75160717ce1ed70186355462fe02
Jesse <jesse.greenberg@colorado.edu>
2025-06-12 13:08:07 -0400

    move FluentVisitor to its own file, see https://github.com/phetsims/chipper/issues/1588

--- js/browser-and-node/FluentLibrary.ts
+++ js/browser-and-node/FluentLibrary.ts
@@ -17,66 +17,10 @@ import { FluentBundle } from '../../../sherpa/lib/fluent/fluent-bundle-0.18.0/sr
 import { FluentResource } from '../../../sherpa/lib/fluent/fluent-bundle-0.18.0/src/resource.js';
 import { Message } from '../../../sherpa/lib/fluent/fluent-syntax-0.19.0/src/ast.js';
 import { FluentParser } from '../../../sherpa/lib/fluent/fluent-syntax-0.19.0/src/parser.js';
-import { Visitor } from '../../../sherpa/lib/fluent/fluent-syntax-0.19.0/src/visitor.js';
 import { Entry } from '../../../sherpa/lib/fluent/fluent-syntax-0.19.0/src/ast.js';
 import { Pattern as SyntaxPattern } from '../../../sherpa/lib/fluent/fluent-syntax-0.19.0/src/ast.js';
 import { Resource } from '../../../sherpa/lib/fluent/fluent-syntax-0.19.0/src/ast.js';
-
-/**
- * A visitor that collects Nodes from the AST so we can inspect them for problems.
- */
-export class FluentVisitor extends Visitor {
-  public readonly usedTermNames = new Set<string>();
-  public readonly usedTerms = new Set<Pattern>();
-  public readonly foundJunk = new Set<string>();
-  public readonly declaredTerms = new Set<string>();
-
-  public readonly referencedMessages = new Set<string>();
-
-  public override visitTerm( node: IntentionalAny ): void {
-    this.declaredTerms.add( node );
-
-    this.genericVisit( node );
-  }
-
-  /**
-   * Visitor for messages that are referenced in another value. Collects
-   * keys in an array for inspection.
-   * For example,
-   *
-   *   name = Fred
-   *   hello_pattern = Hello, { fred }
-   *
-   * Key "fred" will be added to referencedMessages.
-   * @param node
-   */
-  public override visitMessageReference( node: IntentionalAny ): void {
-
-    // Add the message name to the set of used messages
-    this.referencedMessages.add( node.id.name );
-
-    this.genericVisit( node );
-  }
-
-  // IntentionalAny because the node type could not be found in Fluent source.
-  public override visitTermReference( node: IntentionalAny ): void {
-
-    // Add the term name to the set of used terms
-    this.usedTermNames.add( node.id.name );
-    this.usedTerms.add( node );
-
-    // Continue traversing the AST
-    this.genericVisit( node );
-  }
-
-  // Nodes with syntax errors are called "junk" in Fluent and can be visited with this method.
-  public override visitJunk( node: IntentionalAny ): void {
-
-    this.foundJunk.add( node );
-
-    this.genericVisit( node );
-  }
-}
+import FluentVisitor from './FluentVisitor.js';
 
 class FluentLibrary {
 
